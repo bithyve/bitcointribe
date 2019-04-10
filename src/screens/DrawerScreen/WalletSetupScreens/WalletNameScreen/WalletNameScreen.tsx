f33e9e4636eeaa5d06c746d1a07d7312eb8fabdc
appa3151 <appasahebl@bithyve.com>
2019-04-10 18:34:42 +0530

    react and react-native version change,android backgroundColor issue generate.
    Former-commit-id: 8195ad27af76c89c9760248b0393309d94f47e2b

--- src/screens/DrawerScreen/WalletSetupScreens/WalletNameScreen/WalletNameScreen.tsx
+++ src/screens/DrawerScreen/WalletSetupScreens/WalletNameScreen/WalletNameScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, ImageBackground, View } from "react-native";
+import { StyleSheet, ImageBackground, View, Platform } from "react-native";
 import {
     Container,
     Header,
@@ -16,7 +16,7 @@ import {
 import { Icon } from "@up-shared/components";
 import IconFontAwe from "react-native-vector-icons/FontAwesome";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import Pagination, { PagiIcon, Dot } from 'react-native-pagination';
+
 
 //TODO: Custome Pages
 import CustomeStatusBar from "bithyve/src/app/custcompontes/CustomeStatusBar/CustomeStatusBar";
@@ -28,23 +28,78 @@ import { colors, images, localDB } from "bithyve/src/app/constants/Constants";
 
 
 export default class WalletNameScreen extends React.Component<any, any> {
+
+    constructor ( props: any ) {
+        super( props )
+        this.state = ( {
+            walletName: null,
+            flag_ProceedBtnDisable: true
+        } );
+    }
+
+    //TODO: Wallet Name
+    ckeckWalletName( val: string ) {
+        if ( val.length >= 6 ) {
+            this.setState( {
+                flag_ProceedBtnDisable: false
+            } )
+        } else {
+            this.setState( {
+                flag_ProceedBtnDisable: true
+            } )
+        }
+    }
+    //TODO: func click_Proceed
+    click_Proceed() {
+        this.props.navigation.push( "FirstSecretQuestionScreen", {
+            walletName: this.state.walletName
+        } )
+    }
+
     render() {
         return (
             <View style={ styles.container }>
                 <ImageBackground source={ images.WalletSetupScreen.WalletScreen.backgoundImage } style={ styles.container }>
-                    <View style={ styles.viewPagination }>
-                        <Text style={ { fontWeight: "bold", fontFamily: "FiraSans-Medium", fontSize: 22, textAlign: "center" } }>Step 1: What do you want to call your Wallet?</Text>
-                        <Text note style={ { marginTop: 20, textAlign: "center" } }>This name will display on you wallet.</Text>
+                    <CustomeStatusBar backgroundColor={ colors.white } flagShowStatusBar={ false } barStyle="dark-content" />
+                    <View style={ { marginLeft: 10, marginTop: 15 } }>
+                        <Button
+                            transparent
+                            onPress={ () => this.props.navigation.pop() }
+                        >
+                            <Icon name="icon_back" size={ Platform.OS == "ios" ? 25 : 20 } color="#000000" />
+                            <Text style={ { color: "#000000", alignSelf: "center", fontSize: Platform.OS == "ios" ? 25 : 20, marginLeft: 0, fontFamily: "FiraSans-Medium" } }>Set up your wallet</Text>
+                        </Button>
                     </View>
-                    <View style={ styles.viewInputFiled }>
-                        <Item rounded style={ styles.itemInputWalletName }>
-                            <Input placeholder='Enter a name for your wallet' placeholderTextColor="#B7B7B7" />
-                        </Item>
-                    </View>
-                    <View style={ styles.viewProcedBtn }>
-                        <Text note style={ { textAlign: "center", marginLeft: 20, marginRight: 20, marginBottom: 20 } } numberOfLines={ 1 }>Lorem ipsum dolor sit amet, consectetur adipiscing </Text>
-                        <FullLinearGradientButton title="Proceed" disabled={ true } style={ { borderRadius: 10 } } />
-                    </View>
+                    <KeyboardAwareScrollView
+                        enableOnAndroid
+                        extraScrollHeight={ 40 }
+                        contentContainerStyle={ { flexGrow: 1, } }
+                    >
+                        <View style={ styles.viewPagination }>
+                            <Text style={ { fontWeight: "bold", fontFamily: "FiraSans-Medium", fontSize: 22, textAlign: "center" } }>Step 1: What do you want to call your Wallet?</Text>
+                            <Text note style={ { marginTop: 20, textAlign: "center" } }>This name will display on you wallet.</Text>
+                        </View>
+                        <View style={ styles.viewInputFiled }>
+                            <Item rounded style={ styles.itemInputWalletName }>
+                                <Input
+                                    keyboardType="default"
+                                    autoCapitalize='sentences'
+                                    placeholder='Enter a name for your wallet'
+                                    placeholderTextColor="#B7B7B7"
+                                    onChangeText={ ( val ) => {
+                                        this.setState( {
+                                            walletName: val
+                                        } )
+                                        this.ckeckWalletName( val )
+                                    } }
+                                />
+                            </Item>
+                        </View>
+                        <View style={ styles.viewProcedBtn }>
+                            <Text note style={ { textAlign: "center", marginLeft: 20, marginRight: 20, marginBottom: 20 } } numberOfLines={ 1 }>Lorem ipsum dolor sit amet, consectetur adipiscing </Text>
+                            <FullLinearGradientButton title="Proceed" disabled={ this.state.flag_ProceedBtnDisable } style={ [ this.state.flag_ProceedBtnDisable == true ? { opacity: 0.4 } : { opacity: 1 }, { borderRadius: 10 } ] } click_Done={ () => this.click_Proceed() } />
+                        </View>
+                    </KeyboardAwareScrollView>
                 </ImageBackground>
             </View>
 
@@ -55,7 +110,7 @@ export default class WalletNameScreen extends React.Component<any, any> {
 const styles = StyleSheet.create( {
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#FCFCFC",
     },
     viewPagination: {
         flex: 2,
