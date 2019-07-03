dfeb95690cfa433839054945d4414c23f133e39b
appa3151 <appasahebl@bithyve.com>
2019-07-03 19:17:16 +0530

    secret screen.
    Former-commit-id: 9189bf69689b812fe0a1c53420c4915e51b0ecc2

--- android/app/src/main/java/com/hexawallet/MainActivity.java
+++ android/app/src/main/java/com/hexawallet/MainActivity.java
@@ -4,7 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
+   
 public class MainActivity extends ReactActivity {
 
     /**
