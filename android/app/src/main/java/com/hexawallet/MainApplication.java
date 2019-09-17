f7f741e87215fdd70760f0d5e9c0a06991b20d13
appasaheb4 <onlyappasaheb4@gmail.com>
2019-09-17 17:26:35 +0530

    helper model full screen showing android.
    Former-commit-id: e8712f84ed9f6d9ba3acd13bdc85ef330b2aad71

--- android/app/src/main/java/com/hexawallet/MainApplication.java
+++ android/app/src/main/java/com/hexawallet/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
 import com.proyecto26.inappbrowser.RNInAppBrowserPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.hopding.pdflib.PDFLibPackage;
