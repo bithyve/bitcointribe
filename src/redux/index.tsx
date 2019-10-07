0507ab6fd37ba31ec49cd2a41ead01b9e04fbe3b
appasaheb4 <onlyappasaheb4@gmail.com>
2019-10-07 17:23:22 +0530

    setup account.
    Former-commit-id: 298eb643a50cf38ccd13a32f3dbfc28bb8bd9b7a

---
+++ src/redux/index.tsx
@@ -0,0 +1,48 @@
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { all, fork } from 'redux-saga/effects';
+
+// Import from ReduxDucks.
+
+import {
+    setupAccounts,
+    createRegularAccount,
+    createSecureAccount,
+    createSSS,
+    walletReducer,
+    watcherSetupAccounts,
+    watcherRegularAccount,
+    watcherSecureAccount,
+    watcherSSS
+} from './wallet';
+
+const reducers = combineReducers( { walletReducer } );
+
+const rootSaga = function* () {
+    yield all( [
+        fork( watcherSetupAccounts ),
+        fork( watcherRegularAccount ),
+        fork( watcherSecureAccount ),
+        fork( watcherSSS )
+    ] );
+};
+
+const middleWare = [];
+
+// Setup Redux-Saga
+const sagaMiddleware = createSagaMiddleware();
+middleWare.push( sagaMiddleware );
+
+const store = createStore( reducers, {}, composeWithDevTools( applyMiddleware( ...middleWare ) ) );
+
+// Initiate root saga.
+sagaMiddleware.run( rootSaga );
+
+export {
+    store,
+    setupAccounts,
+    createRegularAccount,
+    createSecureAccount,
+    createSSS
+};
