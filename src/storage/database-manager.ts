1e1170b2af991801c3f780f4077ae7bb0969b530
Parsh <parsh.cosmos11@gmail.com>
2019-11-09 06:47:35 +0530

    database, secure-store, redux store and saga

---
+++ src/storage/database-manager.ts
@@ -0,0 +1,13 @@
+import * as database from "./database";
+
+const initialize = () => database.init();
+const fetch = () => database.fetch();
+const insert = (encData: string) => database.insert(encData);
+const update = (encData: string) => database.update(encData);
+
+export default {
+  initialize,
+  fetch,
+  insert,
+  update
+};
---
+++ src/store/actions/storage.ts
@@ -0,0 +1,38 @@
+// types and action creators (pure): dispatched from components
+export const INIT_DB = "INIT_DB";
+export const FETCH_FROM_DB = "FETCH_FROM_DB";
+export const INSERT_INTO_DB = "INSERT_INTO_DB";
+export const KEY_FETCHED = "KEY_FETCHED";
+
+export const initializeDB = () => {
+  return { type: INIT_DB };
+};
+
+export const fetchFromDB = () => {
+  return { type: FETCH_FROM_DB };
+};
+
+export const insertIntoDB = data => {
+  return { type: INSERT_INTO_DB, payload: { data } };
+};
+
+export const keyFetched = key => {
+  return { type: KEY_FETCHED, payload: { key } };
+};
+
+// types and action creators (saga): dispatched from saga workers
+export const DB_INITIALIZED = "DB_INITIALIZED";
+export const DB_FETCHED = "DB_FETCHED";
+export const DB_INSERTED = "DB_INSERTED";
+
+export const dbInitialized = initialized => {
+  return { type: DB_INITIALIZED, payload: { initialized } };
+};
+
+export const dbFetched = database => {
+  return { type: DB_FETCHED, payload: { database } };
+};
+
+export const dbInserted = inserted => {
+  return { type: DB_INSERTED, payload: { inserted } };
+};
