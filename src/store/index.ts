da1dd5fdc080afa43874e974744a2d9f8fca2067
Parsh <parsh.cosmos11@gmail.com>
2019-11-12 17:22:51 +0530

    non-crashing root saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
-import { fork, all } from "redux-saga/effects";
+import { call, all, spawn } from "redux-saga/effects";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import {
@@ -18,21 +18,53 @@ import {
   fetchTransactionsWatcher
 } from "./sagas/accounts";
 
-const rootSaga = function*() {
-  yield all([
-    // database watchers
-    fork(initDBWatcher),
-    fork(fetchDBWatcher),
-    fork(insertDBWatcher),
+// const rootSaga = function*() {
+//   yield all([
+//     // database watchers
+//     fork(initDBWatcher),
+//     fork(fetchDBWatcher),
+//     fork(insertDBWatcher),
 
-    // wallet setup watchers
-    fork(initSetupWatcher),
+//     // wallet setup watchers
+//     fork(initSetupWatcher),
+
+//     // accounts watchers
+//     fork(fetchAddrWatcher),
+//     fork(fetchBalanceWatcher),
+//     fork(fetchTransactionsWatcher)
+//   ]);
+// };
+
+const rootSaga = function*() {
+  const sagas = [
+    // database watchers
+    initDBWatcher,
+    fetchDBWatcher,
+    insertDBWatcher,
+
+    // wallet setup watcher
+    initSetupWatcher,
 
     // accounts watchers
-    fork(fetchAddrWatcher),
-    fork(fetchBalanceWatcher),
-    fork(fetchTransactionsWatcher)
-  ]);
+    fetchAddrWatcher,
+    fetchBalanceWatcher,
+    fetchTransactionsWatcher
+  ];
+
+  yield all(
+    sagas.map(saga =>
+      spawn(function*() {
+        while (true) {
+          try {
+            yield call(saga);
+            break;
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      })
+    )
+  );
 };
 
 const rootReducer = combineReducers({
