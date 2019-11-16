17ac5b1237fcd89322e639fb7ab04b057b20c3e0
Parsh <parsh.cosmos11@gmail.com>
2019-11-16 11:07:06 +0530

    cached MShares

--- src/store/sagas/sss.ts
+++ src/store/sagas/sss.ts
@@ -15,74 +15,6 @@ import {
   IBuddyStaticNonPMDD
 } from "../../bitcoin/utilities/Interface";
 
-const prepareStaticNonPMDD = s3Service => {
-  // mocking till the availability of secure account
-  const secondaryMnemonic =
-    "unique issue slogan party van unfair assault warfare then rubber satisfy snack";
-  const twoFASecret = "some_secret";
-  const dummyBHXpub =
-    "tpubDDGFLSXWA8K2z1fmg37ivgFmQC5xBMcEycBD5pmYuzpM1GyeoeTeykZxFFcQpVVGvv6scShvtCfRkf59tzJzwoL44VMRR78RywTU46TvCrJ";
-  const dummySecoundryXpub =
-    "tpubDDGFLSXWA8K2z1fmg37ivgFmQC5xBMcEycBD5pmYuzpM1GyeoeTeykZxFFcQpVVGvv6scShvtCfRkf59tzJzwoL44VMRR78RywTU46TvCrJ";
-
-  const socialStaticNonPMDD: ISocialStaticNonPMDD = {
-    secondaryXpub: dummySecoundryXpub,
-    bhXpub: dummyBHXpub
-  };
-  const buddyStaticNonPMDD: IBuddyStaticNonPMDD = {
-    secondaryMnemonic,
-    twoFASecret,
-    secondaryXpub: dummySecoundryXpub,
-    bhXpub: dummyBHXpub
-  };
-
-  const encryptedSocialStaticNonPMDD = s3Service.encryptStaticNonPMDD(
-    socialStaticNonPMDD
-  );
-  const encryptedBuddyStaticNonPMDD = s3Service.encryptStaticNonPMDD(
-    buddyStaticNonPMDD
-  );
-
-  return {
-    encryptedSocialStaticNonPMDD,
-    encryptedBuddyStaticNonPMDD
-  };
-};
-
-const generateMetaShares = (
-  s3Service,
-  encryptedSocialStaticNonPMDD,
-  encryptedBuddyStaticNonPMDD
-) => {
-  let index = 0;
-  const metaShares = [];
-  const tag = "Random";
-  for (const encryptedShare of s3Service.sss.encryptedShares) {
-    let res;
-    if (index !== 2) {
-      res = s3Service.createMetaShare(
-        index + 1,
-        encryptedShare,
-        encryptedSocialStaticNonPMDD,
-        tag
-      );
-    } else {
-      res = s3Service.createMetaShare(
-        index + 1,
-        encryptedShare,
-        encryptedBuddyStaticNonPMDD,
-        tag
-      );
-    }
-    const { metaShare } = res.data;
-    metaShares.push(metaShare);
-  }
-  if (metaShares.length !== 5) {
-    throw new Error("Something went wrong while generating metaShares");
-  }
-  return { metaShares };
-};
-
 function* initHCWorker() {
   const s3Service: S3Service = yield select(state => state.sss.service);
   const initialized = s3Service.sss.healthCheckInitialized;
@@ -103,32 +35,39 @@ function* initHCWorker() {
 
 export const initHCWatcher = createWatcher(initHCWorker, INIT_HEALTH_CHECK);
 
-function* prepareMShareWorker() {
+function* generateMetaSharesWorker() {
   const s3Service: S3Service = yield select(state => state.sss.service);
+  const { walletName } = yield select(
+    state => state.storage.database.WALLET_SETUP
+  );
   // const secureAccount: SecureAccount = yield select(
   //   state => state.accounts[SECURE_ACCOUNT].service
   // );
 
-  const {
-    encryptedSocialStaticNonPMDD,
-    encryptedBuddyStaticNonPMDD
-  } = yield call(prepareStaticNonPMDD, s3Service);
+  // mocking till the availability of secure account
+  const secondaryMnemonic =
+    "unique issue slogan party van unfair assault warfare then rubber satisfy snack";
+  const twoFASecret = "some_secret";
+  const bhXpub =
+    "tpubDDGFLSXWA8K2z1fmg37ivgFmQC5xBMcEycBD5pmYuzpM1GyeoeTeykZxFFcQpVVGvv6scShvtCfRkf59tzJzwoL44VMRR78RywTU46TvCrJ";
+  const secondaryXpub =
+    "tpubDDGFLSXWA8K2z1fmg37ivgFmQC5xBMcEycBD5pmYuzpM1GyeoeTeykZxFFcQpVVGvv6scShvtCfRkf59tzJzwoL44VMRR78RywTU46TvCrJ";
 
-  const { metaShares } = yield call(
-    generateMetaShares,
-    s3Service,
-    encryptedSocialStaticNonPMDD,
-    encryptedBuddyStaticNonPMDD
-  );
+  const secureAssets = {
+    secondaryMnemonic,
+    twoFASecret,
+    secondaryXpub,
+    bhXpub
+  };
 
-  console.log({ metaShares });
-  // if (res.status === 200) {
-  //   yield put(healthCheckInitialized());
-  //   yield put(insertIntoDB({ S3_SERVICE: JSON.stringify(s3Service) }));
-  // }
+  if (s3Service.sss.metaShares.length) return;
+  const res = yield call(s3Service.createMetaShares, secureAssets, walletName);
+  if (res.status === 200) {
+    yield put(insertIntoDB({ S3_SERVICE: JSON.stringify(s3Service) }));
+  }
 }
 
-export const prepareMShareWatcher = createWatcher(
-  prepareMShareWorker,
+export const generateMetaSharesWatcher = createWatcher(
+  generateMetaSharesWorker,
   PREPARE_MSHARES
 );
