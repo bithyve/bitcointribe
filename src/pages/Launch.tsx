dc53c1cc4cab3cee2dd8ea97657d4f480007ebcb
Parsh <parsh.cosmos11@gmail.com>
2019-11-19 15:19:43 +0530

    tweaks and 0.60>>0.59 hacks

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,41 +1,36 @@
-import React, { useEffect } from 'react';
-import {
-	View,
-	StyleSheet,
-	StatusBar
-} from 'react-native';
-import Video from 'react-native-video';
-import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import React, { useEffect } from "react";
+import { View, StyleSheet, StatusBar } from "react-native";
+import Video from "react-native-video";
 
 export default function Launch(props) {
-	setTimeout(() => {
-		props.navigation.replace('PasscodeConfirm');
-	}, 5000);
-	return (
-		<View style={styles.container}>
-			<Video
-				source={require('./../assets/video/splash_animation.mp4')}
-				style={{
-					flex: 1
-				}}
-				muted={true}
-				repeat={false}
-				resizeMode={'cover'}
-				rate={1.0}
-				ignoreSilentSwitch={'obey'}
-			/>
-			<StatusBar
-				backgroundColor={'white'}
-				hidden={true}
-				barStyle="dark-content"
-			/>
-		</View>
-	);
+  setTimeout(() => {
+    props.navigation.replace("PasscodeConfirm");
+  }, 5000);
+  return (
+    <View style={styles.container}>
+      <Video
+        source={require("../assets/video/splash_animation.mp4")}
+        style={{
+          flex: 1
+        }}
+        muted={true}
+        repeat={false}
+        resizeMode={"cover"}
+        rate={1.0}
+        ignoreSilentSwitch={"obey"}
+      />
+      <StatusBar
+        backgroundColor={"white"}
+        hidden={true}
+        barStyle="dark-content"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff'
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
 });
