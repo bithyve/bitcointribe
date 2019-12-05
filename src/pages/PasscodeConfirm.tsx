f6bd971d803faa800c1ca96fc82def02fab599b9
Parsh <parsh.cosmos11@gmail.com>
2019-12-06 05:08:54 +0530

    Screen mods

--- src/pages/PasscodeConfirm.tsx
+++ src/pages/PasscodeConfirm.tsx
@@ -215,7 +215,7 @@ export default function PasscodeConfirm(props) {
           {passcode.length == 4 ? (
             <View>
               <Text style={styles.headerInfoText}>
-                Re-enter the <Text style={styles.boldItalicText}>passcode</Text>
+                Re-enter the <Text style={styles.boldItalicText}>passcode</Text>{" "}
                 to <Text style={styles.boldItalicText}>verify and login.</Text>
               </Text>
               <View>
