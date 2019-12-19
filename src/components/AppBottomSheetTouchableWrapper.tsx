e478ae808b59b1622df244f8841068d2e16c2afa
Nikhita Gawande <37994396+cakesoft-nikhita@users.noreply.github.com>
2019-12-19 16:28:45 +0530

    - Resolved model click issue on Home page(BottomSheet library issue is open for Platform specific TouchableOpacity) - Create Wrapper to handle platform specific TouchableOpacity - Changed Home model's touchable opacity to wrapper

---
+++ src/components/AppBottomSheetTouchableWrapper.tsx
@@ -0,0 +1,13 @@
+import { TouchableOpacity, TouchableOpacityProps, Platform} from "react-native";
+import { TouchableOpacity as TouchableOpacityGestureHandler } from "react-native-gesture-handler";
+import React, { ReactNode } from "react";
+
+export const AppBottomSheetTouchableWrapper = (
+    props: { children: ReactNode } & Pick<TouchableOpacityProps,'onPress' | 'style' | 'activeOpacity' | 'disabled'>,
+  ) =>
+    Platform.select({
+      android: <TouchableOpacityGestureHandler {...props} />,
+      ios: (
+        <TouchableOpacity {...props}/>
+      ),
+    });
