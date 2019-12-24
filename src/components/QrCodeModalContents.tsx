c182f9f23f25a2ab748777d984f03da5cc63ad4d
appasaheb4 <onlyappasaheb4@gmail.com>
2019-12-24 16:28:09 +0530

    camera qrcode template add.

--- src/components/QrCodeModalContents.tsx
+++ src/components/QrCodeModalContents.tsx
@@ -35,7 +35,23 @@ export default function QrCodeModalContents( props ) {
 		<KeyboardAvoidingView style={ { flex: 1 } } behavior={ Platform.OS == 'ios' ? 'padding' : '' } enabled>
 			<ScrollView style={ styles.qrModalScrollView }>
 				<View style={ styles.qrModalImageNTextInputView }>
-
+					<RNCamera
+						ref={ cameraRef }
+						style={ {
+							width: wp( '100%' ),
+							height: 20
+						} }
+						onBarCodeRead={ barcodeRecognized }
+					>
+						<View style={ { flexDirection: 'row', paddingTop: 12, paddingRight: 12, paddingLeft: 12, width: '100%' } }>
+							<View style={ { borderLeftWidth: 1, borderTopColor: 'white', borderLeftColor: 'white', height: hp( '5%' ), width: hp( '5%' ), borderTopWidth: 1 } } />
+							<View style={ { borderTopWidth: 1, borderRightWidth: 1, borderRightColor: 'white', borderTopColor: 'white', height: hp( '5%' ), width: hp( '5%' ), marginLeft: 'auto' } } />
+						</View>
+						<View style={ { marginTop: 'auto', flexDirection: 'row', paddingBottom: 30, paddingRight: 12, paddingLeft: 12, width: '100%', } }>
+							<View style={ { borderLeftWidth: 1, borderBottomColor: 'white', borderLeftColor: 'white', height: hp( '5%' ), width: hp( '5%' ), borderBottomWidth: 1 } } />
+							<View style={ { borderBottomWidth: 1, borderRightWidth: 1, borderRightColor: 'white', borderBottomColor: 'white', height: hp( '5%' ), width: hp( '5%' ), marginLeft: 'auto' } } />
+						</View>
+					</RNCamera>
 					{/* <ImageBackground source={require('../assets/images/icons/iPhone-QR.png')} style={styles.qrModalImage} >
                         <View style={{ flexDirection: 'row', paddingTop: 10, paddingRight: 5, paddingLeft: 10, width: '100%' }}>
                             <View style={{ borderLeftWidth: 1, borderTopColor: 'white', borderLeftColor: 'white', height: hp('5%'), width: hp('5%'), borderTopWidth: 1 }} />
