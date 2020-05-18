4ac1764a014d360c2756cc89c9fa12ba0a849a6b
Parsh <parsh.cosmos11@gmail.com>
2020-05-18 10:26:56 +0530

    transfer mod integration

--- src/pages/Accounts/Confirmation.tsx
+++ src/pages/Accounts/Confirmation.tsx
@@ -16,13 +16,13 @@ import {
 import { useDispatch, useSelector } from 'react-redux';
 import SendStatusModalContents from '../../components/SendStatusModalContents';
 
-const Confirmation = props => {
+const Confirmation = (props) => {
   const serviceType = props.navigation.getParam('serviceType');
   const recipientAddress = props.navigation.getParam('recipientAddress');
   const amount = props.navigation.getParam('amount');
 
   const { transfer, loading } = useSelector(
-    state => state.accounts[serviceType],
+    (state) => state.accounts[serviceType],
   );
 
   const dispatch = useDispatch();
@@ -78,7 +78,8 @@ const Confirmation = props => {
           <Button
             title="Send"
             onPress={() => {
-              dispatch(transferST2(serviceType));
+              const txnPriority = 'low';
+              dispatch(transferST2(serviceType, txnPriority));
             }}
           />
           <Button
