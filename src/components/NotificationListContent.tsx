d9a8b499de3b117ef81449e6fe1b264188fd8c9c
cakesoft-shivani <shivani@cakesofttech.com>
2020-06-02 12:34:24 +0530

    Notification UI changes and Removed unwanted code

--- src/components/NotificationListContent.tsx
+++ src/components/NotificationListContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Image, Text, StyleSheet, Platform } from 'react-native';
 import {
   widthPercentageToDP as wp,
@@ -6,12 +6,8 @@ import {
 } from 'react-native-responsive-screen';
 import Colors from '../common/Colors';
 import Fonts from '../common/Fonts';
-import CommonStyles from '../common/Styles';
 import { RFValue } from 'react-native-responsive-fontsize';
-import ContactList from '../components/ContactList';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import BottomInfoBox from './BottomInfoBox';
 import { AppBottomSheetTouchableWrapper } from '../components/AppBottomSheetTouchableWrapper';
 import { ScrollView } from 'react-native-gesture-handler';
 import DeviceInfo from 'react-native-device-info';
@@ -39,244 +35,98 @@ export default function NotificationListContent(props) {
             <AppBottomSheetTouchableWrapper
               onPress={() => props.onNotificationClicked(value)}
               style={{
-                paddingLeft: wp('7%'),
-                paddingRight: wp('4%'),
-                borderBottomWidth: 1,
-                borderBottomColor: Colors.borderColor,
-                paddingBottom: wp('4%'),
-                paddingTop: wp('4%'),
-                backgroundColor: value.read
-                  ? Colors.white
-                  : Colors.shadowBlue,
+                ...styles.notificationElement,
+                backgroundColor: value.read ? Colors.white : Colors.shadowBlue,
               }}
             >
               <View style={{ flexDirection: 'row' }}>
-                <View
-                  style={{ flexDirection: 'row', alignItems: 'center' }}
-                >
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                   <Image
                     source={
                       value.type == 'release'
                         ? require('../assets/images/icons/icon_hexa.png')
                         : require('../assets/images/icons/icon_receive.png')
                     }
-                    style={{
-                      width: wp('8%'),
-                      height: wp('8%'),
-                      marginRight: wp('2%'),
-                    }}
+                    style={styles.notificationElementImage}
                   />
-                  <Text
-                    style={{
-                      color: Colors.blue,
-                      fontSize: RFValue(13),
-                      fontFamily: Fonts.FiraSansRegular,
-                    }}
-                  >
+                  <Text style={styles.notificationElementTitle}>
                     {value.title}
                   </Text>
                 </View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    marginLeft: 'auto',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Text
-                    style={{
-                      color: Colors.textColorGrey,
-                      fontSize: RFValue(11),
-                      fontFamily: Fonts.FiraSansRegular,
-                      marginRight: wp('5%'),
-                    }}
-                  >
+                <View style={styles.notificationElementInfoView}>
+                  <Text style={styles.notificationElementTimeText}>
                     {value.time}
                   </Text>
                   {value.isMandatory ? (
-                    <FontAwesome
-                      name="star"
-                      color={Colors.yellow}
-                      size={17}
-                    />
+                    <FontAwesome name="star" color={Colors.yellow} size={17} />
                   ) : (
-                      <View style={{ width: 17 }} />
-                    )}
+                    <View style={{ width: 17 }} />
+                  )}
                 </View>
               </View>
-              <Text
-                style={{
-                  color: Colors.textColorGrey,
-                  fontSize: RFValue(11),
-                  fontFamily: Fonts.FiraSansRegular,
-                  paddingTop: wp('2%'),
-                  marginLeft: 3,
-                }}
-              >
+              <Text style={styles.notificationElementInfoText}>
                 {value.info}
               </Text>
             </AppBottomSheetTouchableWrapper>
           );
         })}
         {props.NotificationData.length <= 1 ? (
-          <View
-            style={{ backgroundColor: Colors.white, marginTop: 'auto' }}
-          >
-            <View
-              style={{
-                margin: 15,
-                backgroundColor: Colors.backgroundColor,
-                marginBottom:
-                  Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
-                padding: 10,
-                paddingTop: 20,
-                paddingBottom: 20,
-                borderRadius: 7,
-              }}
-            >
-              <Text
-                style={{
-                  color: Colors.black,
-                  fontSize: RFValue(13),
-                  fontFamily: Fonts.FiraSansRegular,
-                }}
-              >
-                Notification Drawer
-                  </Text>
-              <Text
-                style={{
-                  color: Colors.textColorGrey,
-                  fontSize: RFValue(12),
-                  fontFamily: Fonts.FiraSansRegular,
-                }}
-              >
-                All your recent notifications are visible here
-                  </Text>
+          <View style={{ backgroundColor: Colors.white, marginTop: 'auto' }}>
+            <View style={styles.waterMarkInfoBoxView}>
+              <Text style={styles.waterMarkInfoBoxTitle}>
+                No notification yet
+              </Text>
+              <Text style={styles.waterMarkInfoBoxInfo}>
+                All your recent notifications will be visible here
+              </Text>
             </View>
           </View>
         ) : null}
       </View>
     </ScrollView>
   ) : (
-      <View style={styles.modalContainer}>
-        <View style={styles.modalHeaderTitleView}>
-          <View style={{ flexDirection: 'row' }}>
-            <AppBottomSheetTouchableWrapper
-              onPress={() => props.onPressBack()}
-              style={{ height: 30, width: 30, justifyContent: 'center' }}
-            >
-              <FontAwesome name="long-arrow-left" color={Colors.blue} size={17} />
-            </AppBottomSheetTouchableWrapper>
-            <View style={{ justifyContent: 'center' }}>
-              <Text style={styles.modalHeaderTitleText}>{'Notifications'}</Text>
-            </View>
-          </View>
-        </View>
-        {[1, 2, 3, 4,].map((value) => {
-          return (
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                paddingTop: wp('5%'),
-                paddingBottom: wp('5%'),
-                borderBottomWidth: 0.5,
-                marginBottom:
-                  Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
-                borderColor: Colors.borderColor,
-              }}
-            >
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View
-                  style={{
-                    backgroundColor: Colors.backgroundColor,
-                    height: wp('5%'),
-                    width: wp('5%'),
-                    borderRadius: wp('5%') / 2,
-                    marginLeft: 10,
-                    marginRight: 10,
-                  }}
-                />
-                <View>
-                  <View
-                    style={{
-                      backgroundColor: Colors.backgroundColor,
-                      height: wp('5%'),
-                      width: wp('25%'),
-                      borderRadius: 10,
-                    }}
-                  />
-                  <View
-                    style={{
-                      backgroundColor: Colors.backgroundColor,
-                      height: wp('5%'),
-                      width: wp('35%'),
-                      marginTop: 5,
-                      borderRadius: 10,
-                    }}
-                  />
-                </View>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <View
-                  style={{
-                    backgroundColor: Colors.backgroundColor,
-                    height: wp('7%'),
-                    width: wp('20%'),
-                    borderRadius: 10,
-                  }}
-                />
-                <View
-                  style={{
-                    backgroundColor: Colors.backgroundColor,
-                    height: wp('5%'),
-                    width: wp('5%'),
-                    borderRadius: wp('5%') / 2,
-                    marginLeft: 10,
-                    marginRight: 10,
-                  }}
-                />
-              </View>
-            </View>
-          );
-        })}
-
-        <View style={{ backgroundColor: Colors.white, marginTop: 'auto' }}>
-          <View
-            style={{
-              margin: 15,
-              backgroundColor: Colors.backgroundColor,
-              padding: 10,
-              paddingTop: 20,
-              paddingBottom: 20,
-              marginBottom:
-                Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
-              borderRadius: 7,
-            }}
+    <View style={styles.modalContainer}>
+      <View style={styles.modalHeaderTitleView}>
+        <View style={{ flexDirection: 'row' }}>
+          <AppBottomSheetTouchableWrapper
+            onPress={() => props.onPressBack()}
+            style={{ height: 30, width: 30, justifyContent: 'center' }}
           >
-            <Text
-              style={{
-                color: Colors.black,
-                fontSize: RFValue(13),
-                fontFamily: Fonts.FiraSansRegular,
-              }}
-            >
-              Notification Drawer
-              </Text>
-            <Text
-              style={{
-                color: Colors.textColorGrey,
-                fontSize: RFValue(12),
-                fontFamily: Fonts.FiraSansRegular,
-              }}
-            >
-              All your recent notifications are visible here
-              </Text>
+            <FontAwesome name="long-arrow-left" color={Colors.blue} size={17} />
+          </AppBottomSheetTouchableWrapper>
+          <View style={{ justifyContent: 'center' }}>
+            <Text style={styles.modalHeaderTitleText}>{'Notifications'}</Text>
           </View>
         </View>
       </View>
-    );
+      {[1, 2, 3, 4].map((value) => {
+        return (
+          <View style={styles.EmptyListLoaderView}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View style={styles.EmptyListLoaderCircle} />
+              <View>
+                <View style={styles.EmptyListLoaderSmallText} />
+                <View style={styles.EmptyListLoaderLargeText} />
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View style={styles.EmptyListLoaderInfoText} />
+              <View style={styles.EmptyListLoaderLargeCircle} />
+            </View>
+          </View>
+        );
+      })}
+
+      <View style={{ backgroundColor: Colors.white, marginTop: 'auto' }}>
+        <View style={styles.infoBoxView}>
+          <Text style={styles.infoBoxTitle}>Notification Drawer</Text>
+          <Text style={styles.infoBoxInfo}>
+            All your recent notifications are visible here
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   modalContainer: {
@@ -298,5 +148,124 @@ const styles = StyleSheet.create({
     color: Colors.blue,
     fontSize: RFValue(18),
     fontFamily: Fonts.FiraSansMedium,
+  },
+  notificationElement: {
+    paddingLeft: wp('7%'),
+    paddingRight: wp('4%'),
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.borderColor,
+    paddingBottom: wp('4%'),
+    paddingTop: wp('4%'),
+  },
+  notificationElementImage: {
+    width: wp('8%'),
+    height: wp('8%'),
+    marginRight: wp('2%'),
+  },
+  notificationElementTitle: {
+    color: Colors.blue,
+    fontSize: RFValue(13),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+  notificationElementInfoView: {
+    flexDirection: 'row',
+    marginLeft: 'auto',
+    alignItems: 'center',
+  },
+  notificationElementTimeText: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(11),
+    fontFamily: Fonts.FiraSansRegular,
+    marginRight: wp('5%'),
+  },
+  notificationElementInfoText: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(11),
+    fontFamily: Fonts.FiraSansRegular,
+    paddingTop: wp('2%'),
+    marginLeft: 3,
+  },
+  waterMarkInfoBoxView: {
+    margin: 15,
+    backgroundColor: Colors.backgroundColor,
+    marginBottom: Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
+    padding: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderRadius: 7,
+  },
+  waterMarkInfoBoxTitle: {
+    color: Colors.black,
+    fontSize: RFValue(13),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+  waterMarkInfoBoxInfo: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(12),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+  EmptyListLoaderView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingTop: wp('5%'),
+    paddingBottom: wp('5%'),
+    borderBottomWidth: 0.5,
+    marginBottom: Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
+    borderColor: Colors.borderColor,
+  },
+  EmptyListLoaderCircle: {
+    backgroundColor: Colors.backgroundColor,
+    height: wp('5%'),
+    width: wp('5%'),
+    borderRadius: wp('5%') / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  EmptyListLoaderSmallText: {
+    backgroundColor: Colors.backgroundColor,
+    height: wp('5%'),
+    width: wp('25%'),
+    borderRadius: 10,
+  },
+  EmptyListLoaderLargeText: {
+    backgroundColor: Colors.backgroundColor,
+    height: wp('5%'),
+    width: wp('35%'),
+    marginTop: 5,
+    borderRadius: 10,
+  },
+  EmptyListLoaderInfoText: {
+    backgroundColor: Colors.backgroundColor,
+    height: wp('7%'),
+    width: wp('20%'),
+    borderRadius: 10,
+  },
+  EmptyListLoaderLargeCircle: {
+    backgroundColor: Colors.backgroundColor,
+    height: wp('5%'),
+    width: wp('5%'),
+    borderRadius: wp('5%') / 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  infoBoxView: {
+    margin: 15,
+    backgroundColor: Colors.backgroundColor,
+    padding: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    marginBottom: Platform.OS == 'ios' && DeviceInfo.hasNotch() ? 30 : 20,
+    borderRadius: 7,
+  },
+  infoBoxTitle: {
+    color: Colors.black,
+    fontSize: RFValue(13),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+  infoBoxInfo: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(12),
+    fontFamily: Fonts.FiraSansRegular,
   },
 });
