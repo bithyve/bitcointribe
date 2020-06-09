af930065b66409c703350b4ffd61cfac519606ea
Parsh <parsh.cosmos11@gmail.com>
2020-06-09 16:50:56 +0530

    Subview spell fix

--- ios/PdfGenerate.swift
+++ ios/PdfGenerate.swift
@@ -73,7 +73,7 @@ import QRCoder
     txtPart.font = UIFont.systemFont(ofSize: 6.0)
     txtPart.textAlignment = .left
     txtPart.sizeToFit()
-    v1.addSubView(txtPart)
+    v1.addSubview(txtPart)
     
     //          let imageTitle:UIImage? = (title as String).image(withAttributes: [.font: UIFont.systemFont(ofSize: 80.0)])
     //          let imageViewTitle = UIImageView()
@@ -361,7 +361,7 @@ import QRCoder
     txtPart.font = UIFont.systemFont(ofSize: 6.0)
     txtPart.textAlignment = .left
     txtPart.sizeToFit()
-    v5.addSubView(txtPart)
+    v5.addSubview(txtPart)
 
     qrCodeImage = UIImageView();
     generator = QRCodeGenerator();
