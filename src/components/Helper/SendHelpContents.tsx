0971bbb0dfc6d9f536f686b96ccd485eda8bf4c4
Shreyas Nisal <shreyasnisal@gmail.com>
2020-06-11 14:22:24 +0530

    Send and Receive help screens

---
+++ src/components/Helper/SendHelpContents.tsx
@@ -0,0 +1,190 @@
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Colors from '../../common/Colors';
+import Fonts from '../../common/Fonts';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { AppBottomSheetTouchableWrapper } from '../AppBottomSheetTouchableWrapper';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default function SendHelpContents(props) {
+  return (
+    <ScrollView style={styles.modalContainer} snapToInterval={hp('89%')}>
+        <View style={{height: hp('89%'), justifyContent: 'space-between', paddingBottom: hp('4%')}}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Text
+                    style={{
+                        color: Colors.white,
+                        fontFamily: Fonts.FiraSansMedium,
+                        fontSize: RFValue(20),
+                        marginTop: hp('1%'),
+                        marginBottom: hp('1%'),
+                    }}
+                >
+                    Sending Bitcoins
+                </Text>
+            </View>
+            <View
+                style={{
+                backgroundColor: Colors.homepageButtonColor,
+                height: 1,
+                marginLeft: wp('5%'),
+                marginRight: wp('5%'),
+                }}
+            />
+            <Text
+            style={{
+                textAlign: 'center',
+                color: Colors.white,
+                fontSize: RFValue(12),
+                fontFamily: Fonts.FiraSansRegular,
+            }}
+            >
+            When you want to send Bitcoins,{'\n'}you need a destination, which is{'\n'}denoted by the recipient’s bitcoin address 
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={require('../../assets/images/icons/send.png')}
+                    style={{ width: wp('50%'), height: wp('50%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <Text
+            style={{
+                textAlign: 'center',
+                color: Colors.white,
+                fontSize: RFValue(12),
+                fontFamily: Fonts.FiraSansRegular,
+            }}
+            >
+                This address is an alphanumeric{'\n'}string and often wallets use a{'\n'}QR code for the same
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                source={require('../../assets/images/icons/rabbit.png')}
+                style={{ width: wp('25%'), height: wp('25%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <View
+                    style={{
+                        borderStyle: 'dotted',
+                        borderWidth: 1,
+                        borderRadius: 1,
+                        borderColor: Colors.white,
+                        width: wp('70%'),
+                        height: 0,
+                    }}
+                />
+            </View>
+        </View>
+        <View style={{height: hp('89%'), justifyContent: 'space-between', paddingTop:hp('2%'), paddingBottom: hp('4%')}}>
+            <Text
+            style={{
+                textAlign: 'center',
+                color: Colors.white,
+                fontSize: RFValue(12),
+                fontFamily: Fonts.FiraSansRegular,
+            }}
+            >
+            Bitcoin transactions can not be{'\n'}reversed or cancelled
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={require('../../assets/images/icons/send.png')}
+                    style={{ width: wp('50%'), height: wp('50%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    color: Colors.white,
+                    fontSize: RFValue(12),
+                    fontFamily: Fonts.FiraSansRegular,
+                }}
+            >
+            For this reason, it is{'\n'}recommended that you scan a{'\n'}QR code instead of keying{'\n'}in the characters
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={require('../../assets/images/icons/rabbit.png')}
+                    style={{ width: wp('25%'), height: wp('25%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <View
+                    style={{
+                        borderStyle: 'dotted',
+                        borderWidth: 1,
+                        borderRadius: 1,
+                        borderColor: Colors.white,
+                        width: wp('70%'),
+                        height: 0,
+                    }}
+                />
+            </View>
+        </View>
+        <View style={{height: hp('89%'), justifyContent: 'space-between', paddingTop:hp('2%'), paddingBottom: hp('4%')}}>
+            <Text
+            style={{
+                textAlign: 'center',
+                color: Colors.white,
+                fontSize: RFValue(12),
+                fontFamily: Fonts.FiraSansRegular,
+            }}
+            >
+                There are multiple types of addresses{'\n'}in Bitcoin. Addresses begin with{'\n'}“1”, “3" or “bc1” for Bitcoin Mainnet,{'\n'}and “2" for the Bitcoin Testnet
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={require('../../assets/images/icons/send.png')}
+                    style={{ width: wp('50%'), height: wp('50%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    color: Colors.white,
+                    fontSize: RFValue(12),
+                    fontFamily: Fonts.FiraSansRegular,
+                }}
+            >
+                Go further down the rabbit hole by clicking here
+            </Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                <Image
+                    source={require('../../assets/images/icons/rabbit.png')}
+                    style={{ width: wp('25%'), height: wp('25%'), resizeMode: 'contain' }}
+                />
+            </View>
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <View
+                    style={{
+                        borderStyle: 'dotted',
+                        borderWidth: 1,
+                        borderRadius: 1,
+                        borderColor: Colors.white,
+                        width: wp('70%'),
+                        height: 0,
+                    }}
+                />
+            </View>
+        </View>
+    </ScrollView>
+  );
+}
+const styles = StyleSheet.create({
+  modalContainer: {
+    height: '100%',
+    backgroundColor: Colors.blue,
+    alignSelf: 'center',
+    width: '100%',
+    paddingBottom: hp('5%'),
+    elevation: 10,
+    shadowColor: Colors.borderColor,
+    shadowOpacity: 10,
+    shadowOffset: { width: 0, height: 2 },
+  },
+});
