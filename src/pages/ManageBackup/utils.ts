982224b9ca3ce40cc4f2f0694345c319f70199c4
AliMeer <ali@purple7.com>
2020-09-19 18:00:35 +0100

    move verifyPersonalCopyAccess to utils

--- src/pages/ManageBackup/utils.ts
+++ src/pages/ManageBackup/utils.ts
@@ -1,3 +1,6 @@
+import { Platform } from 'react-native';
+import RNFS from 'react-native-fs';
+
 export const getIconByStatus = status => {
   if (status == "Ugly") {
     return require("../../assets/images/icons/icon_error_red.png");
@@ -7,3 +10,21 @@ export const getIconByStatus = status => {
     return require("../../assets/images/icons/icon_check.png");
   }
 };
+
+export const verifyPersonalCopyAccess = personalCopyDetails => {
+  console.log('-*- about to verify file access -*-')
+  const path =
+    Platform.OS == 'android'
+      ? 'file://' + personalCopyDetails.path
+      : personalCopyDetails.path;
+
+  console.log({ path });
+  RNFS.readFile(path)
+    .then((res) => { return true })
+    .catch((err) => {
+      console.log('err.code', err.code);
+      if(err.code=='ENOENT') console.log(' succesfully caught error ')
+      return false
+    });
+    return true
+}
