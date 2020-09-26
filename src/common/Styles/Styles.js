ed336171bd7706be007a7932f98543b8d8addd3b
CypherPoet <CypherPoet@gmail.com>
2020-09-26 00:02:30 -0500

    Initial UI & functionality for New Account Details Screen.
    Connected to https://github.com/bithyve/hexa/issues/1580.

--- src/common/Styles/Styles.js
+++ src/common/Styles/Styles.js
@@ -1,76 +1,86 @@
 import { StyleSheet } from "react-native";
 import Colors from "../Colors";
 import Fonts from "../Fonts";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { widthPercentageToDP } from 'react-native-responsive-screen';
 import { RFValue } from "react-native-responsive-fontsize";
 
 export default StyleSheet.create({
-    headerContainer: {
-        flexDirection: 'row',
-        marginTop: 10,
-        alignItems: 'center',
-        height: 54,
-        backgroundColor: Colors.white,
-        borderBottomColor: Colors.white,
-        borderBottomWidth: 0.5,
-    },
-    headerLeftIconContainer: {
-        height: 54
-    },
-    headerLeftIconInnerContainer:{
-        width: 54,
-        height: 54,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headerTitles:{
-        color: Colors.blue,
-        fontSize: RFValue(25),
-        marginLeft: 20,
-        fontFamily: Fonts.FiraSansRegular
-    },
-    headerTitlesInfoText:{
-        color: Colors.textColorGrey,
-        fontSize: RFValue(12),
-        marginLeft: 20,
-        fontWeight: 'normal',
-        marginRight: 20,
-        marginTop: 5,
-        fontFamily: Fonts.FiraSansRegular
-    },
-    homepageAmountText:{
-        fontFamily: Fonts.FiraSansRegular,
-        fontSize: RFValue(21),
-        marginRight: 5
-    },
-    homepageAmountUnitText:
-    {
-        fontFamily: Fonts.FiraSansRegular,
-        fontSize: RFValue(11),
-        marginBottom: 3
-    },
-    homepageAmountImage:{
-        width: wp('3%'),
-        height: wp('3%'),
-        marginRight: 5,
-        marginBottom: wp('1%'),
-        resizeMode: 'contain'
-    },
-    rootView: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
-    statusBarStyle: {
-        flex: 0,
-    },
-    backIconRootContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    backIconTouchContainer: {
-        height: 30,
-        width: 30,
-        justifyContent: 'center'
-    }
+  headerContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    alignItems: 'center',
+    height: 54,
+    backgroundColor: Colors.white,
+    borderBottomColor: Colors.white,
+    borderBottomWidth: 0.5,
+  },
+
+  headerLeftIconContainer: {
+    height: 54
+  },
+
+  headerLeftIconInnerContainer: {
+    width: 54,
+    height: 54,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  headerTitles: {
+    color: Colors.blue,
+    fontSize: RFValue(25),
+    marginLeft: 20,
+    fontFamily: Fonts.FiraSansRegular
+  },
+
+  headerTitlesInfoText: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(12),
+    marginLeft: 20,
+    fontWeight: 'normal',
+    marginRight: 20,
+    marginTop: 5,
+    fontFamily: Fonts.FiraSansRegular
+  },
+
+  homepageAmountText: {
+    fontFamily: Fonts.FiraSansRegular,
+    fontSize: RFValue(21),
+    marginRight: 5
+  },
+
+  homepageAmountUnitText: {
+    fontFamily: Fonts.FiraSansRegular,
+    fontSize: RFValue(11),
+    marginBottom: 3
+  },
+
+  homepageAmountImage: {
+    width: widthPercentageToDP(3),
+    height: widthPercentageToDP(3),
+    marginRight: 5,
+    marginBottom: widthPercentageToDP(1),
+    resizeMode: 'contain'
+  },
+
+  rootView: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+
+  statusBarStyle: {
+    flex: 0,
+  },
+
+  backIconRootContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  backIconTouchContainer: {
+    height: 30,
+    width: 30,
+    justifyContent: 'center'
+  },
 });
