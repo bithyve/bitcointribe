00658b83b7255deb58474593e421f26cfd709d98
CypherPoet <CypherPoet@gmail.com>
2020-09-26 00:02:30 -0500

    Initial UI & functionality for New Account Details Screen.
    Connected to https://github.com/bithyve/hexa/issues/1580.

--- src/store/index.ts
+++ src/store/index.ts
@@ -61,6 +61,7 @@ import {
   removeTwoFAWatcher,
   setupDonationAccountWatcher,
   updateDonationPreferencesWatcher,
+  addNewAccountWatcher,
   syncViaXpubAgentWatcher,
 } from './sagas/accounts';
 
@@ -171,6 +172,7 @@ const rootSaga = function* () {
     startupSyncWatcher,
     setupDonationAccountWatcher,
     updateDonationPreferencesWatcher,
+    addNewAccountWatcher,
 
     // sss watchers
     initHCWatcher,
