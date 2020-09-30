7ec35acf8dd882b0f56e8b648dab4336b917171c
CypherPoet <CypherPoet@gmail.com>
2020-09-29 19:11:53 -0500

    Refactor `Account`-related data structures.
    New logic to accommodate the fact that some
    accounts and have primary xPubs and secondary xPubs.

--- src/common/data/models/AccountPayload/AccountPayload.ts
+++ src/common/data/models/AccountPayload/AccountPayload.ts
@@ -3,24 +3,63 @@ import BitcoinUnit from '../../enums/BitcoinUnit';
 import ServiceAccountKind from "../../enums/ServiceAccountKind";
 
 
-export interface HexaAccountPayload {
+interface BaseAccountPayload {
+  // Unique Identifier
   uuid: string;
+
   kind: AccountKind;
+
+  // Default displayable title of the account.
   title: string;
+
+  // Default displayable short description of the account.
   shortDescription: string;
+
   accountNumber: number;
+
+  // The order in which this account should be displayed to the user within
+  // a list of accounts.
+  displayOrder: number | null;
+
+  // Balance in Satoshis.
   balance: number;
+
+  // The unit to be used for displaying the account's balance to the user.
   unit: BitcoinUnit;
-  customDisplayName?: string;
-  customDescription?: string;
+
+  // A display name set by the user
+  customDisplayName: string | null;
+
+  // A description set by the user
+  customDescription: string | null;
+
   imageSource: NodeRequire;
 }
 
-export interface ExternalServiceAccountPayload extends HexaAccountPayload {
+
+interface MultiXPubAccountPayload extends BaseAccountPayload {
+  // If an account has several internal XPUBs, accounts related to the non-primary XPUB
+  // can be looked-up by storing their UUIDs here.
+  secondaryAccountUUIDs: string[];
+
+  // Is this account's xPub the primary.
+  isPrimaryAccount: boolean;
+}
+
+
+export interface HexaAccountPayload extends MultiXPubAccountPayload {
+}
+
+
+export interface ExternalServiceAccountPayload extends
+  HexaAccountPayload,
+  MultiXPubAccountPayload
+{
   serviceAccountKind: ServiceAccountKind;
 }
 
-export interface ImportedWalletAccountPayload extends HexaAccountPayload {
+
+export interface ImportedWalletAccountPayload extends BaseAccountPayload {
 }
 
 
