3839781a04f68fa2a4004c72b3e0debd7666dc10
CypherPoet <CypherPoet@gmail.com>
2020-10-05 10:16:11 -0500

    Add `AccountGroup` and `NetworkKind` enums

---
+++ src/common/data/enums/AccountGroup.ts
@@ -0,0 +1,13 @@
+/**
+ * Determines whether or not an account is a compatible destination
+ * for re-mapping another account's transactions.
+ */
+enum AccountGroup {
+  SINGLE_SIG_PUBLIC,
+  SINGLE_SIG_PRIVATE,
+  MULTI_SIG_PUBLIC,
+  MULTI_SIG_PRIVATE,
+  TESTNET,
+}
+
+export default AccountGroup
