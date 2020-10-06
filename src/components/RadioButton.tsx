3b16f71b2ff1cb216ad6de702b862c4cd9735991
CypherPoet <CypherPoet@gmail.com>
2020-10-06 14:34:38 -0500

    AC3: Account Settings -- Reassignment -- Select Transactions Screen
    Connected to https://github.com/bithyve/hexa/issues/1589

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,29 +1,65 @@
-import React, { Component } from 'react';
+import React, { useMemo } from 'react';
 import {
-    View,
-    Image,
-    TouchableOpacity,
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import Colors from "../common/Colors";
-import Fonts from "../common/Fonts";
-import { AppBottomSheetTouchableWrapper } from './AppBottomSheetTouchableWrapper';
+import Colors from '../common/Colors';
 
-export default function RadioButton(props) {
-    if(props.isOnModal){
-        return (<AppBottomSheetTouchableWrapper activeOpacity={10} onPress={() => props.onpress()} style={{ borderColor: props.borderColor, borderWidth: 1, borderRadius: props.size / 2, height: props.size, width: props.size, justifyContent: 'center', alignItems: 'center' }}>
-        {props.isChecked &&
-            <View style={{ backgroundColor: props.color, borderRadius: props.size / 2, height: props.size - 5, width: props.size - 5 }}></View>
-        }
-    </AppBottomSheetTouchableWrapper>
-    )
-    }
-    else{
-    return (<TouchableOpacity activeOpacity={10} onPress={() => props.onpress()} style={{ borderColor: props.borderColor, borderWidth: 1, borderRadius: props.size / 2, height: props.size, width: props.size, justifyContent: 'center', alignItems: 'center' }}>
-        {props.isChecked &&
-            <View style={{ backgroundColor: props.color, borderRadius: props.size / 2, height: props.size - 5, width: props.size - 5 }}></View>
-        }
+export type Props = {
+  isChecked: boolean;
+  size?: number;
+  color? : string;
+  borderColor? : string;
+  onpress?: () => void;
+};
+
+const RadioButton: React.FC<Props> = ({
+  isChecked,
+  size,
+  color,
+  borderColor,
+  onpress,
+}) => {
+  const containerSize = useMemo(() => {
+    return size || 20;
+  }, [size]);
+
+  const containerStyle = useMemo(() => {
+    return {
+      borderColor: borderColor || 'none',
+      borderWidth: 1,
+      borderRadius: containerSize / 2,
+      height: containerSize,
+      width: containerSize,
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  }, [borderColor, containerSize]);
+
+  const innerCircleStyle = useMemo(() => {
+    return {
+      backgroundColor: color || Colors.blue,
+      borderRadius: containerSize / 2,
+      height: containerSize - 5,
+      width: containerSize - 5,
+    };
+  }, [color, containerSize]);
+
+  return (
+    <TouchableOpacity
+      style={containerStyle}
+      activeOpacity={1}
+      onPress={() => {
+        if (onpress) { onpress(); }
+      }}
+    >
+      {isChecked &&
+        <View style={innerCircleStyle} />
+      }
     </TouchableOpacity>
-    )
-    }
+  );
 }
+
+export default RadioButton;
