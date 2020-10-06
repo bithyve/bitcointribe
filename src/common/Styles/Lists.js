75a7d8a21028b4cc257da9f7cc2dae9e897e1271
CypherPoet <CypherPoet@gmail.com>
2020-10-06 08:42:51 -0500

    AC3: Account Settings -- Reassignment -- Options Screen
    Connected to https://github.com/bithyve/hexa/issues/1588.

---
+++ src/common/Styles/Lists.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+import Colors from "../Colors";
+import Fonts from "../Fonts";
+import { RFValue } from "react-native-responsive-fontsize";
+
+
+const ListStyles = StyleSheet.create({
+  listItemTitle: {
+    color: Colors.blue,
+    fontSize: RFValue(14),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+
+  listItemSubtitle: {
+    color: Colors.textColorGrey,
+    fontSize: RFValue(10),
+    fontFamily: Fonts.FiraSansRegular,
+  },
+});
+
+export default ListStyles;
+
+
