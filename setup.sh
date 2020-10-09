336ad9378ff0d74c9c621f689639b861731f1ceb
AliMeer <ali@purple7.com>
2020-10-09 11:52:39 +0100

    updating setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,12 @@ cp mods/react-native-mail/RNMailModule.java node_modules/react-native-mail/andro
 # enabling node core modules
 rn-nodeify --install --hack --yarn
 
+#patch cocoapods
+cp ./rnPatchFiles/RNLocalize.podspec ./node_modules/react-native-localize/RNLocalize.podspec
+cp ./rnPatchFiles/react-native-netinfo.podspec ./node_modules/@react-native-community/netinfo/react-native-netinfo.podspec
+
 # ios dependency installation
-cd ios && pod install
+cd ios && pod deintegrate && rm podfile.lock && pod install
 
 # android SDK location configuration
 cd ../android
@@ -20,7 +24,3 @@ test -f node_modules/react-native/React/Views/RCTWebView.h && rm -f node_modules
 test -f node_modules/react-native/React/Views/RCTWebView.m && rm -f node_modules/react-native/React/Views/RCTWebView.m
 test -f node_modules/react-native/React/Views/RCTWebViewManager.h && rm -f node_modules/react-native/React/Views/RCTWebViewManager.h
 test -f node_modules/react-native/React/Views/RCTWebViewManager.m && rm -f node_modules/react-native/React/Views/RCTWebViewManager.m
-
-#patch cocoapods
-cp ./rnPatchFiles/RNLocalize.podspec ./node_modules/react-native-localize/RNLocalize.podspec
-cp ./rnPatchFiles/react-native-netinfo.podspec ./node_modules/@react-native-community/netinfo/react-native-netinfo.podspec
