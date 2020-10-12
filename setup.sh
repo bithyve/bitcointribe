1db33fa7f930e7942badba448bca5c0016a542ea
Muneer PP <abdulmuneer22@gmail.com>
2020-10-12 17:35:42 +0530

    updated setup

--- setup.sh
+++ setup.sh
@@ -6,21 +6,20 @@ cp mods/react-native-mail/RNMailModule.java node_modules/react-native-mail/andro
 # enabling node core modules
 rn-nodeify --install --hack --yarn
 
-#patch cocoapods
+echo "patch cocoapods"
 cp ./rnPatchFiles/RNLocalize.podspec ./node_modules/react-native-localize/RNLocalize.podspec
 cp ./rnPatchFiles/react-native-netinfo.podspec ./node_modules/@react-native-community/netinfo/react-native-netinfo.podspec
 
 # ios dependency installation
-cd ios && pod deintegrate && rm podfile.lock && pod install
+cd ios && pod deintegrate && pod install
 
 # android SDK location configuration
-cd ../android
-touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" >local.properties
+cd ../android && touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" >local.properties
 
 # Deleting UIWebView related files from node_modules. Which causes IPA rejection.
 # echo "Deleting UIWebView related files from node_modules"
-cd ../
-test -f node_modules/react-native/React/Views/RCTWebView.h && rm -f node_modules/react-native/React/Views/RCTWebView.h
-test -f node_modules/react-native/React/Views/RCTWebView.m && rm -f node_modules/react-native/React/Views/RCTWebView.m
-test -f node_modules/react-native/React/Views/RCTWebViewManager.h && rm -f node_modules/react-native/React/Views/RCTWebViewManager.h
-test -f node_modules/react-native/React/Views/RCTWebViewManager.m && rm -f node_modules/react-native/React/Views/RCTWebViewManager.m
+# cd ../
+# test -f node_modules/react-native/React/Views/RCTWebView.h && rm -f node_modules/react-native/React/Views/RCTWebView.h
+# test -f node_modules/react-native/React/Views/RCTWebView.m && rm -f node_modules/react-native/React/Views/RCTWebView.m
+# test -f node_modules/react-native/React/Views/RCTWebViewManager.h && rm -f node_modules/react-native/React/Views/RCTWebViewManager.h
+# test -f node_modules/react-native/React/Views/RCTWebViewManager.m && rm -f node_modules/react-native/React/Views/RCTWebViewManager.m
