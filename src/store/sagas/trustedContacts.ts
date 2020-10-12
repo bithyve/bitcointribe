0567c04765af98a3f11ff0fab7af46ccd5885e48
Parsh <parsh.cosmos11@gmail.com>
2020-10-12 15:40:15 +0530

    syncronised share download

--- src/store/sagas/trustedContacts.ts
+++ src/store/sagas/trustedContacts.ts
@@ -47,6 +47,7 @@ import TestAccount from '../../bitcoin/services/accounts/TestAccount';
 import RelayServices from '../../bitcoin/services/RelayService';
 import SSS from '../../bitcoin/utilities/sss/SSS';
 import Toast from '../../components/Toast';
+import { downloadMetaShareWorker } from './sss';
 
 const sendNotification = (trustedContacts, contactName, walletName) => {
   const receivers = [];
@@ -342,8 +343,8 @@ function* updateEphemeralChannelWorker({ payload }) {
     if (data && data.shareTransferDetails) {
       const { otp, encryptedKey } = data.shareTransferDetails;
       // yield delay(1000); // introducing delay in order to evade database insertion collision
-      yield put(downloadMShare(encryptedKey, otp));
-      yield delay(1000); // allowing share-download saga to finish execution
+      yield call(downloadMetaShareWorker, { payload: { encryptedKey, otp } });
+      Toast('You have been successfully added as a Keeper');
       yield put(trustedContactApproved(contactInfo.contactName, true));
     } else if(payload.uploadXpub) {
       Toast('Contact successfully added to Friends and Family'); 
