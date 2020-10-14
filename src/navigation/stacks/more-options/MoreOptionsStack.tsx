89e87319ef64cd8063598854cc2493bc5b9ef9d4
CypherPoet <CypherPoet@gmail.com>
2020-10-13 21:10:46 -0500

    Begin modularizing navigation stacks.
    These are some minor code architecture changes that set the stage for some of the navigation refactoring I plan to do in https://github.com/bithyve/hexa/issues/1831,  https://github.com/bithyve/hexa/issues/1832, https://github.com/bithyve/hexa/issues/1833, and other stories that will involve creating new routable screens.

--- src/navigation/stacks/more-options/MoreOptionsStack.tsx
+++ src/navigation/stacks/more-options/MoreOptionsStack.tsx
@@ -1,59 +1,17 @@
 import React from 'react';
 import { createStackNavigator } from "react-navigation-stack";
-import ChangeCurrencyScreen from '../../../pages/ChangeCurrencyScreen';
 import MoreOptionsContainerScreen from "../../../pages/MoreOptions/MoreOptionsContainerScreen";
-import ManagePasscodeScreen from '../../../pages/ManagePasscodeScreen';
 import FriendsAndFamilyScreen from '../../../pages/FriendsAndFamily/FriendsAndFamilyScreen';
-import WalletSettingsContainerScreen from "../../../pages/MoreOptions/WalletSettings/WalletSettingsContainerScreen";
 import FundingSourcesScreen from "../../../pages/FundingSources/FundingSourcesContainerScreen";
 import FundingSourceDetailsScreen from "../../../pages/FundingSources/FundingSourceDetailsScreen";
-import CommonStyles from '../../../common/Styles/Styles';
 import SmallNavHeaderCloseButton from '../../../components/navigation/SmallNavHeaderCloseButton';
 import { goHomeAction } from '../../actions/NavigationActions';
 import SmallNavHeaderBackButton from '../../../components/navigation/SmallNavHeaderBackButton';
+import NavStyles from '../../../common/Styles/NavStyles';
+import WalletSettingsStack from './WalletSettingsStack';
 
 
-export const WalletSettingsStack = createStackNavigator(
-  {
-    WalletSettingsRoot: {
-      screen: WalletSettingsContainerScreen,
-      navigationOptions: {
-        title: "Wallet Settings",
-      },
-    },
-    ManagePasscode: {
-      screen: ManagePasscodeScreen,
-      navigationOptions: {
-        title: "Manage Passcode",
-      },
-    },
-    ChangeCurrency: {
-      screen: ChangeCurrencyScreen,
-      navigationOptions: {
-        title: "Change Currency",
-      },
-    },
-  },
-  {
-    defaultNavigationOptions: ({ navigation }) => {
-      return {
-        headerTitleStyle: CommonStyles.modalHeaderTitleText,
-        headerLeft: () => {
-          return (
-            <SmallNavHeaderCloseButton
-              onPress={() => { navigation.pop() }}
-              containerStyle={{ paddingLeft: 16 }}
-            />
-          );
-        },
-      };
-    },
-    mode: 'modal',
-  },
-);
-
-
-export const MoreOptionsStack = createStackNavigator(
+const MoreOptionsStack = createStackNavigator(
   {
     MoreOptionsRoot: {
       screen: MoreOptionsContainerScreen,
@@ -61,12 +19,7 @@ export const MoreOptionsStack = createStackNavigator(
         return {
           title: "More",
           headerLeft: () => {
-            return (
-              <SmallNavHeaderBackButton
-                onPress={() => { navigation.dispatch(goHomeAction) }}
-                containerStyle={{ paddingLeft: 16 }}
-              />
-            );
+            return <SmallNavHeaderBackButton onPress={() => { navigation.dispatch(goHomeAction) }} />;
           },
         };
       },
@@ -98,17 +51,14 @@ export const MoreOptionsStack = createStackNavigator(
     initialRouteName: 'MoreOptionsRoot',
     defaultNavigationOptions: ({ navigation }) => {
       return {
-        headerTitleStyle: CommonStyles.modalHeaderTitleText,
+        headerTitleStyle: NavStyles.modalHeaderTitleText,
         headerLeft: () => {
-          return (
-            <SmallNavHeaderCloseButton
-              onPress={() => { navigation.pop() }}
-              containerStyle={{ paddingLeft: 16 }}
-            />
-          );
+          return <SmallNavHeaderCloseButton onPress={() => { navigation.pop() }} />;
         },
       };
     },
   },
 );
 
+
+export default MoreOptionsStack;
