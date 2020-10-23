3c0cdaafd1a24939f61844ff778ee7f530ea1d5d
CypherPoet <CypherPoet@gmail.com>
2020-10-22 22:10:29 -0500

    fix internet warning presentation typo

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -7,6 +7,7 @@ import {
   Alert,
   AsyncStorage,
   Platform,
+  AppState,
 } from 'react-native';
 import Video from 'react-native-video';
 import Colors from '../common/Colors';
@@ -93,21 +94,27 @@ class Launch extends Component<HomePropsTypes, HomeStateTypes> {
       console.log("Launch::handleDeepLinking::initialURL: " + url);
 
       setTimeout(async () => {
-        if (await AsyncStorage.getItem('hasCreds'))
-          if (!url) this.props.navigation.replace('Login');
-          else {
+        if (await AsyncStorage.getItem('hasCreds')) {
+          if (!url) {
+            this.props.navigation.replace('Login');
+          } else {
             const splits = url.split('/');
+
             if (splits[5] === 'sss') {
               const requester = splits[4];
+
               if (splits[6] === 'ek') {
                 const custodyRequest = {
                   requester,
                   ek: splits[7],
                   uploadedAt: splits[8],
                 };
+
                 this.props.navigation.replace('Login', { custodyRequest });
+
               } else if (splits[6] === 'rk') {
                 const recoveryRequest = { requester, rk: splits[7] };
+
                 this.props.navigation.replace('Login', { recoveryRequest });
               }
             } else if (['tc', 'tcg', 'atcg', 'ptc'].includes(splits[4])) {
@@ -162,15 +169,19 @@ class Launch extends Component<HomePropsTypes, HomeStateTypes> {
               );
             } else if (url.includes('fastbitcoins')) {
               const userKey = url.substr(url.lastIndexOf('/') + 1);
+
               this.props.navigation.navigate('Login', { userKey });
             } else {
               const EmailToken = url.substr(url.lastIndexOf('/') + 1);
-              // console.log('EmailToken', EmailToken);
+
               this.props.navigation.navigate('SignUpDetails', { EmailToken });
             }
           }
-        else this.props.navigation.replace('PasscodeConfirm');
+        } else {
+          this.props.navigation.replace('PasscodeConfirm');
+        }
       }, 4000);
+
     } catch (err) {
       (this.errorBottomSheet as any).current.snapTo(1);
     }
