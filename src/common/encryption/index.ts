e47bc2ef666d1ce0d13a7447ea267ce664d89348
Muneer PP <abdulmuneer22@gmail.com>
2020-10-23 15:47:03 +0530

    replaced encryption file

--- src/common/encryption/index.ts
+++ src/common/encryption/index.ts
@@ -1,5 +1,5 @@
 import cryptoJS from "crypto-js";
-import 'react-native-get-random-values';
+import { getRandomBytesAsync } from "expo-random";
 
 export const encrypt = (data, key) => {
   const ciphertext = cryptoJS.AES.encrypt(JSON.stringify(data), key);
@@ -15,6 +15,6 @@ export const decrypt = (ciphertext, key) => {
 export const hash = data => cryptoJS.SHA512(data).toString();
 
 export const generateKey = async () => {
-  const randomBytes = await crypto.getRandomValues(new Uint8Array(16));
+  const randomBytes = await getRandomBytesAsync(16);
   return hash(randomBytes.toString());
 };
