57dcfdd1cb9ecba46088d54c9dc81e3c593b05e3
cakesoft-shivani <shivani@cakesofttech.com>
2020-10-28 11:35:43 +0530

    disabled contact option

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,29 +1,63 @@
-import React, { Component } from 'react';
-import {
-    View,
-    Image,
-    TouchableOpacity,
-} from 'react-native';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import Colors from "../common/Colors";
-import Fonts from "../common/Fonts";
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import { AppBottomSheetTouchableWrapper } from './AppBottomSheetTouchableWrapper';
 
 export default function RadioButton(props) {
-    if(props.isOnModal){
-        return (<AppBottomSheetTouchableWrapper activeOpacity={10} onPress={() => props.onpress()} style={{ borderColor: props.borderColor, borderWidth: 1, borderRadius: props.size / 2, height: props.size, width: props.size, justifyContent: 'center', alignItems: 'center' }}>
-        {props.isChecked &&
-            <View style={{ backgroundColor: props.color, borderRadius: props.size / 2, height: props.size - 5, width: props.size - 5 }}></View>
-        }
-    </AppBottomSheetTouchableWrapper>
-    )
-    }
-    else{
-    return (<TouchableOpacity activeOpacity={10} onPress={() => props.onpress()} style={{ borderColor: props.borderColor, borderWidth: 1, borderRadius: props.size / 2, height: props.size, width: props.size, justifyContent: 'center', alignItems: 'center' }}>
-        {props.isChecked &&
-            <View style={{ backgroundColor: props.color, borderRadius: props.size / 2, height: props.size - 5, width: props.size - 5 }}></View>
-        }
-    </TouchableOpacity>
-    )
-    }
+  if (props.isOnModal) {
+    return (
+      <AppBottomSheetTouchableWrapper
+        disabled={props.disabled ? props.disabled : false}
+        activeOpacity={10}
+        onPress={() => props.onpress()}
+        style={{
+          borderColor: props.borderColor,
+          borderWidth: 1,
+          borderRadius: props.size / 2,
+          height: props.size,
+          width: props.size,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {props.isChecked && (
+          <View
+            style={{
+              backgroundColor: props.color,
+              borderRadius: props.size / 2,
+              height: props.size - 5,
+              width: props.size - 5,
+            }}
+          ></View>
+        )}
+      </AppBottomSheetTouchableWrapper>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        disabled={props.disabled ? props.disabled : false}
+        activeOpacity={10}
+        onPress={() => props.onpress()}
+        style={{
+          borderColor: props.borderColor,
+          borderWidth: 1,
+          borderRadius: props.size / 2,
+          height: props.size,
+          width: props.size,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {props.isChecked && (
+          <View
+            style={{
+              backgroundColor: props.color,
+              borderRadius: props.size / 2,
+              height: props.size - 5,
+              width: props.size - 5,
+            }}
+          ></View>
+        )}
+      </TouchableOpacity>
+    );
+  }
 }
