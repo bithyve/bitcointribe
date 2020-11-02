b2573f50fec8843f67dd3a9d7ae2da8d79dfd8fe
AliMeer <ali@purple7.com>
2020-11-02 20:20:10 +0000

    use correct message to request permission

--- src/pages/Recovery/ContactList.tsx
+++ src/pages/Recovery/ContactList.tsx
@@ -37,9 +37,10 @@ async function requestContactsPermission() {
     await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
       {
-        'title': 'Contacts',
+        'title': 'Hexa Would Like to Access Your Contacts',
         'message': 'Address book details like name and image are used for you to better recognize your Hexa Friends and Family contacts. Don’t worry, they don’t get stored anywhere else and are for your eyes only.',
-        'buttonPositive': 'Accept'
+        'buttonPositive': 'Allow',
+        'buttonNegative': 'Deny'
       }
     );
     return PermissionsAndroid.RESULTS.GRANTED;
