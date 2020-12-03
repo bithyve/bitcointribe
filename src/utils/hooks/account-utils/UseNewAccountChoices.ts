8c665e3babc5fa151e7c55dabb0b51189d178f64
CypherPoet <CypherPoet@gmail.com>
2020-12-03 14:33:05 -0600

    Add New Account: Logic for dynamic default names.
    Connected to https://github.com/bithyve/hexa/issues/2185

--- src/utils/hooks/account-utils/UseNewAccountChoices.ts
+++ src/utils/hooks/account-utils/UseNewAccountChoices.ts
@@ -9,19 +9,61 @@ import TrustedContactsSubAccountInfo from "../../../common/data/models/SubAccoun
 import FullyImportedWalletSubAccountInfo from "../../../common/data/models/SubAccountInfo/ImportedWalletSubAccounts/FullyImportedWalletSubAccountInfo";
 import WatchOnlyImportedWalletSubAccountInfo from "../../../common/data/models/SubAccountInfo/ImportedWalletSubAccounts/WatchOnlyImportedWalletSubAccountInfo";
 import SubAccountDescribing from "../../../common/data/models/SubAccountInfo/Interfaces";
+import useActiveAccountShells from "../state-selectors/accounts/UseActiveAccountShells";
+import TestAccount from "../../../bitcoin/services/accounts/TestAccount";
+import SubAccountKind from "../../../common/data/enums/SubAccountKind";
 
-export default function useNewAccountChoices(): Record<string, SubAccountDescribing[]> {
-  return useMemo(() => {
+type Choices = {
+  hexaAccounts: SubAccountDescribing[];
+  serviceAccounts: SubAccountDescribing[];
+  importedWalletAccounts: SubAccountDescribing[];
+};
+
+export default function useNewAccountChoices() {
+  const accountShells = useActiveAccountShells();
+
+  let testAccountCount = 0;
+  let checkingAccountCount = 0;
+  let savingsAccountCount = 0;
+  let donationAccountCount = 0;
+
+  accountShells.forEach(shell => {
+    switch (shell.primarySubAccount.kind) {
+      case SubAccountKind.TEST_ACCOUNT:
+        testAccountCount += 1;
+        break;
+      case SubAccountKind.REGULAR_ACCOUNT:
+        checkingAccountCount += 1;
+        break;
+      case SubAccountKind.SECURE_ACCOUNT:
+        savingsAccountCount += 1;
+        break;
+      case SubAccountKind.DONATION_ACCOUNT:
+        donationAccountCount += 1;
+        break;
+      default:
+        break;
+    }
+  })
+
+  return useMemo<Choices>(() => {
     return {
       hexaAccounts: [
-        new TestSubAccountInfo({}),
-        new SavingsSubAccountInfo({}),
-        new CheckingSubAccountInfo({}),
+        new TestSubAccountInfo({
+          defaultTitle: `Test Account${testAccountCount > 0 ? ` ${testAccountCount + 1}`: ''}`,
+        }),
+        new SavingsSubAccountInfo({
+          defaultTitle: `Savings Account${savingsAccountCount > 0 ? ` ${savingsAccountCount + 1}`: ''}`,
+        }),
+        new CheckingSubAccountInfo({
+          defaultTitle: `Checking Account${checkingAccountCount > 0 ? ` ${checkingAccountCount + 1}`: ''}`,
+        }),
         new TrustedContactsSubAccountInfo({}),
 
         new DonationSubAccountInfo({
-          doneeName: 'Sample Donee',
-          causeName: 'Bitcoin Development Fund',
+          defaultTitle: `Donation Account${donationAccountCount > 0 ? ` ${donationAccountCount + 1}` : ''}`,
+          doneeName: '',
+          causeName: '',
         }),
       ],
 
