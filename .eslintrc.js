c2ff6aa01cd0af27e1e4c3d821b1faae3ab78f3e
AliMeer <ali@purple7.com>
2020-12-10 18:37:07 +0000

    added rules for spacing in brackets and braces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [ 'react', '@typescript-eslint' ],
+  rules: {
+    'space-in-brackets': [ 'error', 'always', {
+      'singleValue': true,
+      'objectsInArrays': true,
+      'arraysInArrays': true,
+      'arraysInObjects': true,
+      'objectsInObjects': true,
+      'propertyName': true
+    } ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'space-in-parens': [ 'error', 'always' ],
+    'quotes': [ 'error', 'single' ]
+  }
+}
