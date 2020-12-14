93029019750ee4350ad7c41946687e3f7d61e82b
CypherPoet <CypherPoet@gmail.com>
2020-12-13 18:49:26 -0600

    Use flex sizing instead of margin for account details layout

--- src/pages/Accounts/Details/AccountDetailsContainerScreen.tsx
+++ src/pages/Accounts/Details/AccountDetailsContainerScreen.tsx
@@ -1,111 +1,113 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
-import { View, Text, StyleSheet, Alert, RefreshControl, ScrollView } from 'react-native';
-import { useDispatch } from 'react-redux';
-import NavHeader from '../../../components/account-details/AccountDetailsNavHeader';
-import AccountDetailsCard from '../../../components/account-details/AccountDetailsCard';
-import TransactionsList from '../../../components/account-details/AccountDetailsTransactionsList';
-import SendAndReceiveButtonsFooter from './SendAndReceiveButtonsFooter';
-import { useBottomSheetModal } from '@gorhom/bottom-sheet';
+import React, { useCallback, useEffect, useMemo } from 'react'
+import { View, Text, StyleSheet, Alert, RefreshControl, ScrollView } from 'react-native'
+import { useDispatch } from 'react-redux'
+import NavHeader from '../../../components/account-details/AccountDetailsNavHeader'
+import AccountDetailsCard from '../../../components/account-details/AccountDetailsCard'
+import TransactionsList from '../../../components/account-details/AccountDetailsTransactionsList'
+import SendAndReceiveButtonsFooter from './SendAndReceiveButtonsFooter'
+import { useBottomSheetModal } from '@gorhom/bottom-sheet'
 import KnowMoreBottomSheet, {
   KnowMoreBottomSheetHandle,
-} from '../../../components/account-details/AccountDetailsKnowMoreBottomSheet';
-import TransactionDescribing from '../../../common/data/models/Transactions/Interfaces';
-import useAccountShellFromNavigation from '../../../utils/hooks/state-selectors/accounts/UseAccountShellFromNavigation';
-import usePrimarySubAccountForShell from '../../../utils/hooks/account-utils/UsePrimarySubAccountForShell';
-import useTransactionReassignmentCompletedEffect from '../../../utils/hooks/account-effects/UseTransactionReassignmentCompletedEffect';
-import TransactionReassignmentSuccessBottomSheet from '../../../components/bottom-sheets/account-management/TransactionReassignmentSuccessBottomSheet';
-import { resetStackToAccountDetails } from '../../../navigation/actions/NavigationActions';
-import useAccountShellMergeCompletionEffect from '../../../utils/hooks/account-effects/UseAccountShellMergeCompletionEffect';
-import AccountShellMergeSuccessBottomSheet from '../../../components/bottom-sheets/account-management/AccountShellMergeSuccessBottomSheet';
-import AccountShell from '../../../common/data/models/AccountShell';
-import defaultBottomSheetConfigs from '../../../common/configs/BottomSheetConfigs';
-import { NavigationScreenConfig } from 'react-navigation';
-import { NavigationStackOptions } from 'react-navigation-stack';
+} from '../../../components/account-details/AccountDetailsKnowMoreBottomSheet'
+import TransactionDescribing from '../../../common/data/models/Transactions/Interfaces'
+import useAccountShellFromNavigation from '../../../utils/hooks/state-selectors/accounts/UseAccountShellFromNavigation'
+import usePrimarySubAccountForShell from '../../../utils/hooks/account-utils/UsePrimarySubAccountForShell'
+import useTransactionReassignmentCompletedEffect from '../../../utils/hooks/account-effects/UseTransactionReassignmentCompletedEffect'
+import TransactionReassignmentSuccessBottomSheet from '../../../components/bottom-sheets/account-management/TransactionReassignmentSuccessBottomSheet'
+import { resetStackToAccountDetails } from '../../../navigation/actions/NavigationActions'
+import useAccountShellMergeCompletionEffect from '../../../utils/hooks/account-effects/UseAccountShellMergeCompletionEffect'
+import AccountShellMergeSuccessBottomSheet from '../../../components/bottom-sheets/account-management/AccountShellMergeSuccessBottomSheet'
+import AccountShell from '../../../common/data/models/AccountShell'
+import defaultBottomSheetConfigs from '../../../common/configs/BottomSheetConfigs'
+import { NavigationScreenConfig } from 'react-navigation'
+import { NavigationStackOptions } from 'react-navigation-stack'
 
-import { refreshAccountShell } from '../../../store/actions/accounts';
-import SourceAccountKind from '../../../common/data/enums/SourceAccountKind';
-import NetworkKind from '../../../common/data/enums/NetworkKind';
-import config from '../../../bitcoin/HexaConfig';
-import { DerivativeAccountTypes } from '../../../bitcoin/utilities/Interface';
-import SubAccountKind from '../../../common/data/enums/SubAccountKind';
-import useAccountsState from '../../../utils/hooks/state-selectors/accounts/UseAccountsState';
-import useTransactionsForAccountShell from '../../../utils/hooks/state-selectors/accounts/UseTransactionsForAccountShell';
-import TransactionPreviewHeader from './TransactionPreviewHeader';
-import useSpendableBalanceForAccountShell from '../../../utils/hooks/account-utils/UseSpendableBalanceForAccountShell';
+import { refreshAccountShell } from '../../../store/actions/accounts'
+import SourceAccountKind from '../../../common/data/enums/SourceAccountKind'
+import NetworkKind from '../../../common/data/enums/NetworkKind'
+import config from '../../../bitcoin/HexaConfig'
+import { DerivativeAccountTypes } from '../../../bitcoin/utilities/Interface'
+import SubAccountKind from '../../../common/data/enums/SubAccountKind'
+import useAccountsState from '../../../utils/hooks/state-selectors/accounts/UseAccountsState'
+import useTransactionsForAccountShell from '../../../utils/hooks/state-selectors/accounts/UseTransactionsForAccountShell'
+import TransactionPreviewHeader from './TransactionPreviewHeader'
+import useSpendableBalanceForAccountShell from '../../../utils/hooks/account-utils/UseSpendableBalanceForAccountShell'
 
 export type Props = {
   navigation: any;
 };
 
-const AccountDetailsContainerScreen: React.FC<Props> = ({ navigation }) => {
-  const dispatch = useDispatch();
+const AccountDetailsContainerScreen: React.FC<Props> = ( { navigation } ) => {
+  const dispatch = useDispatch()
 
-  const accountShellID = useMemo(() => {
-    return navigation.getParam('accountShellID');
-  }, [navigation]);
+  const accountShellID = useMemo( () => {
+    return navigation.getParam( 'accountShellID' )
+  }, [ navigation ] )
 
-  const accountShell = useAccountShellFromNavigation(navigation);
-  const accountsState = useAccountsState();
-  const primarySubAccount = usePrimarySubAccountForShell(accountShell);
-  const accountTransactions = AccountShell.getAllTransactions(accountShell);
-  const spendableBalance = useSpendableBalanceForAccountShell(accountShell)
-  const averageTxFees = accountsState.averageTxFees;
+  const accountShell = useAccountShellFromNavigation( navigation )
+  const accountsState = useAccountsState()
+  const primarySubAccount = usePrimarySubAccountForShell( accountShell )
+  const accountTransactions = AccountShell.getAllTransactions( accountShell )
+  const spendableBalance = useSpendableBalanceForAccountShell( accountShell )
+  const averageTxFees = accountsState.averageTxFees
 
-  let derivativeAccountKind: any = primarySubAccount.kind;
+  let derivativeAccountKind: any = primarySubAccount.kind
 
   if (
     primarySubAccount.kind === SubAccountKind.REGULAR_ACCOUNT ||
     primarySubAccount.kind === SubAccountKind.SECURE_ACCOUNT
   ) {
-    if (primarySubAccount.instanceNumber) {
-      derivativeAccountKind = DerivativeAccountTypes.SUB_PRIMARY_ACCOUNT;
+    if ( primarySubAccount.instanceNumber ) {
+      derivativeAccountKind = DerivativeAccountTypes.SUB_PRIMARY_ACCOUNT
     }
   }
 
   const derivativeAccountDetails: {
     type: string;
     number: number;
-  } = config.EJECTED_ACCOUNTS.includes(derivativeAccountKind) ?
+  } = config.EJECTED_ACCOUNTS.includes( derivativeAccountKind ) ?
     {
       type: derivativeAccountKind,
       number: primarySubAccount.instanceNumber,
     }
-    : null;
+    : null
 
 
-  const isRefreshing = useMemo(() => {
-    return accountShell.isSyncInProgress;
-  }, [accountShell.isSyncInProgress]);
+  const isRefreshing = useMemo( () => {
+    return accountShell.isSyncInProgress
+  }, [ accountShell.isSyncInProgress ] )
 
   const {
     present: presentBottomSheet,
     dismiss: dismissBottomSheet,
-  } = useBottomSheetModal();
+  } = useBottomSheetModal()
 
-  function handleTransactionSelection(transaction: TransactionDescribing) {
-    navigation.navigate('TransactionDetails', {
+  function handleTransactionSelection( transaction: TransactionDescribing ) {
+    navigation.navigate( 'TransactionDetails', {
       transaction,
       accountShellID: accountShell.id,
-    });
+    } )
   }
 
   function navigateToTransactionsList() {
-    navigation.navigate('TransactionsList', {
+    navigation.navigate( 'TransactionsList', {
       accountShellID,
-    });
+    } )
   }
 
   function navigateToAccountSettings() {
-    navigation.navigate('SubAccountSettings', {
+    navigation.navigate( 'SubAccountSettings', {
       accountShellID,
-    });
+    } )
   }
 
   function performRefreshOnPullDown() {
-    dispatch(refreshAccountShell(accountShell, { autoSync: false }));
+    dispatch( refreshAccountShell( accountShell, {
+      autoSync: false
+    } ) )
   }
 
-  const showKnowMoreSheet = useCallback(() => {
+  const showKnowMoreSheet = useCallback( () => {
     presentBottomSheet(
       <KnowMoreBottomSheet
         accountKind={primarySubAccount.kind}
@@ -113,176 +115,184 @@ const AccountDetailsContainerScreen: React.FC<Props> = ({ navigation }) => {
       />,
       {
         ...defaultBottomSheetConfigs,
-        snapPoints: [0, '95%'],
+        snapPoints: [ 0, '95%' ],
         handleComponent: KnowMoreBottomSheetHandle,
       },
-    );
-  }, [presentBottomSheet, dismissBottomSheet]);
+    )
+  }, [ presentBottomSheet, dismissBottomSheet ] )
 
   const showReassignmentConfirmationBottomSheet = useCallback(
-    (destinationID) => {
+    ( destinationID ) => {
       presentBottomSheet(
         <TransactionReassignmentSuccessBottomSheet
           onViewAccountDetailsPressed={() => {
-            dismissBottomSheet();
+            dismissBottomSheet()
             navigation.dispatch(
-              resetStackToAccountDetails({
+              resetStackToAccountDetails( {
                 accountShellID: destinationID,
-              }),
-            );
+              } ),
+            )
           }}
         />,
         {
           ...defaultBottomSheetConfigs,
-          snapPoints: [0, '40%'],
+          snapPoints: [ 0, '40%' ],
         },
-      );
+      )
     },
-    [presentBottomSheet, dismissBottomSheet],
-  );
+    [ presentBottomSheet, dismissBottomSheet ],
+  )
 
   const showMergeConfirmationBottomSheet = useCallback(
-    ({ source, destination }) => {
+    ( { source, destination } ) => {
       presentBottomSheet(
         <AccountShellMergeSuccessBottomSheet
           sourceAccountShell={source}
           destinationAccountShell={destination}
           onViewAccountDetailsPressed={() => {
-            dismissBottomSheet();
+            dismissBottomSheet()
             navigation.dispatch(
-              resetStackToAccountDetails({
+              resetStackToAccountDetails( {
                 accountShellID: destination.id,
-              }),
-            );
+              } ),
+            )
           }}
         />,
         {
           ...defaultBottomSheetConfigs,
-          snapPoints: [0, '67%'],
+          snapPoints: [ 0, '67%' ],
         },
-      );
+      )
     },
-    [presentBottomSheet, dismissBottomSheet],
-  );
+    [ presentBottomSheet, dismissBottomSheet ],
+  )
 
-  useTransactionReassignmentCompletedEffect({
+  useTransactionReassignmentCompletedEffect( {
     onSuccess: showReassignmentConfirmationBottomSheet,
     onError: () => {
       Alert.alert(
         'Transaction Reassignment Error',
         'An error occurred while attempting to reassign transactions',
-      );
+      )
     },
-  });
+  } )
 
-  useAccountShellMergeCompletionEffect({
+  useAccountShellMergeCompletionEffect( {
     onSuccess: showMergeConfirmationBottomSheet,
     onError: () => {
       Alert.alert(
         'Account Merge Error',
         'An error occurred while attempting to merge accounts.',
-      );
+      )
     },
-  });
+  } )
 
-  useEffect(() => {
-    dispatch(refreshAccountShell(accountShell, { autoSync: true }));
-  }, []);
+  useEffect( () => {
+    dispatch( refreshAccountShell( accountShell, {
+      autoSync: true
+    } ) )
+  }, [] )
 
   return (
-    <View style={{ paddingVertical: 20 }}>
-    <ScrollView
-      style={styles.rootContainer}
-      refreshControl={
-        <RefreshControl
-          onRefresh={performRefreshOnPullDown}
-          refreshing={isRefreshing}
-        />
-      }
-    >
+    <View style={styles.rootContainer}>
+      <ScrollView
+        style={styles.scrollViewContainer}
+        refreshControl={
+          <RefreshControl
+            onRefresh={performRefreshOnPullDown}
+            refreshing={isRefreshing}
+          />
+        }
+      >
         <AccountDetailsCard
           accountShell={accountShell}
           onKnowMorePressed={showKnowMoreSheet}
           onSettingsPressed={navigateToAccountSettings}
         />
 
-      <View style={{ paddingVertical: 20 }}>
-        <TransactionPreviewHeader
-          availableBalance={spendableBalance}
-          bitcoinUnit={accountShell.unit}
-          isTestAccount={primarySubAccount.kind === SubAccountKind.TEST_ACCOUNT}
-          onViewMorePressed={navigateToTransactionsList}
-        />
-      </View>
+        <View style={{
+          paddingVertical: 20
+        }}>
+          <TransactionPreviewHeader
+            availableBalance={spendableBalance}
+            bitcoinUnit={accountShell.unit}
+            isTestAccount={primarySubAccount.kind === SubAccountKind.TEST_ACCOUNT}
+            onViewMorePressed={navigateToTransactionsList}
+          />
+        </View>
 
-      {/* <TransactionPreviewTabs  */}
+        {/* <TransactionPreviewTabs  */}
 
-      {/* /> */}
+        {/* /> */}
 
-      <View style={{ marginBottom: 20 }}>
-        <TransactionsList
-          transactions={accountTransactions.slice(0, 3)}
-          onTransactionSelected={handleTransactionSelection}
-        />
-      </View>
+        <View style={{
+          marginBottom: 20
+        }}>
+          <TransactionsList
+            transactions={accountTransactions.slice( 0, 3 )}
+            onTransactionSelected={handleTransactionSelection}
+          />
+        </View>
 
-      <View style={styles.footerSection}>
-        <SendAndReceiveButtonsFooter
-          onSendPressed={() => {
-            navigation.navigate('Send', {
-              serviceType: primarySubAccount.sourceKind,
-              averageTxFees,
-              spendableBalance: AccountShell.getSpendableBalance(accountShell),
-              derivativeAccountDetails,
-              accountShellID,
-            });
-          }}
-          onReceivePressed={() => {
-            navigation.navigate('Receive', {
-              serviceType: primarySubAccount.sourceKind,
-              derivativeAccountDetails,
-            });
-          }}
-          averageTxFees={averageTxFees}
-          network={
-            config.APP_STAGE === 'dev' ||
+        <View style={styles.footerSection}>
+          <SendAndReceiveButtonsFooter
+            onSendPressed={() => {
+              navigation.navigate( 'Send', {
+                serviceType: primarySubAccount.sourceKind,
+                averageTxFees,
+                spendableBalance: AccountShell.getSpendableBalance( accountShell ),
+                derivativeAccountDetails,
+                accountShellID,
+              } )
+            }}
+            onReceivePressed={() => {
+              navigation.navigate( 'Receive', {
+                serviceType: primarySubAccount.sourceKind,
+                derivativeAccountDetails,
+              } )
+            }}
+            averageTxFees={averageTxFees}
+            network={
+              config.APP_STAGE === 'dev' ||
             primarySubAccount.sourceKind === SourceAccountKind.TEST_ACCOUNT
-              ? NetworkKind.TESTNET
-              : NetworkKind.MAINNET
-          }
-        />
-      </View>
-    </ScrollView>
+                ? NetworkKind.TESTNET
+                : NetworkKind.MAINNET
+            }
+          />
+        </View>
+      </ScrollView>
     </View>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   rootContainer: {
+    paddingTop: 20,
+    flex: 1,
+  },
+
+  scrollViewContainer: {
     paddingHorizontal: 24,
   },
 
   footerSection: {
     paddingVertical: 38,
-    marginBottom: 100
   },
-});
+} )
 
-AccountDetailsContainerScreen.navigationOptions = ({
-  navigation,
-}): NavigationScreenConfig<NavigationStackOptions, any> => {
+AccountDetailsContainerScreen.navigationOptions = ( { navigation, } ): NavigationScreenConfig<NavigationStackOptions, any> => {
   return {
     header() {
-      const { accountShellID } = navigation.state.params;
+      const { accountShellID } = navigation.state.params
 
       return (
         <NavHeader
           accountShellID={accountShellID}
           onBackPressed={() => navigation.pop()}
         />
-      );
+      )
     },
-  };
-};
+  }
+}
 
-export default AccountDetailsContainerScreen;
+export default AccountDetailsContainerScreen
