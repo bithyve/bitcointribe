7f47d5b58ef9a96f7ce3751f2401dc7d34d1c7dc
CypherPoet <CypherPoet@gmail.com>
2020-12-14 14:31:58 -0600

    Donation Account Web View Settings Screen
    Navigate to a full page Web View Settings screen when the Settings button is selected from the Donation Account Web Page Bottom Sheet on Account Details.
    
    Connected to https://github.com/bithyve/hexa/issues/2295.

--- src/navigation/stacks/accounts/AccountDetailsStack.tsx
+++ src/navigation/stacks/accounts/AccountDetailsStack.tsx
@@ -1,96 +1,13 @@
-import React from 'react';
-import { createStackNavigator } from "react-navigation-stack";
-import XPubSourceKind from '../../../common/data/enums/XPubSourceKind';
-import AccountSettingsEditDisplayPropertiesScreen from "../../../pages/Accounts/AccountSettings/AccountSettingsEditDisplayPropertiesScreen";
-import AccountSettingsMainScreen from "../../../pages/Accounts/AccountSettings/AccountSettingsMainScreen";
-import ReassignTransactionsMainOptionsScreen from "../../../pages/Accounts/AccountSettings/ReassignTransactions/MainOptionsScreen";
-import SelectReassignableTransactionsScreen from '../../../pages/Accounts/AccountSettings/ReassignTransactions/SelectReassignableTransactionsScreen';
-import AccountDetailsContainerScreen from "../../../pages/Accounts/Details/AccountDetailsContainerScreen";
-import TransactionDetailsContainerScreen from "../../../pages/Accounts/Transactions/TransactionDetailsContainerScreen";
-import TransactionsListContainerScreen from "../../../pages/Accounts/Transactions/TransactionsListContainerScreen";
-import ReassignTransactionsSelectDestinationScreen from '../../../pages/Accounts/AccountSettings/ReassignTransactions/SelectDestinationAccountScreen';
-import ReassignSubAccountSourcesSelectSourcesScreen from '../../../pages/Accounts/AccountSettings/ReassignTransactions/SelectSubAccountSourcesScreen';
-import AccountSettingsEditVisibilityScreen from '../../../pages/Accounts/AccountSettings/EditVisibilityScreen';
-import AccountSettingsMergeAccountShellsScreen from '../../../pages/Accounts/AccountSettings/MergeAccountShellsScreen';
-import defaultStackScreenNavigationOptions from '../../options/DefaultStackScreenNavigationOptions';
-import SmallNavHeaderCloseButton from '../../../components/navigation/SmallNavHeaderCloseButton';
-import SendStack from '../send/SendStack';
-import SmallNavHeaderBackButton from '../../../components/navigation/SmallNavHeaderBackButton';
-
-
-const SubAccountSettingsStack = createStackNavigator(
-  {
-    AccountSettingsMain: {
-      screen: AccountSettingsMainScreen,
-      navigationOptions: ({ navigation }) => {
-        return {
-          title: "Account Settings",
-          headerLeft: () => {
-            return <SmallNavHeaderCloseButton onPress={() => { navigation.pop() }} />;
-          },
-        };
-      },
-    },
-    EditDisplayProperties: {
-      screen: AccountSettingsEditDisplayPropertiesScreen,
-      navigationOptions: {
-        title: "Name & Description",
-      },
-    },
-    ReassignTransactionsMainOptions: {
-      screen: ReassignTransactionsMainOptionsScreen,
-      navigationOptions: {
-        title: "Reassign Transactions",
-      },
-    },
-    SelectReassignableTransactions: {
-      screen: SelectReassignableTransactionsScreen,
-      navigationOptions: {
-        title: "Reassign Transactions",
-      },
-    },
-    ReassignSubAccountSourcesSelectSources: {
-      screen: ReassignSubAccountSourcesSelectSourcesScreen,
-      navigationOptions: {
-        title: "Reassign Sources",
-      },
-    },
-    ReassignTransactionsSelectDestination: {
-      screen: ReassignTransactionsSelectDestinationScreen,
-      navigationOptions: ({ navigation }) => {
-        const reassignmentKind = navigation.getParam('reassignmentKind');
-        const nameText = reassignmentKind === XPubSourceKind.DESIGNATED ? 'Sources' : 'Transactions';
-
-        return {
-          title: `Reassign ${nameText}`,
-        };
-      },
-    },
-    EditVisibility: {
-      screen: AccountSettingsEditVisibilityScreen,
-      navigationOptions: {
-        title: "Account Visibility",
-      },
-    },
-    MergeAccounts: {
-      screen: AccountSettingsMergeAccountShellsScreen,
-      navigationOptions: {
-        title: "Merge Accounts",
-      },
-    },
-  },
-  {
-    initialRouteName: 'AccountSettingsMain',
-    defaultNavigationOptions: ({ navigation }) => {
-      return {
-        ...defaultStackScreenNavigationOptions,
-        headerLeft: () => {
-          return <SmallNavHeaderBackButton onPress={() => { navigation.pop(); }} />;
-        },
-      };
-    },
-  },
-);
+import React from 'react'
+import { createStackNavigator } from 'react-navigation-stack'
+import AccountDetailsContainerScreen from '../../../pages/Accounts/Details/AccountDetailsContainerScreen'
+import TransactionDetailsContainerScreen from '../../../pages/Accounts/Transactions/TransactionDetailsContainerScreen'
+import TransactionsListContainerScreen from '../../../pages/Accounts/Transactions/TransactionsListContainerScreen'
+import defaultStackScreenNavigationOptions from '../../options/DefaultStackScreenNavigationOptions'
+import SmallNavHeaderCloseButton from '../../../components/navigation/SmallNavHeaderCloseButton'
+import SendStack from '../send/SendStack'
+import SubAccountSettingsStack from './SubAccountSettingsStack'
+import DonationAccountWebViewSettingsScreen from '../../../pages/Accounts/AccountSettings/DonationAccountWebViewSettingsScreen'
 
 
 const AccountDetailsStack = createStackNavigator(
@@ -105,13 +22,19 @@ const AccountDetailsStack = createStackNavigator(
     TransactionsList: {
       screen: TransactionsListContainerScreen,
       navigationOptions: {
-        title: "All Transactions",
+        title: 'All Transactions',
       },
     },
     TransactionDetails: {
       screen: TransactionDetailsContainerScreen,
       navigationOptions: {
-        title: "Transaction Details",
+        title: 'Transaction Details',
+      },
+    },
+    DonationAccountWebViewSettings: {
+      screen: DonationAccountWebViewSettingsScreen,
+      navigationOptions: {
+        header: null,
       },
     },
     SubAccountSettings: {
@@ -124,15 +47,15 @@ const AccountDetailsStack = createStackNavigator(
   {
     mode: 'modal',
     initialRouteName: 'AccountDetailsRoot',
-    defaultNavigationOptions: ({ navigation }) => {
+    defaultNavigationOptions: ( { navigation } ) => {
       return {
         ...defaultStackScreenNavigationOptions,
         headerLeft: () => {
-          return <SmallNavHeaderCloseButton onPress={() => { navigation.pop(); }} />;
+          return <SmallNavHeaderCloseButton onPress={() => { navigation.pop() }} />
         },
-      };
+      }
     },
   },
-);
+)
 
-export default AccountDetailsStack;
+export default AccountDetailsStack
