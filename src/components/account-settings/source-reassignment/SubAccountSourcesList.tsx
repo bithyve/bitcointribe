dc383947bd96880ca14ed61a0d91d9faa09f8893
AliMeer <ali@purple7.com>
2020-12-20 21:04:51 +0000

    components/account-settings/source-reassignment code style update

--- src/components/account-settings/source-reassignment/SubAccountSourcesList.tsx
+++ src/components/account-settings/source-reassignment/SubAccountSourcesList.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import RadioButton from '../../RadioButton';
-import SubAccountDescribing from '../../../common/data/models/SubAccountInfo/Interfaces';
-import DesignatedSourceListItemContent from '../DesignatedSourceListItemContent';
+import React, { useCallback } from 'react'
+import { StyleSheet, FlatList } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import RadioButton from '../../RadioButton'
+import SubAccountDescribing from '../../../common/data/models/SubAccountInfo/Interfaces'
+import DesignatedSourceListItemContent from '../DesignatedSourceListItemContent'
 
 export type Props = {
   selectableSources: SubAccountDescribing[];
@@ -11,7 +11,7 @@ export type Props = {
   onSourceSelected: (source: SubAccountDescribing) => void;
 };
 
-const listItemKeyExtractor = (item: SubAccountDescribing) => item.id;
+const listItemKeyExtractor = (item: SubAccountDescribing) => item.id
 
 const SubAccountSourcesList: React.FC<Props> = ({
   selectableSources,
@@ -20,8 +20,8 @@ const SubAccountSourcesList: React.FC<Props> = ({
 }: Props) => {
 
   const isChecked = useCallback((subAccountInfo: SubAccountDescribing) => {
-    return selectedSourceIDs.has(subAccountInfo.id);
-  }, [selectedSourceIDs]);
+    return selectedSourceIDs.has(subAccountInfo.id)
+  }, [ selectedSourceIDs ])
 
   const renderItem = ({ item: subAccountInfo }: { item: SubAccountDescribing }) => {
     return (
@@ -36,24 +36,26 @@ const SubAccountSourcesList: React.FC<Props> = ({
 
         <DesignatedSourceListItemContent subAccountInfo={subAccountInfo} />
       </ListItem>
-    );
-  };
+    )
+  }
 
   return (
     <FlatList
       style={styles.rootContainer}
-      contentContainerStyle={{ paddingHorizontal: 14 }}
+      contentContainerStyle={{
+        paddingHorizontal: 14 
+      }}
       extraData={selectedSourceIDs}
       data={selectableSources}
       keyExtractor={listItemKeyExtractor}
       renderItem={renderItem}
     />
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   rootContainer: {
   }
-});
+})
 
-export default SubAccountSourcesList;
+export default SubAccountSourcesList
