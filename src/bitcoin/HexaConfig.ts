3355a3b68616639ea6b32690b2f464866e51d1ea
AliMeer <ali@purple7.com>
2020-12-21 11:38:51 +0000

    resolve merge conflicts

--- src/bitcoin/HexaConfig.ts
+++ src/bitcoin/HexaConfig.ts
@@ -27,7 +27,7 @@ class HexaConfig {
     Config.BIT_REQUEST_TIMEOUT.trim(),
     10,
   );
-  public GAP_LIMIT: number = parseInt(Config.BIT_GAP_LIMIT.trim(), 10);
+  public GAP_LIMIT: number = parseInt( Config.BIT_GAP_LIMIT.trim(), 10 );
   public DERIVATIVE_GAP_LIMIT = 5;
   public CIPHER_SPEC: {
     algorithm: string;
@@ -35,11 +35,11 @@ class HexaConfig {
     iv: Buffer;
     keyLength: number;
   } = {
-      algorithm: Config.BIT_CIPHER_ALGORITHM.trim(),
-      salt: Config.BIT_CIPHER_SALT.trim(),
-      keyLength: parseInt(Config.BIT_CIPHER_KEYLENGTH.trim(), 10),
-      iv: Buffer.alloc(16, 0),
-    };
+    algorithm: Config.BIT_CIPHER_ALGORITHM.trim(),
+    salt: Config.BIT_CIPHER_SALT.trim(),
+    keyLength: parseInt( Config.BIT_CIPHER_KEYLENGTH.trim(), 10 ),
+    iv: Buffer.alloc( 16, 0 ),
+  };
   public KEY_STRETCH_ITERATIONS = parseInt(
     Config.BIT_KEY_STRETCH_ITERATIONS.trim(),
     10,
@@ -59,33 +59,33 @@ class HexaConfig {
     SIGNING_SERVER: Config.BIT_API_URLS_SIGNING_SERVER.trim(),
   };
   public BSI = {
-    INIT_INDEX: parseInt(Config.BIT_BSI_INIT_INDEX.trim(), 10),
-    MAXUSEDINDEX: parseInt(Config.BIT_BSI_MAXUSEDINDEX.trim(), 10),
-    MINUNUSEDINDEX: parseInt(Config.BIT_BSI_MINUNUSEDINDEX.trim(), 10),
+    INIT_INDEX: parseInt( Config.BIT_BSI_INIT_INDEX.trim(), 10 ),
+    MAXUSEDINDEX: parseInt( Config.BIT_BSI_MAXUSEDINDEX.trim(), 10 ),
+    MINUNUSEDINDEX: parseInt( Config.BIT_BSI_MINUNUSEDINDEX.trim(), 10 ),
     DEPTH: {
-      INIT: parseInt(Config.BIT_BSI_DEPTH_INIT.trim(), 10),
-      LIMIT: parseInt(Config.BIT_BSI_DEPTH_LIMIT.trim(), 10),
+      INIT: parseInt( Config.BIT_BSI_DEPTH_INIT.trim(), 10 ),
+      LIMIT: parseInt( Config.BIT_BSI_DEPTH_LIMIT.trim(), 10 ),
     },
   };
-  public SSS_TOTAL: number = parseInt(Config.BIT_SSS_TOTAL.trim(), 10);
-  public SSS_THRESHOLD: number = parseInt(Config.BIT_SSS_THRESHOLD.trim(), 10);
-  public MSG_ID_LENGTH: number = parseInt(Config.BIT_MSG_ID_LENGTH.trim(), 10);
-  public CHUNK_SIZE: number = parseInt(Config.BIT_CHUNK_SIZE.trim(), 10);
-  public CHECKSUM_ITR: number = parseInt(Config.BIT_CHECKSUM_ITR.trim(), 10);
+  public SSS_TOTAL: number = parseInt( Config.BIT_SSS_TOTAL.trim(), 10 );
+  public SSS_THRESHOLD: number = parseInt( Config.BIT_SSS_THRESHOLD.trim(), 10 );
+  public MSG_ID_LENGTH: number = parseInt( Config.BIT_MSG_ID_LENGTH.trim(), 10 );
+  public CHUNK_SIZE: number = parseInt( Config.BIT_CHUNK_SIZE.trim(), 10 );
+  public CHECKSUM_ITR: number = parseInt( Config.BIT_CHECKSUM_ITR.trim(), 10 );
   public HEXA_ID: string = Config.BIT_HEXA_ID.trim();
-  public DPATH_PURPOSE: number = parseInt(Config.BIT_DPATH_PURPOSE.trim(), 10);
+  public DPATH_PURPOSE: number = parseInt( Config.BIT_DPATH_PURPOSE.trim(), 10 );
   public SSS_METASHARE_SPLITS: number = parseInt(
     Config.BIT_SSS_METASHARE_SPLITS.trim(),
     10,
   );
   public STATUS = {
-    SUCCESS: parseInt(Config.BIT_SUCCESS_STATUS_CODE.trim(), 10),
-    ERROR: parseInt(Config.BIT_ERROR_STATUS_CODE.trim(), 10),
+    SUCCESS: parseInt( Config.BIT_SUCCESS_STATUS_CODE.trim(), 10 ),
+    ERROR: parseInt( Config.BIT_ERROR_STATUS_CODE.trim(), 10 ),
   };
   public STANDARD = {
-    BIP44: parseInt(Config.BIT_STANDARD_BIP44.trim(), 10),
-    BIP49: parseInt(Config.BIT_STANDARD_BIP49.trim(), 10),
-    BIP84: parseInt(Config.BIT_STANDARD_BIP84.trim(), 10),
+    BIP44: parseInt( Config.BIT_STANDARD_BIP44.trim(), 10 ),
+    BIP49: parseInt( Config.BIT_STANDARD_BIP49.trim(), 10 ),
+    BIP84: parseInt( Config.BIT_STANDARD_BIP84.trim(), 10 ),
   };
 
   public HEALTH_STATUS = {
@@ -104,8 +104,8 @@ class HexaConfig {
     },
 
     TIME_SLOTS: {
-      SHARE_SLOT1: parseInt(Config.BIT_SHARE_HEALTH_TIME_SLOT1.trim(), 10),
-      SHARE_SLOT2: parseInt(Config.BIT_SHARE_HEALTH_TIME_SLOT2.trim(), 10),
+      SHARE_SLOT1: parseInt( Config.BIT_SHARE_HEALTH_TIME_SLOT1.trim(), 10 ),
+      SHARE_SLOT2: parseInt( Config.BIT_SHARE_HEALTH_TIME_SLOT2.trim(), 10 ),
     },
   };
 
@@ -113,7 +113,7 @@ class HexaConfig {
     Config.BIT_LEGACY_TC_REQUEST_EXPIRY.trim(),
     10,
   );
-  public TC_REQUEST_EXPIRY = parseInt(Config.BIT_TC_REQUEST_EXPIRY.trim(), 10);
+  public TC_REQUEST_EXPIRY = parseInt( Config.BIT_TC_REQUEST_EXPIRY.trim(), 10 );
 
   public ESPLORA_API_ENDPOINTS = {
     TESTNET: {
@@ -175,34 +175,34 @@ class HexaConfig {
   };
 
   public SUB_PRIMARY_ACCOUNT: DerivativeAccount = {
-    series: parseInt(Config.BIT_SUB_PRIMARY_ACCOUNT_SERIES.trim(), 10),
+    series: parseInt( Config.BIT_SUB_PRIMARY_ACCOUNT_SERIES.trim(), 10 ),
     instance: {
-      max: parseInt(Config.BIT_SUB_PRIMARY_ACCOUNT_INSTANCE_COUNT.trim(), 10),
+      max: parseInt( Config.BIT_SUB_PRIMARY_ACCOUNT_INSTANCE_COUNT.trim(), 10 ),
       using: 0,
     },
   };
 
   public FAST_BITCOINS: DerivativeAccount = {
-    series: parseInt(Config.BIT_FAST_BITCOINS_SERIES.trim(), 10),
+    series: parseInt( Config.BIT_FAST_BITCOINS_SERIES.trim(), 10 ),
     instance: {
-      max: parseInt(Config.BIT_FAST_BITCOINS_INSTANCE_COUNT.trim(), 10),
+      max: parseInt( Config.BIT_FAST_BITCOINS_INSTANCE_COUNT.trim(), 10 ),
       using: 0,
     },
   };
 
   public TRUSTED_CONTACTS: TrustedContactDerivativeAccount = {
     // corresponds to trusted channels
-    series: parseInt(Config.BIT_TRUSTED_CONTACTS_SERIES.trim(), 10),
+    series: parseInt( Config.BIT_TRUSTED_CONTACTS_SERIES.trim(), 10 ),
     instance: {
-      max: parseInt(Config.BIT_TRUSTED_CONTACTS_INSTANCE_COUNT.trim(), 10),
+      max: parseInt( Config.BIT_TRUSTED_CONTACTS_INSTANCE_COUNT.trim(), 10 ),
       using: 0,
     },
   };
 
   public DONATION_ACCOUNT: DonationDerivativeAccount = {
-    series: parseInt(Config.BIT_DONATION_ACCOUNT_SERIES.trim(), 10),
+    series: parseInt( Config.BIT_DONATION_ACCOUNT_SERIES.trim(), 10 ),
     instance: {
-      max: parseInt(Config.BIT_DONATION_ACCOUNT_INSTANCE_COUNT.trim(), 10),
+      max: parseInt( Config.BIT_DONATION_ACCOUNT_INSTANCE_COUNT.trim(), 10 ),
       using: 0,
     },
   };
@@ -214,13 +214,13 @@ class HexaConfig {
     DONATION_ACCOUNT: this.DONATION_ACCOUNT,
   };
 
-  public EJECTED_ACCOUNTS = [SUB_PRIMARY_ACCOUNT, DONATION_ACCOUNT];
+  public EJECTED_ACCOUNTS = [ SUB_PRIMARY_ACCOUNT, DONATION_ACCOUNT ];
 
-  public DERIVATIVE_ACC_TO_SYNC = Object.keys(this.DERIVATIVE_ACC).filter(
-    (account) => !this.EJECTED_ACCOUNTS.includes(account),
+  public DERIVATIVE_ACC_TO_SYNC = Object.keys( this.DERIVATIVE_ACC ).filter(
+    ( account ) => !this.EJECTED_ACCOUNTS.includes( account ),
   );
 
-  constructor(env: string) {
+  constructor( env: string ) {
     this.ENVIRONMENT = env
     // console.log({ env });
 
@@ -243,21 +243,21 @@ class HexaConfig {
     // console.log(Config.BIT_SERVER_MODE.trim(), this.RELAY, this.SIGNING_SERVER);
     this.setNetwork()
 
-    this.BITCOIN_NODE = new Client({
+    this.BITCOIN_NODE = new Client( {
       network:
         this.NETWORK === bitcoinJS.networks.bitcoin ? 'mainnet' : 'testnet',
       timeout: 10000,
       username: Config.BIT_RPC_USERNAME.trim(),
       password: Config.BIT_RPC_PASSWORD.trim(),
       host: Config.BIT_HOST_IP.trim(),
-    })
+    } )
 
     if (
       Config.BIT_SERVER_MODE.trim() === 'LOCAL' ||
       Config.BIT_SERVER_MODE.trim() === 'DEV'
     ) {
       this.APP_STAGE = 'dev'
-    } else if (Config.BIT_SERVER_MODE.trim() === 'STA') {
+    } else if ( Config.BIT_SERVER_MODE.trim() === 'STA' ) {
       this.APP_STAGE = 'sta'
     } else {
       this.APP_STAGE = 'app'
@@ -267,33 +267,33 @@ class HexaConfig {
   }
 
   public setNetwork = (): void => {
-    if (this.ENVIRONMENT === 'MAIN') {
+    if ( this.ENVIRONMENT === 'MAIN' ) {
       this.NETWORK = bitcoinJS.networks.bitcoin
     } else {
       this.NETWORK = bitcoinJS.networks.testnet
     }
   };
 
-  public connectToOwnNode = async () => {
-    let ownNodes: any = await AsyncStorage.getItem('OwnNodes')
+  public connectToOwnNode =  async () => {
+    let ownNodes:any = await AsyncStorage.getItem( 'OwnNodes' )
 
-    if (ownNodes) {
-      ownNodes = JSON.parse(ownNodes)
-      const ownNodeURL = ownNodes.urls[ownNodes.active]
-      if (ownNodeURL) {
+    if( ownNodes ){
+      ownNodes = JSON.parse( ownNodes )
+      const ownNodeURL = ownNodes.urls[ ownNodes.active ]
+      if( ownNodeURL ){
         const ownNodeEPs = {
           MULTIBALANCE: ownNodeURL + '/balances',
-          MULTIUTXO: ownNodeURL + '/utxos',
+          MULTIUTXO:  ownNodeURL + '/utxos',
           MULTITXN: ownNodeURL + '/data',
           MULTIBALANCETXN: ownNodeURL + '/baltxs',
-          MULTIUTXOTXN: ownNodeURL + '/utxotxs',
+          MULTIUTXOTXN: ownNodeURL + '/utxotxs', 
           NEWMULTIUTXOTXN: ownNodeURL + '/nutxotxs',
-          TXN_FEE: ownNodeURL + 'fee-estimates',
+          TXN_FEE: ownNodeURL  + 'fee-estimates',
           TXNDETAILS: ownNodeURL + '/tx',
           BROADCAST_TX: ownNodeURL + '/tx',
         }
 
-        if (this.ENVIRONMENT === 'MAIN')
+        if( this.ENVIRONMENT === 'MAIN' )
           this.ESPLORA_API_ENDPOINTS = {
             ...this.ESPLORA_API_ENDPOINTS,
             MAINNET: ownNodeEPs
@@ -308,4 +308,4 @@ class HexaConfig {
   }
 }
 
-export default new HexaConfig(Config.BIT_ENVIRONMENT.trim())
+export default new HexaConfig( Config.BIT_ENVIRONMENT.trim() )
