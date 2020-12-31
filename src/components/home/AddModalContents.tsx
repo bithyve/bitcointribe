0505929d6b8da56a7340ad8072c3a5ac5007ba3b
AliMeer <ali@purple7.com>
2020-12-31 19:44:26 +0000

    Add Modal Menu for home screen

--- src/components/home/AddModalContents.tsx
+++ src/components/home/AddModalContents.tsx
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import { View, Image, Text, StyleSheet } from 'react-native'
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import Colors from '../../common/Colors';
-import Fonts from '../../common/Fonts';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { AppBottomSheetTouchableWrapper } from '../AppBottomSheetTouchableWrapper';
-import { FlatList } from 'react-native-gesture-handler';
+} from 'react-native-responsive-screen'
+import Colors from '../../common/Colors'
+import Fonts from '../../common/Fonts'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { AppBottomSheetTouchableWrapper } from '../AppBottomSheetTouchableWrapper'
+import { FlatList } from 'react-native-gesture-handler'
 
-export default function AddModalContents(props) {
-  const [addData, setAddData] = useState([
+export default function AddModalContents( props ) {
+  const [ addData, setAddData ] = useState( [
     {
-      title: `Buy bitcoin into Hexa wallet`,
-      image: require('../assets/images/icons/icon_fastbitcoins_light_blue.png'),
+      title: 'Buy bitcoin into Hexa wallet',
+      image: require( '../assets/images/icons/icon_fastbitcoins_light_blue.png' ),
       info: 'Redeem a FastBitcoins voucher',
       type: 'buyBitcoins',
     },
     {
       title: 'Add a Contact',
-      image: require('../assets/images/icons/icon_addcontact.png'),
+      image: require( '../assets/images/icons/icon_addcontact.png' ),
       info: 'Add contacts from your Address Book',
       type: 'addContact',
     },
-  ]);
+  ] )
 
   return (
     <View style={styles.modalContentContainer}>
-      <View style={{ marginBottom: hp('13%') }}>
+      <View style={{
+        marginBottom: hp( '13%' ) 
+      }}>
         <FlatList
           data={addData}
           ItemSeparatorComponent={() => (
-            <View style={{ backgroundColor: Colors.white }}>
+            <View style={{
+              backgroundColor: Colors.white 
+            }}>
               <View style={styles.separatorView} />
             </View>
           )}
-          renderItem={({ item }) => (
+          renderItem={( { item } ) => (
             <AppBottomSheetTouchableWrapper
-              onPress={() => props.onPressElements(item.type)}
-              style={{ ...styles.addModalView, backgroundColor: Colors.white }}
+              onPress={() => props.onPressElements( item.type )}
+              style={{
+                ...styles.addModalView, backgroundColor: Colors.white 
+              }}
             >
               <View style={styles.modalElementInfoView}>
-                <View style={{ justifyContent: 'center' }}>
+                <View style={{
+                  justifyContent: 'center' 
+                }}>
                   <Image
                     source={item.image}
-                    style={{ width: 25, height: 25 }}
+                    style={{
+                      width: 25, height: 25 
+                    }}
                   />
                 </View>
                 <View
@@ -64,9 +74,9 @@ export default function AddModalContents(props) {
         />
       </View>
     </View>
-  );
+  )
 }
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
   modalContentContainer: {
     height: '100%',
     backgroundColor: Colors.white,
@@ -80,12 +90,12 @@ const styles = StyleSheet.create({
   },
   addModalTitleText: {
     color: Colors.blue,
-    fontSize: RFValue(13),
+    fontSize: RFValue( 13 ),
     fontFamily: Fonts.FiraSansRegular,
   },
   addModalInfoText: {
     color: Colors.textColorGrey,
-    fontSize: RFValue(11),
+    fontSize: RFValue( 11 ),
     fontFamily: Fonts.FiraSansRegular,
     flexWrap: 'wrap',
   },
@@ -101,4 +111,4 @@ const styles = StyleSheet.create({
     height: 2,
     backgroundColor: Colors.backgroundColor,
   },
-});
+} )
