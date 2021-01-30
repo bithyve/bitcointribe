a8efe961ee3667e41ea50b5edbfe5daa56f7903f
AliMeer <ali@purple7.com>
2021-01-30 23:26:25 +0000

    swan service call to redeem oidc code for token

--- src/services/swan.ts
+++ src/services/swan.ts
@@ -1,23 +1,45 @@
 import axios from 'axios'
 import Config from '../bitcoin/HexaConfig'
+import { useSelector } from 'react-redux'
 
-const swanOAuthURL = Config.SWAN_BASE_URL || 'https://login-demo.curity.io/oauth/v2/oauth-token'//'https://dev-api.swanbitcoin.com/'
+const swanOAuthURL = Config.SWAN_BASE_URL || 'https://dev-api.swanbitcoin.com'
+const swanClientId = Config.SWAN_CLIENT_ID || 'hexa-dev'
 
-export const redeemAuthCode = ( data ) =>
-  axios( {
-    method: 'post',
-    url: `${swanOAuthURL}/oauth`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: new URLSearchParams( {
-      grant_type: 'authorization_code',
-      code:'ESJaHCabOUKwf46LEqdSerokFDNb5DLa',
-      client_id:'demo-web-client',
-      client_secret:'6koyn9KpRuofYt2U',
-      redirect_uri: 'https://oauth.tools/callback/code'
+export const redeemAuthCodeForToken = () =>{
+  const { code_verifier, code } = useSelector( state => state.swanIntegration )
+  try {
+    const swanResponse = axios( {
+      method: 'post',
+      url: `${swanOAuthURL}/oidc/token`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'auth': {
+          'username': swanClientId,
+          'password': ''
+        }
+      },
+      data: new URLSearchParams( {
+        grant_type: 'authorization_code',
+        code,
+        code_verifier,
+        client_id:swanClientId,
+        redirect_uri: 'https://hexawallet.io/dev/swan/'
+      } )
     } )
-  } )
+    console.log( {
+      swanResponse
+    } )
+    return swanResponse || {
+      result: 'no response'
+    }
+  }
+  catch( error ) {
+    console.log( 'error calling wyre ', error )
+    return {
+      error
+    }
+  }
+}
 
 export const getSwanAuthToken = ( data ) =>
   axios( {
