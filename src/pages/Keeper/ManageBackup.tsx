0d60adc4df0bf9f6c701a9dc599a91dcda1b471e
Nikhita Gawande <37994396+cakesoft-nikhita@users.noreply.github.com>
2021-02-08 13:04:18 +0530

    - Notification related changes on ManageBackup

--- src/pages/Keeper/ManageBackup.tsx
+++ src/pages/Keeper/ManageBackup.tsx
@@ -61,7 +61,8 @@ import {
 import { modifyLevelStatus } from "./ManageBackupFunction";
 import ApproveSetup from "./ApproveSetup";
 import { MetaShare, notificationType } from "../../bitcoin/utilities/Interface";
-import firebase from "react-native-firebase";
+import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import { fetchKeeperTrustedChannel } from "../../store/actions/keeper";
 import { nameToInitials } from "../../common/CommonFunctions";
 import S3Service from '../../bitcoin/services/sss/S3Service';
@@ -203,11 +204,50 @@ class ManageBackup extends Component<
   }
 
   createNotificationListeners = async () => {
-    this.firebaseNotificationListener = firebase
-      .notifications()
-      .onNotification((notification) => {
-        this.onNotificationArrives(notification);
-      });
+    PushNotification.configure( {
+      onNotification: ( notification ) => {
+        console.log( 'NOTIFICATION:', notification )
+        // process the notification
+        if( notification.data ){
+          this.onNotificationArrives( notification )
+          // (required) Called when a remote is received or opened, or local notification is opened
+          notification.finish( PushNotificationIOS.FetchResult.NoData )
+        }
+      },
+
+      // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+      onAction: ( notification ) => {
+        console.log( 'ACTION:', notification.action )
+        console.log( 'NOTIFICATION:', notification )
+
+        // process the action
+      },
+
+      // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+      onRegistrationError: ( err ) => {
+        console.error( err.message, err )
+      },
+
+      // IOS ONLY (optional): default: all - Permissions to register.
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true,
+      },
+
+      // Should the initial notification be popped automatically
+      // default: true
+      popInitialNotification: true,
+
+      /**
+       * (optional) default: true
+       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+       * - if you are not using remote notification or do not have Firebase installed, use this:
+       *     requestPermissions: Platform.OS === 'ios'
+       */
+      requestPermissions: true,
+    } )
   };
 
   onNotificationArrives = async (notification) => {
