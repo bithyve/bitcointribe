ce4e62074b529c313d312555c4d795787ffba506
AliMeer <ali@purple7.com>
2021-03-18 21:05:33 +0000

    use fork for cloud backup saga

--- src/store/actions/cloud.ts
+++ src/store/actions/cloud.ts
@@ -2,21 +2,21 @@ export const SET_CLOUD_DATA = 'SET_CLOUD_DATA'
 export const UPDATE_HEALTH_FOR_CLOUD = 'UPDATE_HEALTH_FOR_CLOUD'
 export const UPDATE_CLOUD_HEALTH = 'UPDATE_CLOUD_HEALTH'
 
-export const setCloudData = (callback, kpInfo?, level?, share? ) => {
+export const setCloudData = ( callback, kpInfo?, level?, share? ) => {
   return {
     type: SET_CLOUD_DATA,
     payload: {
-        kpInfo, level, share, callback
+      kpInfo, level, share, callback
     },
   }
 }
 
-export const updateHealthForCloud = (share?) => {
+export const updateHealthForCloud = ( share? ) => {
   return {
     type: UPDATE_HEALTH_FOR_CLOUD,
     payload: {
-        share
+      share
     },
   }
 }
- 
+
