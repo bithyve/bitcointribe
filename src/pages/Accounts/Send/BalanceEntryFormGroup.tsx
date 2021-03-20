4f6f615011031fd3aecdee065eddff99aefea4fc
AliMeer <ali@purple7.com>
2021-03-20 20:38:07 +0000

    ui changes to use qr scanned amounts

--- src/pages/Accounts/Send/BalanceEntryFormGroup.tsx
+++ src/pages/Accounts/Send/BalanceEntryFormGroup.tsx
@@ -47,8 +47,8 @@ const BalanceEntryFormGroup: React.FC<Props> = ( {
   const dispatch = useDispatch()
 
   const [ isSendingMax, setIsSendingMax ] = useState( false )
-  const [ currentSatsAmountTextValue, setCurrentSatsAmountTextValue ] = useState( '' )
-  const [ currentFiatAmountTextValue, setCurrentFiatAmountTextValue ] = useState( '' )
+  const [ currentSatsAmountTextValue, setCurrentSatsAmountTextValue ] = useState( String( currentRecipient.amount ) )
+  const [ currentFiatAmountTextValue, setCurrentFiatAmountTextValue ] = useState( String( convertSatsToFiat( currentRecipient.amount ) ) )
 
   const currentSatsAmountFormValue = useMemo( () => {
     return Number( currentSatsAmountTextValue )
