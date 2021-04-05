92d57242eb696385f0fd02f961173f988191d578
AliMeer <Ali@purple7.com>
2021-04-05 16:15:31 -0400

    3114: adding please

--- src/pages/NewBHR/ManageBackupNewBHR.tsx
+++ src/pages/NewBHR/ManageBackupNewBHR.tsx
@@ -966,7 +966,7 @@ class ManageBackupNewBHR extends Component<
           }}>
             <Text style={{
               color: Colors.textColorGrey, fontSize: RFValue( 12 ), fontFamily: Fonts.FiraSansRegular
-            }}>{currentLevel === 1 ? 'Cloud backup complete, upgrade security to Level 2' : currentLevel === 2 ? 'Double backup complete, upgrade security to Level 3' : currentLevel === 3 ? 'Multi-key backup complete' : 'Cloud backup incomplete, complete Level 1' }</Text>
+            }}>{currentLevel === 1 ? 'Cloud backup complete, please upgrade security to Level 2' : currentLevel === 2 ? 'Double backup complete, please upgrade security to Level 3' : currentLevel === 3 ? 'Multi-key backup complete' : 'Cloud backup incomplete, please complete Level 1' }</Text>
           </View>
           <View
             style={{
