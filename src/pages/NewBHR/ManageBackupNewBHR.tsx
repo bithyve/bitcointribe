498e8c4ebb1b822e4549b6a8c5894b8977bdc4c2
Nikhita Gawande <37994396+cakesoft-nikhita@users.noreply.github.com>
2021-04-14 14:29:03 +0530

    - Resolved Text change on Home screen and wallet security screen. #3177

--- src/pages/NewBHR/ManageBackupNewBHR.tsx
+++ src/pages/NewBHR/ManageBackupNewBHR.tsx
@@ -973,7 +973,7 @@ class ManageBackupNewBHR extends Component<
           }}>
             <Text style={{
               color: Colors.textColorGrey, fontSize: RFValue( 12 ), fontFamily: Fonts.FiraSansRegular
-            }}>{currentLevel === 1 ? 'Cloud backup complete, please upgrade security to Level 2' : currentLevel === 2 ? 'Double backup complete, please upgrade security to Level 3' : currentLevel === 3 ? 'Multi-key backup complete' : 'Cloud backup incomplete. Please complete Level 1' }</Text>
+            }}>{currentLevel === 1 ? 'Cloud Backup complete, please upgrade security to Level 2' : currentLevel === 2 ? 'Double Backup complete, please upgrade security to Level 3' : currentLevel === 3 ? 'Multi-key Backup complete' : 'Cloud Backup incomplete. Please complete Level 1' }</Text>
           </View>
           <View
             style={{
