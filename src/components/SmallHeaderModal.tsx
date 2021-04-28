2f20ffde7698314c1ade5770f76d28c266f9c7ef
cakesoft-shivani <shivani@cakesofttech.com>
2021-04-28 15:16:17 +0530

    Wallet Security>Know More button not working #3242

--- src/components/SmallHeaderModal.tsx
+++ src/components/SmallHeaderModal.tsx
@@ -1,53 +1,55 @@
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-    View,
-    Image,
-    TouchableOpacity,
-    Text,
-    StyleSheet
-} from 'react-native';
-import Colors from "../common/Colors";
-import Fonts from "../common/Fonts";
-import { RFValue } from 'react-native-responsive-fontsize';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { AppBottomSheetTouchableWrapper } from './AppBottomSheetTouchableWrapper';
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from 'react-native'
+import Colors from '../common/Colors'
+import Fonts from '../common/Fonts'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import { AppBottomSheetTouchableWrapper } from './AppBottomSheetTouchableWrapper'
 
-export default function SmallHeaderModal(props) {
-    return <AppBottomSheetTouchableWrapper
+export default function SmallHeaderModal( props ) {
+  return <AppBottomSheetTouchableWrapper
     activeOpacity={10}
     onPress={() => props.onPressHeader && props.onPressHeader()}
-    style={{...styles.modalHeaderContainer, backgroundColor: props.backgroundColor ? props.backgroundColor : Colors.white, borderLeftColor:props.borderColor? props.borderColor: Colors.borderColor, borderRightColor: props.borderColor? props.borderColor:Colors.borderColor, borderTopColor: props.borderColor? props.borderColor:Colors.borderColor}}
+    style={{
+      ...styles.modalHeaderContainer, backgroundColor: props.backgroundColor ? props.backgroundColor : Colors.white, borderLeftColor:props.borderColor? props.borderColor: Colors.borderColor, borderRightColor: props.borderColor? props.borderColor:Colors.borderColor, borderTopColor: props.borderColor? props.borderColor:Colors.borderColor
+    }}
   >
     <View style={styles.modalHeaderHandle} />
   </AppBottomSheetTouchableWrapper>
 }
 
-const styles = StyleSheet.create({
-    modalHeaderContainer: {
-        marginTop: 'auto',
-        flex: 1,
-        backgroundColor: Colors.blue,
-        height: 20,
-        borderTopLeftRadius: 10,
-        borderLeftWidth: 1,
-        borderTopRightRadius: 10,
-        borderRightWidth: 1,
-        borderTopWidth: 1,
-        zIndex: 9999,
-      },
-      modalHeaderHandle: {
-        width: 50,
-        height: 5,
-        backgroundColor: Colors.borderColor,
-        borderRadius: 10,
-        alignSelf: 'center',
-        marginTop: 7,
-      },
-      modalHeaderTitleText: {
-        color: Colors.blue,
-        fontSize: RFValue(18),
-        fontFamily: Fonts.FiraSansRegular,
-        marginLeft: 15,
-      },
-})
+const styles = StyleSheet.create( {
+  modalHeaderContainer: {
+    marginTop: 'auto',
+    flex: 1,
+    backgroundColor: Colors.blue,
+    height: 20,
+    borderTopLeftRadius: 10,
+    borderLeftWidth: 1,
+    borderTopRightRadius: 10,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    zIndex: 9999,
+  },
+  modalHeaderHandle: {
+    width: 50,
+    height: 5,
+    backgroundColor: Colors.borderColor,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginTop: 7,
+  },
+  modalHeaderTitleText: {
+    color: Colors.blue,
+    fontSize: RFValue( 18 ),
+    fontFamily: Fonts.FiraSansRegular,
+    marginLeft: 15,
+  },
+} )
