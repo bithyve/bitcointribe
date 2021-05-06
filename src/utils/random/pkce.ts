3455ead1d45a8d32f9144b3e61ab3359cd5cf14f
AliMeer <Ali@purple7.com>
2021-05-06 22:47:48 +0100

    using custom pkce code

---
+++ src/utils/random/pkce.ts
@@ -0,0 +1,59 @@
+import crypto from 'crypto'
+import { base64UrlEncode, generateRandomBytes } from './random'
+
+
+export const generatePKCEParameters = async () => {
+  const challenge = await asyncPkceChallenge()
+  console.log( {
+    challenge
+  } )
+  const code_challenge = challenge.codeChallenge
+  const code_verifier = challenge.codeVerifier
+
+  const nonce = `${generateRandomNumber ( 11 )}-${generateRandomString( 3 )}`
+
+  const state = `${generateRandomNumber ( 11 )}-${generateRandomString( 3 )}`
+
+  return {
+    code_challenge, code_verifier, nonce, state
+  }
+}
+
+const generateRandomString = ( length: number ): string => {
+  let randomString = ''
+  const possibleChars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  for ( let itr = 0; itr < length; itr++ ) {
+    randomString += possibleChars.charAt(
+      Math.floor( Math.random() * possibleChars.length ),
+    )
+  }
+  return randomString
+}
+
+const generateRandomNumber = ( digits: number ): number => Math.floor( Math.random() * 10**digits )
+
+const generateChallenge = ( verifier ) => {
+  return base64UrlEncode(
+    crypto
+      .createHash( 'sha256' )
+      .update( verifier )
+      .digest( 'hex' )
+  )
+}
+
+const  generateVerifier =()=> {
+  return base64UrlEncode( generateRandomBytes( 96 ) )
+}
+
+const asyncPkceChallenge = () =>{
+  return new Promise( ( resolve ) => {
+    generateVerifier().then( ( verifier ) => {
+      const challenge = generateChallenge( verifier )
+      resolve( {
+        codeChallenge: challenge,
+        codeVerifier: verifier
+      } )
+    } )
+  } )
+}
+
