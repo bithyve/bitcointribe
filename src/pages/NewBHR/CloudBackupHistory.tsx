946a108fa38f4f4be7a7d685277b699fd2d3d5c3
cakesoft-shivani <shivani@cakesofttech.com>
2021-06-05 13:31:41 +0530

    Space between the words Google and Drive #3349

--- src/pages/NewBHR/CloudBackupHistory.tsx
+++ src/pages/NewBHR/CloudBackupHistory.tsx
@@ -206,7 +206,7 @@ const CloudBackupHistory = ( props ) => {
       <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
       <HistoryHeaderComponent
         onPressBack={() => props.navigation.goBack()}
-        selectedTitle={Platform.OS == 'ios' ? 'iCloud backup' : 'GoogleDrive backup'}
+        selectedTitle={Platform.OS == 'ios' ? 'iCloud backup' : 'Google Drive backup'}
         selectedTime={props.navigation.state.params.selectedTime}
         moreInfo={''}
         tintColor={Colors.deepBlue}
