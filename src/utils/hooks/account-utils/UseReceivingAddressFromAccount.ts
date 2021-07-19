6d84bd548813c8718293f6bee6501c36c54c969d
Parsh <parsh.cosmos11@gmail.com>
2021-07-19 14:50:24 +0530

    deposit accountI: UI integration

--- src/utils/hooks/account-utils/UseReceivingAddressFromAccount.ts
+++ src/utils/hooks/account-utils/UseReceivingAddressFromAccount.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
-import { Accounts, AccountType } from '../../../bitcoin/utilities/Interface'
+import { Accounts, AccountType, Wallet } from '../../../bitcoin/utilities/Interface'
 import AccountShell from '../../../common/data/models/AccountShell'
 import { AccountsState } from '../../../store/reducers/accounts'
 
@@ -13,9 +13,11 @@ export default function useReceivingAddressFromAccount(
   const accountState: AccountsState =  useSelector( state => state.accounts )
   const accountShells: AccountShell[] = accountState.accountShells
   const accounts: Accounts = accountState.accounts
+  const wallet: Wallet = useSelector( state => state.storage.wallet )
 
   return useMemo( () => {
     let receivingAddress
+    if( !wallet.accounts[ pickAddressFrom ] ) pickAddressFrom = AccountType.CHECKING_ACCOUNT // default account
     switch( pickAddressFor ) {
         case AccountType.WYRE_ACCOUNT:
         case AccountType.RAMP_ACCOUNT:
@@ -26,5 +28,5 @@ export default function useReceivingAddressFromAccount(
     }
 
     return receivingAddress
-  }, [ accountShells, accounts ] )
+  }, [ accountShells, accounts, wallet ] )
 }
