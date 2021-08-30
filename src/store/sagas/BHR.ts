a072a62759c00a8cca03d97b8f3a5f0d78c46914
Shashank Shinde <shashank@cakesofttech.com>
2021-08-30 18:20:23 +0530

    Undefined Transactions. #4119

--- src/store/sagas/BHR.ts
+++ src/store/sagas/BHR.ts
@@ -640,7 +640,6 @@ const asyncDataToBackup = async () => {
 }
 
 function* updateWalletImageWorker() {
-  //console.log( 'Update Wallet Image' )
   yield put( switchS3LoadingStatus( 'updateWIStatus' ) )
   const wallet = yield call( dbManager.getWallet )
   const contacts = yield call( dbManager.getTrustedContacts )
@@ -667,7 +666,8 @@ function* updateWalletImageWorker() {
         type: tx.type
       } )
     } )
-    data.transactions = txns
+    data.transactions = []
+    data.transactionsMeta = txns
     console.log( data )
     const cipher = crypto.createCipheriv(
       BHROperations.cipherSpec.algorithm,
