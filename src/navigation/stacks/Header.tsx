5250b38d508315d2fd3a5f08255ece9e1aa3fd75
cakesoft-shivani <shivani@cakesofttech.com>
2021-09-22 19:40:24 +0530

    Test upgrade notifications #4365 notification crash issue

--- src/navigation/stacks/Header.tsx
+++ src/navigation/stacks/Header.tsx
@@ -552,9 +552,14 @@ class Home extends PureComponent<HomePropsTypes, HomeStateTypes> {
       // smallIcon:'ic_notification',
       onNotification: ( notification ) => {
         this.props.getMessages()
-        const { content } = notification.data
-        const notificationId = JSON.parse( content ).notificationId
-        this.currentNotificationId = notificationId
+        if( notification.data && notification.data.content ){
+          const { content } = notification.data
+          const notificationId = JSON.parse( content ).notificationId
+          this.currentNotificationId = notificationId
+        } else if( notification.data[ 'google.message_id' ] ){
+          const notificationId = notification.data[ 'google.message_id' ]
+          this.currentNotificationId = notificationId
+        }
         this.notificationCheck()
         // process the notification
         if ( notification.data ) {
