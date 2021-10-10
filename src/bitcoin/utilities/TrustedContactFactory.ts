bb617045c84a89820fd006a0b26614bd25064708
Parsh <parsh.cosmos11@gmail.com>
2021-10-10 17:16:05 +0530

    TC: Factory

---
+++ src/bitcoin/utilities/TrustedContactFactory.ts
@@ -0,0 +1,38 @@
+import idx from 'idx'
+import crypto from 'crypto'
+import { ContactDetails, TrustedContact, UnecryptedStreamData } from './Interface'
+
+export function generateTrustedContact(
+  {
+    contactDetails,
+    channelKey,
+    secondaryChannelKey,
+    contactsSecondaryChannelKey,
+    unEncryptedOutstreamUpdates,
+  }: {
+    contactDetails: ContactDetails,
+    channelKey: string,
+    secondaryChannelKey?: string,
+    unEncryptedOutstreamUpdates: UnecryptedStreamData,
+    contactsSecondaryChannelKey?: string,
+ }
+): TrustedContact {
+  const newContact: TrustedContact = {
+    contactDetails,
+    channelKey,
+    permanentChannelAddress: crypto
+      .createHash( 'sha256' )
+      .update( channelKey )
+      .digest( 'hex' ),
+    relationType: idx(
+      unEncryptedOutstreamUpdates,
+      ( _ ) => _.primaryData.relationType
+    ),
+    secondaryChannelKey,
+    contactsSecondaryChannelKey,
+    isActive: true,
+    hasNewData: true,
+  }
+
+  return newContact
+}
