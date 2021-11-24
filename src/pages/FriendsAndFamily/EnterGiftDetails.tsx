6e66e5b757c1eafd522d397ef16ce3bad4e6b9f6
Shashank Shinde <shashank@cakesofttech.com>
2021-11-24 14:50:06 +0530

    Take out the gift theme option #4863

--- src/pages/FriendsAndFamily/EnterGiftDetails.tsx
+++ src/pages/FriendsAndFamily/EnterGiftDetails.tsx
@@ -196,6 +196,7 @@ const GiftDetails = ( { navigation } ) => {
         >{'Scan the QR or click the link to accept your gift.'}
         </Text>
         <TouchableOpacity
+          disabled
           onPress={() => setDropdownBoxOpenClose( !dropdownBoxOpenClose )}
           style={[ styles.dashedContainer, {
             borderColor: dropdownBoxValue?.color ?? Colors.lightBlue
@@ -230,9 +231,9 @@ const GiftDetails = ( { navigation } ) => {
                   </Text>
                 </View>
               </View>
-              {
+              {/* {
                 dropdownBoxOpenClose ? <ArrowUp />  : <ArrowDown />
-              }
+              } */}
             </View>
           </View>
         </TouchableOpacity>
