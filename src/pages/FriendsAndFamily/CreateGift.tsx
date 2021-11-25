95b6c2d7774bbc5d153652b0fb57496b7ec5ee09
Parsh <parsh.cosmos11@gmail.com>
2021-11-25 16:53:05 +0530

    create gift: valid acc check

--- src/pages/FriendsAndFamily/CreateGift.tsx
+++ src/pages/FriendsAndFamily/CreateGift.tsx
@@ -338,7 +338,7 @@ const CreateGift = ( { navigation } ) => {
       height: 'auto'
     }}>
       { activeAccounts.map( ( item, index ) => {
-        if ( item.primarySubAccount.type === AccountType.SWAN_ACCOUNT || !item.primarySubAccount.isUsable ) return
+        if ( [ AccountType.SWAN_ACCOUNT, AccountType.DONATION_ACCOUNT ].includes( item.primarySubAccount.type ) || !item.primarySubAccount.isUsable ) return
         return(
           <View key={index} style={{
             backgroundColor: Colors.white
