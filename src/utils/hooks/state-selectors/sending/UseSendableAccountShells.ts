a004f0201172a290982286473684a46413291096
Parsh <parsh.cosmos11@gmail.com>
2021-11-29 11:21:23 +0530

    fixed: hidden acc recipient crash

--- src/utils/hooks/state-selectors/sending/UseSendableAccountShells.ts
+++ src/utils/hooks/state-selectors/sending/UseSendableAccountShells.ts
@@ -2,21 +2,22 @@ import AccountShell from '../../../../common/data/models/AccountShell'
 import useAccountsState from '../accounts/UseAccountsState'
 import { useMemo } from 'react'
 import SubAccountKind from '../../../../common/data/enums/SubAccountKind'
+import useActiveAccountShells from '../accounts/UseActiveAccountShells'
 
 
 export default function useSendableAccountShells( currentAccountShell: AccountShell ): AccountShell[] {
-  const accountsState = useAccountsState()
+  const accountShells = useActiveAccountShells()
 
   return useMemo( () => {
     if( currentAccountShell.primarySubAccount.kind === SubAccountKind.TEST_ACCOUNT ) return []
 
     const sendableAccountShells = []
-    accountsState.accountShells.forEach( ( accountShell ) => {
+    accountShells.forEach( ( accountShell ) => {
       if( accountShell.primarySubAccount.isUsable && accountShell.primarySubAccount.kind !== SubAccountKind.TEST_ACCOUNT )
         if( accountShell.id !== currentAccountShell.id )
           sendableAccountShells.push( accountShell )
     } )
 
     return sendableAccountShells
-  }, [ accountsState.accountShells ] )
+  }, [ accountShells ] )
 }
