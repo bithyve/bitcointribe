94746d7bb2400b8868f922f4f884c7759fa1bffd
Shashank Shinde <shashank@cakesofttech.com>
2022-01-10 13:32:07 +0530

    purchase sats from Wyre #5214

--- src/common/CommonFunctions/index.ts
+++ src/common/CommonFunctions/index.ts
@@ -476,6 +476,12 @@ export const processDeepLink = ( deepLink: string ) => {
         }
       }
 
+    if ( splits.includes( 'wyre' ) ) {
+      if( splits.includes( 'failed' ) ) {
+        Alert.alert( 'Wyre purchase failed', 'Please try again after sometime.' )
+      }
+      return
+    }
     // hexa links
     if ( splits[ 3 ] !== config.APP_STAGE ){
       Alert.alert(
