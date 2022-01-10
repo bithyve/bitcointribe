e8732507db027d70f6c5298132017e39bc85cfbe
Rahil Ahmad <rahilahmad1258@gmail.com>
2022-01-10 15:30:07 +0500

    fix sender name issue when send gift from f&f #5161

--- src/pages/FriendsAndFamily/EnterGiftDetails.tsx
+++ src/pages/FriendsAndFamily/EnterGiftDetails.tsx
@@ -572,6 +572,7 @@ const GiftDetails = ( { navigation } ) => {
             navigation.navigate( 'AddContact', {
               fromScreen: 'GiftDetails',
               giftId,
+              senderName: name,
               setActiveTab: navigation.state.params.setActiveTab
             } )
           } else {
