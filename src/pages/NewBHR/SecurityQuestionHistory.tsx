7e90e779f8f20c35b249eb0aa74f54b26ee4be59
Parsh <parsh.cosmos11@gmail.com>
2022-01-29 11:21:22 +0530

    time formatter: crash fix

--- src/pages/NewBHR/SecurityQuestionHistory.tsx
+++ src/pages/NewBHR/SecurityQuestionHistory.tsx
@@ -43,6 +43,7 @@ import CardWithRadioBtn from '../../components/CardWithRadioBtn'
 import HeaderTitle from '../../components/HeaderTitle'
 import Feather from 'react-native-vector-icons/Feather'
 import ButtonStyles from '../../common/Styles/ButtonStyles'
+import { getTime } from '../../common/CommonFunctions/timeFormatter'
 
 const SecurityQuestionHistory = ( props ) => {
   const strings  = translations[ 'bhr' ]
