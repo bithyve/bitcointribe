3b0cd520e74853a9b1ed681499680327069bb3fc
Parsh <parsh.cosmos11@gmail.com>
2022-02-07 09:10:52 +0530

    multi-sig accounts: xpub log

--- src/pages/Accounts/AccountSettings/XPubDetailsScreen.tsx
+++ src/pages/Accounts/AccountSettings/XPubDetailsScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import QRCode from '../../../components/QRCode'
 import { heightPercentageToDP } from 'react-native-responsive-screen'
 import BottomInfoBox from '../../../components/BottomInfoBox'
@@ -11,7 +11,14 @@ import useAccountShellForID from '../../../utils/hooks/state-selectors/accounts/
 import useAccountShellFromNavigation from '../../../utils/hooks/state-selectors/accounts/UseAccountShellFromNavigation'
 import useXPubForSubAccount from '../../../utils/hooks/state-selectors/accounts/UseXPubForSubAccount'
 import HeadingStyles from '../../../common/Styles/HeadingStyles'
+import { Account, MultiSigAccount } from '../../../bitcoin/utilities/Interface'
+import useAccountByAccountShell from '../../../utils/hooks/state-selectors/accounts/UseAccountByAccountShell'
 
+enum XpubTypes {
+  PRIMARY = 'PRIMARY',
+  SECONDARY = 'SECONDARY',
+  BITHYVE = 'BITHYVE'
+}
 
 export type Props = {
   navigation: any;
@@ -19,9 +26,33 @@ export type Props = {
 
 const XPubDetailsScreen: React.FC<Props> = ( { navigation }: Props ) => {
   const accountShell = useAccountShellFromNavigation( navigation )
-  const primarySubAccount = usePrimarySubAccountForShell( accountShell )
+  const account: Account | MultiSigAccount = useAccountByAccountShell( accountShell )
+  const [ xpubs, setXpubs ] = useState( [] )
 
-  const xPub = useXPubForSubAccount( primarySubAccount ) || 'No xPub Found'
+  useEffect( () => {
+    const availableXpubs = []
+    const primaryXpub = account.xpub
+    availableXpubs.push( {
+      xpub: primaryXpub,
+      type: XpubTypes.PRIMARY
+    } )
+
+    if( ( account as MultiSigAccount ).is2FA ){
+      const  { secondary, bithyve } = ( account as MultiSigAccount ).xpubs
+      availableXpubs.push( {
+        xpub: secondary,
+        type: XpubTypes.SECONDARY
+      } )
+      availableXpubs.push( {
+        xpub: bithyve,
+        type: XpubTypes.BITHYVE
+      } )
+    }
+
+    setXpubs( availableXpubs )
+  }, [ account ] )
+
+
 
   return (
     <View style={styles.rootContainer}>
@@ -31,13 +62,36 @@ const XPubDetailsScreen: React.FC<Props> = ( { navigation }: Props ) => {
         </Text>
       </View>
 
-      <View>
-        <View style={styles.qrCodeContainer}>
-          <QRCode title="xPub" value={xPub} size={heightPercentageToDP( 33 )} />
-        </View>
+      <FlatList
+        data={xpubs}
+        renderItem={( { item } ) => {
+          let title
+          switch( item.type ){
+              case XpubTypes.PRIMARY:
+                title = 'xPub'
+                break
 
-        <CopyThisText text={xPub} />
-      </View>
+              case XpubTypes.SECONDARY:
+                title = 'secondary xPub'
+                break
+
+              case XpubTypes.BITHYVE:
+                title = 'bithyve xPub'
+                break
+          }
+
+          return (
+            <View>
+              <View style={styles.qrCodeContainer}>
+                <QRCode title={title} value={item.xpub} size={heightPercentageToDP( 33 )} />
+              </View>
+
+              <CopyThisText text={item.xpub} />
+            </View>
+          )
+        }}
+        keyExtractor={ item => item}
+      />
 
       <View
         style={{
