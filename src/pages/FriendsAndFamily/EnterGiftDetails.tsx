6fb92091767f6cb97d2dd72c16cc41739f7d80f1
Parsh <parsh.cosmos11@gmail.com>
2022-02-17 17:46:08 +0530

    Gift deep-link: default OTP encryption

--- src/pages/FriendsAndFamily/EnterGiftDetails.tsx
+++ src/pages/FriendsAndFamily/EnterGiftDetails.tsx
@@ -133,15 +133,15 @@ const GiftDetails = ( { navigation } ) => {
   const strings = translations[ 'f&f' ]
   // const login = translations[ 'login' ]
   const common = translations[ 'common' ]
-  const [ note, setNote ] = useState( 
+  const [ note, setNote ] = useState(
     navigation.state.params.giftMsg != undefined ? navigation.state.params.giftMsg :
-    'Bitcoin is a new type of money that is not controlled by any government or company' )
+      'Bitcoin is a new type of money that is not controlled by any government or company' )
   const [ name, setName ] = useState( '' )
   const [ dropdownBoxOpenClose, setDropdownBoxOpenClose ] = useState( false )
   const [ addfNf, setAddfNf ] = useState( false )
   const [ dropdownBoxList, setDropdownBoxList ] = useState( [] )
   const [ advanceSettingsModal, setAdvanceSettingsModal ] = useState( false )
-  const [ selectedAdvancedOption, setSelectedAdvancedOption ] = useState( AdvancedSetting.NO_2FA )
+  const [ selectedAdvancedOption, setSelectedAdvancedOption ] = useState( AdvancedSetting.SIMPLE_OTP )
   const [ FnFIdentificationModal, setFnFIdentificationModal ] = useState( false )
   const [ selectedFAndF, setSelectedFAndF ] = useState( FNF_IDENTIFICATION_TYPE.PHONE_NUMBER )
 
@@ -152,7 +152,7 @@ const GiftDetails = ( { navigation } ) => {
   const [ confirmSecretPhrase, setConfirmSecretPhrase ] = useState( '' )
   const [ confirmSecretPhraseVisibility, setconfirmSecretPhraseVisibility ] = useState( true )
   const [ secretPhraseHint, setSecretPhraseHint ] = useState( '' )
-  const [ encryptionType, setEncryptionType ] = useState( DeepLinkEncryptionType.DEFAULT )
+  const [ encryptionType, setEncryptionType ] = useState( DeepLinkEncryptionType.OTP )
 
   const [ dropdownBoxValue, setDropdownBoxValue ] = useState( {
     id: GiftThemeId.ONE,
@@ -224,7 +224,7 @@ const GiftDetails = ( { navigation } ) => {
           break
 
         default:
-          setEncryptionType( DeepLinkEncryptionType.DEFAULT )
+          setEncryptionType( DeepLinkEncryptionType.OTP )
     }
   }
 
@@ -658,13 +658,13 @@ const GiftDetails = ( { navigation } ) => {
               }}
             >
               <View style={styles.settingIcon}>
-              <Setting/>
+                <Setting/>
               </View>
               <Text style={{
                 color: 'white'
               }}>{'Advanced'}</Text>
               <View style={styles.menuIcon}>
-              <Menu/>
+                <Menu/>
               </View>
             </TouchableOpacity>
           </View>
@@ -1089,7 +1089,7 @@ const styles = StyleSheet.create( {
     backgroundColor: Colors.lightTextColor,
     marginHorizontal: wp( 2 ),
     alignSelf: 'center'
-    
+
   },
   timeInfo:{
     width: '87%',
