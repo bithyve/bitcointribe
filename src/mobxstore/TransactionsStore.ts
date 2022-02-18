9c2127e5a18fba4e30f2f79e8469014556eee96c
Shashank Shinde <shashank@cakesofttech.com>
2022-02-18 09:20:27 +0530

    fix: onchain tx not showing

--- src/mobxstore/TransactionsStore.ts
+++ src/mobxstore/TransactionsStore.ts
@@ -64,7 +64,7 @@ export default class TransactionsStore {
           this.transactions = data.transactions
             .slice()
             .reverse()
-            .filter( tx=> tx.amount > 0 )
+            //.filter( tx=> tx.amount > 0 )
             .map( ( tx: any ) => new Transaction( tx ) )
           this.loading = false
         } )
