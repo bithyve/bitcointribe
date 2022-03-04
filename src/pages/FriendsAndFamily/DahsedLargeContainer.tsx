b83daa05ab64afa666c9b48db367baad0c36f55a
Shashank Shinde <shashank@cakesofttech.com>
2022-03-04 19:41:42 +0530

    Textual changes. #5574

--- src/pages/FriendsAndFamily/DahsedLargeContainer.tsx
+++ src/pages/FriendsAndFamily/DahsedLargeContainer.tsx
@@ -127,7 +127,7 @@ const DashedLargeContainer = ( props ) => {
               }}>
                 {props.subText}
               </Text>
-              {'\n\nClick on the link and follow the steps to receive bitcoin in your Hexa 2.0 bitcoin wallet'}
+              {!props.isAccept ? '\n\nClick on the link and follow the steps to receive bitcoin in your Hexa 2.0 bitcoin wallet': '\n\nYou can either add the sats to an Account or retain it to forward to your loved ones.'}
             </Text>
           </View>
           {props.date &&
