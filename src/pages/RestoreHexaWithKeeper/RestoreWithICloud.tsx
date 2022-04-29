4718d2f100885caee42223c1108fb48f14d2a2c2
Bhumika Reddy <reddybhumi1999@gmail.com>
2022-04-29 17:38:45 +0530

    Language json changes

--- src/pages/RestoreHexaWithKeeper/RestoreWithICloud.tsx
+++ src/pages/RestoreHexaWithKeeper/RestoreWithICloud.tsx
@@ -819,7 +819,9 @@ class RestoreWithICloud extends Component<
 
       <RestoreFromICloud
         title={`${strings[ 'Recoverfrom' ]} ${Platform.OS == 'ios'  ? 'iCloud' : 'GDrive'}`}
-        subText={ formatString( `${strings[ 'Clickingon' ]} ${Platform.OS == 'ios'  ? 'iCloud' : 'GDrive'}` )}
+        subText= {
+          formatString( 'Clickingon', Platform.OS == 'ios'  ? 'iCloud' : 'GDrive' )
+        }
         cardInfo={strings[ 'RestoringWalletfrom' ]}
         cardTitle={selectedBackup && selectedBackup.walletName ? selectedBackup.walletName : ''}
         levelStatus={
