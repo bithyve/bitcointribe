3a4697d57e168850d0062e6d13a7e048b3ed4373
PraneethGunas <praneethgunas404@gmail.com>
2022-05-12 12:30:41 +0530

    Added flipper debugger integration

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ echo "patch cocoapods"
 cp ./rnPatchFiles/RNLocalize.podspec ./node_modules/react-native-localize/RNLocalize.podspec
 cp ./rnPatchFiles/react-native-netinfo.podspec ./node_modules/@react-native-community/netinfo/react-native-netinfo.podspec
 
+rm -rf ./node_modules/react-native-tcp/ios/CocoaAsyncSocket/
+cp ./rnPatchFiles/TcpSockets.podspec ./node_modules/react-native-tcp/TcpSockets.podspec
+
 # ios dependency installation
 cd ios && pod deintegrate && pod install
 
