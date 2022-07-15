c726a6051525a47815bdd71bf3716e42993906cb
Vatsal032 <vatsalm7071@gmail.com>
2022-07-15 15:47:58 +0530

    Fixed Issues #4323

--- src/pages/Accounts/Send/AccountSendConfirmationContainerScreen.tsx
+++ src/pages/Accounts/Send/AccountSendConfirmationContainerScreen.tsx
@@ -184,11 +184,23 @@ const AccountSendConfirmationContainerScreen: React.FC<Props> = ( { navigation }
   useAccountSendST2CompletionEffect( {
     onSuccess: ( txid: string | null, amt: number | null ) => {
       if ( txid ) {
+        let type;
+        if (sourceAccountShell.primarySubAccount.type === undefined) {
+          type = -1;
+        } else if (sourceAccountShell.primarySubAccount.type === 'TEST_ACCOUNT') {
+          type = 0;
+        } else if (sourceAccountShell.primarySubAccount.type === 'CHECKING_ACCOUNT') {
+          type = 1;
+        } else if (sourceAccountShell.primarySubAccount.type === 'SWAN_ACCOUNT') {
+          type = 2;
+        } else if (sourceAccountShell.primarySubAccount.type === 'SAVINGS_ACCOUNT') {
+          type = 3;
+        }
+
         if (amt) {
-          // const formatedAmt = useFormattedAmountText(amt);
-          dispatch( sendTxNotification( txid, amt + ' ' + formattedUnitText ) )
+          dispatch( sendTxNotification( txid, amt + ' ' + formattedUnitText, type ) )
         } else {
-          dispatch( sendTxNotification( txid, null ) )
+          dispatch( sendTxNotification( txid, null, type ) )
         }
         // showSendSuccessBottomSheet()
         setSuccess( true )
