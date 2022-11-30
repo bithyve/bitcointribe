e8b2d73d1cfe9104ddb502e72a141ff24e76c855
Akshay Chavan <akshaychavan8551@gmail.com>
2022-11-30 16:20:12 +0530

    minor

--- src/pages/Contacts/AddContactSendRequest.tsx
+++ src/pages/Contacts/AddContactSendRequest.tsx
@@ -508,6 +508,7 @@ export default function AddContactSendRequest( props ) {
           onPress={() => setSecure2FAModal( true )}
           style={{
             flex: 1,
+            margin: 3
           }}>
           <BottomInfoBox
             icon={true}
