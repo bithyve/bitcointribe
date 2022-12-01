9ae1e7e693cf8ab0f35e064aa66040b53cb68536
Akshay Chavan <akshaychavan8551@gmail.com>
2022-12-01 15:16:38 +0530

    change statusbar content style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -455,7 +455,7 @@ export default function Login( props ) {
       flex: 1,
       backgroundColor: Colors.blue
     } }>
-      <StatusBar backgroundColor={Colors.white} barStyle="light-content"/>
+      <StatusBar barStyle="light-content"/>
       <View style={{
         flex: 1
       }}>
