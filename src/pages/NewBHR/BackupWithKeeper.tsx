7afb1440bfeb3adcff1bb8ed18c3715b030c3917
Shashank Shinde <shashank@cakesofttech.com>
2023-02-09 13:41:45 +0530

    minor

--- src/pages/NewBHR/BackupWithKeeper.tsx
+++ src/pages/NewBHR/BackupWithKeeper.tsx
@@ -47,7 +47,7 @@ export default function BackupWithKeeper( { navigation } ) {
     setSeed( primaryMnemonic )
     const path = AccountUtilities.getDerivationPath( Config.NETWORK_TYPE, AccountType.CHECKING_ACCOUNT, 0 )
     setPath( path )
-    const url = `keeperdev://backup/${Buffer.from( `&seed=${primaryMnemonic.replace( / /g, ',' )}&path=${path}&name=hexa&appId=hexa`, 'utf8' ).toString(
+    const url = `keeperdev://backup/${Buffer.from( `&seed=${primaryMnemonic.replace( / /g, ',' )}&path=${path}&name=hexa&appId=hexadev`, 'utf8' ).toString(
       'base64',
     )}`
     setDeeplinkUrl( url )
