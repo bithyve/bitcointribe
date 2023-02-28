908765624aba175c1d69030feb2699ad05b2c7d6
Shashank Shinde <shashank@cakesofttech.com>
2023-02-28 18:12:39 +0530

    backup with keeper removed

--- src/pages/MoreOptions/MoreOptionsContainerScreen.tsx
+++ src/pages/MoreOptions/MoreOptionsContainerScreen.tsx
@@ -78,7 +78,7 @@ const MoreOptionsContainerScreen: React.FC<Props> = ( { navigation }: Props ) =>
           ? 'Wallet backup confirmed'
           :'Confirm backup phrase',
       title: bhrStrings[ 'WalletBackup' ],
-      screenName: 'BackupMethods',
+      screenName: 'WalletBackup',
     },
     {
       title: strings.accountManagement,
@@ -201,7 +201,7 @@ const MoreOptionsContainerScreen: React.FC<Props> = ( { navigation }: Props ) =>
     if ( typeof menuOption.onOptionPressed === 'function' ) {
       menuOption.onOptionPressed()
     } else if ( menuOption.screenName !== undefined ) {
-      if( menuOption.screenName == 'BackupMethods' ) {
+      if( menuOption.screenName == 'WalletBackup' ) {
         if( levelData[ 0 ].keeper1ButtonText?.toLowerCase() == 'seed'||
         levelData[ 0 ].keeper1ButtonText?.toLowerCase() == 'write down seed-words' ){
           if ( ( levelHealth.length == 0 ) ||
@@ -217,7 +217,7 @@ const MoreOptionsContainerScreen: React.FC<Props> = ( { navigation }: Props ) =>
               selectedKeeper: navigationObj?.selectedKeeper,
               selectedLevelId: levelData[ 0 ].id
             }
-            navigation.navigate( 'BackupMethods', navigationParams )
+            navigation.navigate( 'WalletBackup', navigationParams )
           } else {
             setSelectedKeeper( levelData[ 0 ].keeper1 )
             dispatch( onPressKeeper( levelData[ 0 ], 1 ) )
