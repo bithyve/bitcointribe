c82e807d421173618b0e7aaa4c64289dadabf00b
Pawan <pawan.git92@gmail.com>
2023-05-08 23:43:21 +0530

    minor Fixes for UI & messages

---
+++ src/common/CommonFunctions/BackUpMessage.ts
@@ -0,0 +1,28 @@
+import BackupWithKeeperState from '../data/enums/BackupWithKeeperState'
+import CreateWithKeeperState from '../data/enums/CreateWithKeeperState'
+
+export function backUpMessage( days, levelData, createWithKeeperStatus,
+  backupWithKeeperStatus ): string {
+  console.log( 'SKK LEVEL DATA   ' + JSON.stringify( levelData ) )
+  console.log( 'SKK LEVEL createWithKeeperStatus   ' + JSON.stringify( createWithKeeperStatus ) )
+  console.log( 'SKK LEVEL backupWithKeeperStatus   ' + JSON.stringify( backupWithKeeperStatus ) )
+
+  if( days > 180 )
+    return 'Wallet backup phrase is expired'
+  if ( days > 150 )
+    return 'Wallet backup phrase will expire soon'
+  if( ( createWithKeeperStatus == CreateWithKeeperState.BACKEDUP ||
+      backupWithKeeperStatus == BackupWithKeeperState.BACKEDUP ) &&
+      levelData[ 0 ].keeper1ButtonText?.toLowerCase() == 'seed' &&
+    levelData[ 0 ].status != 'notSetup' )
+    return 'Your wallet is backup with keeper & backup Phrase confirmed'
+  if( createWithKeeperStatus == CreateWithKeeperState.BACKEDUP ||
+    backupWithKeeperStatus == BackupWithKeeperState.BACKEDUP )
+    return 'Your wallet is backed up with Keeper'
+  // if( levelData[ 0 ].keeper1.shareType == '' )
+  //   return 'Confirm Backup Phrase'
+  if ( levelData[ 0 ].keeper1ButtonText?.toLowerCase() == 'seed' &&
+    levelData[ 0 ].status != 'notSetup' )
+    return 'Wallet backup confirmed'
+  return 'Backup to safeguard your wallet'
+}
