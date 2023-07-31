2ff683f56b52d2148529f6ea8b81a11680e21e6c
Raihan Khan <iamrk98@gmail.com>
2023-07-31 15:05:08 +0530

    added some external packages

--- ingest.py
+++ ingest.py
@@ -1,15 +1,9 @@
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-from urls import urls
-from dotenv import load_dotenv
-import os
-
 
 def create_vectorstore(url, OPENAI_API_KEY):
+    from langchain.document_loaders import UnstructuredURLLoader
+    from langchain.text_splitter import CharacterTextSplitter
+    from langchain.vectorstores import FAISS
+    from langchain.embeddings import OpenAIEmbeddings
     openai_api_key = OPENAI_API_KEY
     loaders = UnstructuredURLLoader(urls=url)
     data = loaders.load()
@@ -19,17 +13,7 @@ def create_vectorstore(url, OPENAI_API_KEY):
                                           chunk_overlap=200)
 
     docs = text_splitter.split_documents(data)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     doc_vectorstore = FAISS.from_documents(docs, embeddings)
+    print("Vectorstore created successfully!")
     return doc_vectorstore
-
-
-def test_vectorstore(question: str):
-    llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
-    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-    print(chain({"question": f"{question}"}, return_only_outputs=True))
-
-
-if __name__ == "__main__":
-    vectorstore = create_vectorstore("vectorstore")
-    test_vectorstore("What is Worldcoin?")
