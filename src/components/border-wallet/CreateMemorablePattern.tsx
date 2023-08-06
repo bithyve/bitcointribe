a5c4a184a1ac591b80046a760a18d3a0396ce8d9
akshay8551 <akshaychavan8551@gmail.com>
2023-08-06 20:09:56 +0530

    Created dir for components/pages

---
+++ src/components/border-wallet/CreateMemorablePattern.tsx
@@ -0,0 +1,180 @@
+import React, { useRef } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+import Colors from '../../common/Colors'
+import Fonts from '../../common/Fonts'
+import { RFValue } from 'react-native-responsive-fontsize'
+import { AppBottomSheetTouchableWrapper } from '../AppBottomSheetTouchableWrapper'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import { ScrollView } from 'react-native-gesture-handler'
+import { translations } from '../../common/content/LocContext'
+import EntropyGridIllustration from '../../assets/images/svgs/generateGridEntropyIllustration.svg'
+
+export default function CreateMemorablePattern( props ) {
+  const scrollViewRef = useRef<ScrollView>()
+  const strings  = translations[ 'accounts' ]
+
+  return (
+    <View style={{
+      ...styles.modalContainer, ...props.containerStyle
+    }}>
+      <View style={{
+        height: hp( 78 )
+      }}>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => {
+            props.closeModal()
+          }}
+          style={{
+            width: wp( 7 ), height: wp( 7 ), borderRadius: wp( 7/2 ),
+            alignSelf: 'flex-end',
+            backgroundColor: Colors.white, alignItems: 'center', justifyContent: 'center',
+            marginTop: wp( 3 ), marginRight: wp( 3 ), marginBottom:wp( -4 ), zIndex:999
+          }}
+        >
+          <FontAwesome name="close" color={Colors.blue} size={19} style={{
+          // marginTop: hp( 0.5 )
+          }} />
+        </TouchableOpacity>
+        <AppBottomSheetTouchableWrapper
+          style={{
+            justifyContent: 'center', alignItems: 'center', marginTop: 10
+          }}
+          activeOpacity={10}
+          onPress={() => props.titleClicked && props.titleClicked()}
+        >
+          <Text style={styles.headerText}>Create a Memorable Pattern</Text>
+
+        </AppBottomSheetTouchableWrapper>
+        <ScrollView
+          ref={scrollViewRef}
+          style={{
+            flex: 1,
+            backgroundColor: Colors.blue,
+          }}
+          snapToInterval={hp( '70%' )}
+          decelerationRate="fast"
+        >
+          <View style={styles.ElementView}>
+            <Text
+              style={{
+                ...styles.infoText,
+                marginTop: wp( '2%' ),
+              }}
+            >
+            You now need to create a <Text style={styles.boldText}>memorable pattern</Text> to apply onto your <Text style={styles.boldText}>Entropy Grid</Text> in order to generate your Border Wallet.
+            </Text>
+            <View style={{
+              justifyContent: 'center', alignItems: 'center'
+            }}>
+              <EntropyGridIllustration />
+            </View>
+            <Text
+              style={{
+                ...styles.infoText,
+              }}
+            >
+              Your pattern must comprise <Text style={styles.boldText}>11 cells</Text>, selected in your own <Text style={styles.boldText}>preferred order</Text>
+            </Text>
+            <Text
+              style={{
+                ...styles.infoText,
+              }}
+            >
+           Border Wallet to be recovered at a future date, you will need to be able to recall your pattern, so choose it carefully
+            </Text>
+            <Text
+              style={{
+                ...styles.infoText,
+              }}
+            >
+           The row numbers and column letters will assist you.
+            </Text>
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  )
+}
+const styles = StyleSheet.create( {
+  modalContainer: {
+    // height: '100%',
+    backgroundColor: Colors.blue,
+    alignSelf: 'center',
+    width: '100%',
+  },
+  headerText: {
+    color: Colors.backgroundColor1,
+    fontFamily: Fonts.Regular,
+    fontSize: RFValue( 18 ),
+    letterSpacing: 0.54,
+    marginTop: hp( '1%' ),
+    marginBottom: hp( '1%' ),
+    textAlign: 'center'
+  },
+  headerSeparator: {
+    backgroundColor: Colors.homepageButtonColor,
+    height: 1,
+    marginLeft: wp( '5%' ),
+    marginRight: wp( '5%' ),
+    marginBottom: hp( '1%' ),
+  },
+  infoText: {
+    textAlign: 'center',
+    color: Colors.backgroundColor1,
+    fontSize: RFValue( 12 ),
+    letterSpacing: 0.6,
+    fontFamily: Fonts.Regular,
+    marginHorizontal: wp( '10%' ),
+  },
+  clickHereText: {
+    color: Colors.backgroundColor1,
+    fontSize: RFValue( 13 ),
+    fontFamily: Fonts.Regular,
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+  toKnowMoreText: {
+    color: Colors.backgroundColor1,
+    fontSize: RFValue( 13 ),
+    fontFamily: Fonts.Regular,
+  },
+  linkView: {
+    flexDirection: 'row',
+    marginLeft: wp( '10%' ),
+    marginRight: wp( '10%' ),
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  ElementView: {
+    height: hp( '60%' ),
+    justifyContent: 'space-between',
+  },
+  separatorView: {
+    width: wp( '70%' ),
+    height: 0,
+    alignSelf: 'center',
+    marginBottom: wp( '1%' ),
+    borderStyle: 'dotted',
+    borderWidth: 1,
+    borderRadius: 1,
+    borderColor: Colors.white,
+  },
+  helperImage: {
+    width: wp( '80%' ),
+    height: wp( '60%' ),
+    resizeMode: 'contain',
+  },
+  bottomLinkView: {
+    marginLeft: wp( '10%' ),
+    marginRight: wp( '10%' ),
+    marginBottom: wp( '15%' ),
+  },
+  boldText:{
+    fontWeight: 'bold'
+  }
+} )
---
+++ src/pages/BorderWallet/CreateWithBorderWallet.tsx
@@ -0,0 +1,197 @@
+import React, { useState, useEffect, useCallback, createRef } from 'react'
+import {
+  View,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+import Colors from '../../common/Colors'
+import _ from 'underscore'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import SeedHeaderComponent from '../NewBHR/SeedHeaderComponent'
+import SeedPageComponent from '../NewBHR/SeedPageComponent'
+import { useDispatch } from 'react-redux'
+import { setSeedBackupHistory, updateSeedHealth } from '../../store/actions/BHR'
+import AlertModalContents from '../../components/AlertModalContents'
+import RNPreventScreenshot from 'react-native-screenshot-prevent'
+import dbManager from '../../storage/realm/dbManager'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import moment from 'moment'
+import BottomInfoBox from '../../components/BottomInfoBox'
+import LinearGradient from 'react-native-linear-gradient'
+import deviceInfoModule from 'react-native-device-info'
+import { hp, wp } from '../../common/data/responsiveness/responsive'
+import ModalContainer from '../../components/home/ModalContainer'
+import CreateMemorablePattern from '../../components/border-wallet/CreateMemorablePattern'
+
+const CreateWithBorderWallet = ( props ) => {
+  const [ headerTitle, setHeaderTitle ]=useState( 'Generate New Entropy Grid' )
+  const [ generateEntropyGrid, setGenerateEntropyGrid ] = useState( false )
+  const DATA = [
+    {
+      id: '01',
+      title: 'soup',
+    },
+    {
+      id: '02',
+      title: 'example',
+    },
+    {
+      id: '03',
+      title: 'crater',
+    },
+    {
+      id: '04',
+      title: 'canyon',
+    },
+    {
+      id: '05',
+      title: 'air',
+    },
+    {
+      id: '06',
+      title: 'tiger',
+    },
+    {
+      id: '07',
+      title: 'either',
+    },
+    {
+      id: '08',
+      title: 'repair',
+    },
+    {
+      id: '09',
+      title: 'warfare',
+    },
+    {
+      id: '10',
+      title: 'blind',
+    },
+    {
+      id: '11',
+      title: 'permit',
+    },
+    {
+      id: '12',
+      title: 'art',
+    }
+  ]
+  type ItemProps = {title: string, id: string};
+
+  const Item = ( { title, id }: ItemProps ) => (
+    <View style={styles.item}>
+      <View style={[ styles.indexWrapper ]}>
+        <Text style={styles.gridItemIndex}>{id}</Text>
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  )
+
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1, backgroundColor: Colors.backgroundColor
+      }}
+    >
+      <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+      <SeedHeaderComponent
+        onPressBack={() => {
+          // RNPreventScreenshot.enabled( false )
+          props.navigation.goBack()
+          // props.navigation.navigate( 'Home' )
+        }}
+        info1={'Step 1 of Creating a Border Wallet'}
+        info={'Note down these 12 word Regeneration Mnemonic'}
+        selectedTitle={headerTitle}
+      />
+
+      <FlatList
+        data={DATA}
+        renderItem={( { item } ) => <Item title={item.title} id={item.id} />}
+        keyExtractor={item => item.id}
+        numColumns={2}
+      />
+      <BottomInfoBox
+        backgroundColor={Colors.white}
+        title={'Note'}
+        infoText={'Treat these words & grid with the same degree of security that you would a Bitcoin seed phrase'}
+      />
+      <View style={styles.bottomButtonView}>
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              setGenerateEntropyGrid( true )
+            }}
+          >
+            <LinearGradient colors={[ Colors.blue, Colors.darkBlue ]}
+              start={{
+                x: 0, y: 0
+              }} end={{
+                x: 1, y: 0
+              }}
+              locations={[ 0.2, 1 ]}
+              style={styles.buttonView}
+            >
+              <Text style={styles.buttonText}>Generate Grid</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ModalContainer onBackground={() =>setGenerateEntropyGrid( false )}
+        visible={generateEntropyGrid}
+        closeBottomSheet={() => { }}>
+        <CreateMemorablePattern closeModal={() => setGenerateEntropyGrid( false )}/>
+      </ModalContainer>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create( {
+  item: {
+    flexDirection: 'row',
+    width: '42%',
+    backgroundColor: '#F8F8F8',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center'
+  },
+  indexWrapper: {
+    width: '30%'
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#717171'
+  },
+  gridItemIndex: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.blue
+  },
+  buttonView: {
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: Colors.blue,
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: RFValue( 13 ),
+    // fontFamily: Fonts.Medium,
+  },
+  bottomButtonView: {
+    flexDirection: 'row',
+    paddingHorizontal: hp( 6 ),
+    paddingBottom: deviceInfoModule.hasNotch() ? hp( 4 ) : hp( 3 ),
+    justifyContent: 'flex-end'
+  },
+} )
+export default CreateWithBorderWallet
