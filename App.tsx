2b0cc73eecb01ff54cf99eaf9fd6d02212de8985
PraneethGunas <praneethgunas404@gmail.com>
2023-09-04 21:03:31 +0530

    React native upgrade 0.71.x

--- App.tsx
+++ App.tsx
@@ -1,60 +1,60 @@
-import React, { useEffect, useState, useMemo, useCallback } from 'react'
-import Navigator from './src/navigation/Navigator'
-import { Provider } from 'react-redux'
-import { Provider as MobxProvider } from 'mobx-react'
-import Stores from './src/mobxstore'
-import makeStore from './src/store'
-import NoInternetModalContents from './src/components/NoInternetModalContents'
-import { useDispatch } from 'react-redux'
-import NetInfo from '@react-native-community/netinfo'
-import { getVersion, getBuildId, getBuildNumber } from 'react-native-device-info'
-import { setApiHeaders } from './src/services/api'
-import firebase from '@react-native-firebase/app'
-import analytics from '@react-native-firebase/analytics'
-import crashlytics from '@react-native-firebase/crashlytics'
-import { updatePreference } from './src/store/actions/preferences'
-import usePreferencesState from './src/utils/hooks/state-selectors/preferences/UsePreferencesState'
-import { BottomSheetModalProvider, useBottomSheetModal } from '@gorhom/bottom-sheet'
-import defaultBottomSheetConfigs from './src/common/configs/BottomSheetConfigs'
-import getActiveRouteName from './src/utils/navigation/GetActiveRouteName'
-import { LogBox } from 'react-native'
-import ModalContainer from './src/components/home/ModalContainer'
-import { RootSiblingParent } from 'react-native-root-siblings'
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
+import Navigator from './src/navigation/Navigator';
+import { Provider } from 'react-redux';
+import { Provider as MobxProvider } from 'mobx-react';
+import Stores from './src/mobxstore';
+import makeStore from './src/store';
+import NoInternetModalContents from './src/components/NoInternetModalContents';
+import { useDispatch } from 'react-redux';
+import NetInfo from '@react-native-community/netinfo';
+import {
+  getVersion,
+  getBuildId,
+  getBuildNumber,
+} from 'react-native-device-info';
+import { setApiHeaders } from './src/services/api';
+import { updatePreference } from './src/store/actions/preferences';
+import usePreferencesState from './src/utils/hooks/state-selectors/preferences/UsePreferencesState';
+import {
+  BottomSheetModalProvider,
+  useBottomSheetModal,
+} from '@gorhom/bottom-sheet';
+import defaultBottomSheetConfigs from './src/common/configs/BottomSheetConfigs';
+import getActiveRouteName from './src/utils/navigation/GetActiveRouteName';
+import { LogBox } from 'react-native';
+import ModalContainer from './src/components/home/ModalContainer';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
-import { LocalizationProvider } from './src/common/content/LocContext'
-LogBox.ignoreAllLogs( true )
+import { LocalizationProvider } from './src/common/content/LocContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+LogBox.ignoreAllLogs(true);
 
-export const URI_PREFIX = 'hexa://'
+export const URI_PREFIX = 'hexa://';
 
 async function configureAPIHeaders() {
-  const version = await getVersion()
-  const buildNumber = getBuildNumber()
+  const version = await getVersion();
+  const buildNumber = getBuildNumber();
 
-  setApiHeaders( {
-    appVersion: version, appBuildNumber: buildNumber
-  } )
+  setApiHeaders({
+    appVersion: version,
+    appBuildNumber: buildNumber,
+  });
 }
 
 export default function AppWrapper() {
-
   // Creates and holds an instance of the store so only children in the `Provider`'s
   // context can have access to it. (see: https://stackoverflow.com/a/60329482/8859365)
-  const store = makeStore()
+  const store = makeStore();
 
-  function updare() {
+  function updare() {}
 
-  }
-
-  useEffect( () => {
-    ( async () => {
-      configureAPIHeaders()
-      await firebase.analytics().setAnalyticsCollectionEnabled( true )
-      await crashlytics().setCrashlyticsCollectionEnabled( true )
-    } )()
-  }, [] )
+  useEffect(() => {
+    (async () => {
+      configureAPIHeaders();
+    })();
+  }, []);
 
   return (
-
     <MobxProvider
       SettingsStore={Stores.settingsStore}
       BalanceStore={Stores.walletStore}
@@ -67,50 +67,58 @@ export default function AppWrapper() {
       PaymentsStore={Stores.paymentsStore}
       FeeStore={Stores.feeStore}
       UTXOsStore={Stores.utxosStore}
-      ActivityStore={Stores.activityStore}
-    >
-      <RootSiblingParent>
-        <Provider store={store} uriPrefix={URI_PREFIX}>
-          <BottomSheetModalProvider>
-            <LocalizationProvider>
-              <AppContent />
-            </LocalizationProvider>
-          </BottomSheetModalProvider>
-        </Provider>
-      </RootSiblingParent>
-
+      ActivityStore={Stores.activityStore}>
+      <GestureHandlerRootView
+        style={{
+          flex: 1,
+        }}>
+        <RootSiblingParent>
+          <Provider store={store} uriPrefix={URI_PREFIX}>
+            <BottomSheetModalProvider>
+              <LocalizationProvider>
+                <AppContent />
+              </LocalizationProvider>
+            </BottomSheetModalProvider>
+          </Provider>
+        </RootSiblingParent>
+      </GestureHandlerRootView>
     </MobxProvider>
-  )
+  );
 }
 
-
 function AppContent() {
-  const dispatch = useDispatch()
-  const { present: presentBottomSheet, dismiss: dismissBottomSheet } = useBottomSheetModal()
-  const [ noInternetModal, showNoInternetModal ] = useState( false )
-  const preferencesState = usePreferencesState()
-  const [ previousScreenName, setPreviousScreenName ] = useState<string | null>()
-  const [ currentScreenName, setCurrentScreenName ] = useState<string | null>()
-  const forceUpdate = useState()[ 1 ].bind( null, {
-  } )
+  const dispatch = useDispatch();
+  const { present: presentBottomSheet, dismiss: dismissBottomSheet } =
+    useBottomSheetModal();
+  const [noInternetModal, showNoInternetModal] = useState(false);
+  const preferencesState = usePreferencesState();
+  const [previousScreenName, setPreviousScreenName] = useState<string | null>();
+  const [currentScreenName, setCurrentScreenName] = useState<string | null>();
+  const forceUpdate = useState()[1].bind(null, {});
 
   function update() {
-    forceUpdate()
+    forceUpdate();
   }
-  const canShowNoInternetWarning = useMemo( () => {
+  const canShowNoInternetWarning = useMemo(() => {
     return (
       currentScreenName != 'Login' &&
       currentScreenName != 'Launch' &&
       currentScreenName != 'ReLogin' &&
       preferencesState.hasShownNoInternetWarning === false
-    )
-  }, [ previousScreenName, currentScreenName, preferencesState.hasShownNoInternetWarning ] )
+    );
+  }, [
+    previousScreenName,
+    currentScreenName,
+    preferencesState.hasShownNoInternetWarning,
+  ]);
 
   async function resetInternetWarningFlag() {
-    await dispatch( updatePreference( {
-      key: 'hasShownNoInternetWarning',
-      value: false,
-    } ) )
+    await dispatch(
+      updatePreference({
+        key: 'hasShownNoInternetWarning',
+        value: false,
+      }),
+    );
   }
 
   // const showNoInternetWarning = useCallback( () => {
@@ -129,61 +137,60 @@ function AppContent() {
   // }, [ presentBottomSheet, dismissBottomSheet ] )
 
   function setupInternetWarningListener() {
-    return NetInfo.addEventListener( ( state ) => {
+    return NetInfo.addEventListener(state => {
       if (
         state.isInternetReachable == null ||
         canShowNoInternetWarning == false
       ) {
-        return
+        return;
       }
 
-      if ( state.isInternetReachable ) {
-        showNoInternetModal( false )
+      if (state.isInternetReachable) {
+        showNoInternetModal(false);
       } else {
-        showNoInternetModal( true )
+        showNoInternetModal(true);
       }
-    } )
+    });
   }
 
-  useEffect( () => {
+  useEffect(() => {
     return () => {
       // reset when the app component unmounts
-      resetInternetWarningFlag()
-    }
-  }, [] )
+      resetInternetWarningFlag();
+    };
+  }, []);
 
-  useEffect( () => {
-    const unsubscribe = setupInternetWarningListener()
+  useEffect(() => {
+    const unsubscribe = setupInternetWarningListener();
 
     return () => {
-      unsubscribe()
-    }
-  }, [] )
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <>
       <Navigator
-        onNavigationStateChange={async ( prevState, currentState ) => {
-          setPreviousScreenName( getActiveRouteName( prevState ) )
-          setCurrentScreenName( getActiveRouteName( currentState ) )
-
-          if ( previousScreenName !== currentScreenName ) {
-            analytics().logEvent( currentScreenName )
-          }
+        onNavigationStateChange={async (prevState, currentState) => {
+          setPreviousScreenName(getActiveRouteName(prevState));
+          setCurrentScreenName(getActiveRouteName(currentState));
         }}
       />
-      <ModalContainer visible={noInternetModal} closeBottomSheet={() => {showNoInternetModal( false )}} >
+      <ModalContainer
+        visible={noInternetModal}
+        closeBottomSheet={() => {
+          showNoInternetModal(false);
+        }}>
         <NoInternetModalContents
           onPressTryAgain={() => {
-            showNoInternetModal( false )
+            showNoInternetModal(false);
           }}
           onPressIgnore={() => {
-            resetInternetWarningFlag()
-            showNoInternetModal( false )
+            resetInternetWarningFlag();
+            showNoInternetModal(false);
           }}
         />
       </ModalContainer>
     </>
-  )
+  );
 }
-
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 rn-nodeify --install buffer,events,process,stream,util,inherits,fs,path,assert,crypto --hack --yarn
-cd ios && pod install
+cd ios && RCT_NEW_ARCH_ENABLED=0 pod install
 cd ../android && touch local.properties && echo "sdk.dir = /Users/$(whoami)/Library/Android/sdk" > local.properties
