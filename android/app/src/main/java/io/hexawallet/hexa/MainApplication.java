368aa34e70bc3fd6368a1990732691b1d8e78ff4
PraneethGunas <praneethgunas404@gmail.com>
2023-09-04 21:03:31 +0530

    React native upgrade 0.71.x

--- android/app/src/main/java/io/hexawallet/hexa/MainApplication.java
+++ android/app/src/main/java/io/hexawallet/hexa/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
@@ -31,21 +33,13 @@ import java.util.List;
 import io.hexawallet.hexa.PdfPasswordPackage;
 import io.hexawallet.hexa.GoogleDrivePackage; 
 
-import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactInstanceManager;
-import java.lang.reflect.InvocationTargetException;
 
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
-    // @Override
-    // public String getFileProviderAuthority() {
-    //     return BuildConfig.APPLICATION_ID + ".provider";
-    // }
-
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
             return BuildConfig.DEBUG;
@@ -55,8 +49,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             @SuppressWarnings("UnnecessaryLocalVariable")
             List<ReactPackage> packages = new PackageList(this).getPackages();
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
 
             packages.add(new PdfPasswordPackage());
             packages.add(new GoogleDrivePackage());
@@ -73,6 +65,15 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
             return "index";
         }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+        @Override
+        protected Boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
     };
 
     @Override
@@ -84,37 +85,10 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    }
-
-    /**
-    * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-    * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    *
-    * @param context
-    * @param reactInstanceManager
-    */
-    private static void initializeFlipper(
-        Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-        try {
-            /*
-            We use reflection here to pick up the class that initializes Flipper,
-            since Flipper library is not available in release mode
-            */
-            Class<?> aClass = Class.forName("io.hexawallet.hexa.ReactNativeFlipper");
-            aClass
-                .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                .invoke(null, context, reactInstanceManager);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        // If you opted-in for the New Architecture, we load the native entry point for this app.
+        DefaultNewArchitectureEntryPoint.load();
         }
-        }
+        ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 }
--- ios/HEXA/AppDelegate.h
+++ ios/HEXA/AppDelegate.h
@@ -1,11 +1,5 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-
-//react-native-push-notification-ios
 #import <UserNotifications/UNUserNotificationCenter.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+@interface AppDelegate : RCTAppDelegate
 @end
