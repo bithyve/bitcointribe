c151d892ac7ddb37d35725ed6f0863d5d19c1678
akshay8551 <akshaychavan8551@gmail.com>
2023-09-19 11:14:36 +0530

    Added cancel btn for NFC modal #6784

--- src/pages/Gift/GiftScreen.tsx
+++ src/pages/Gift/GiftScreen.tsx
@@ -132,7 +132,7 @@ class GiftScreen extends React.Component<
       showGiftModal: false,
       showGiftFailureModal: false,
       cardDetails: null,
-      showNFCModal: false,
+      showNFCModal: true,
       satCardBalance: 0,
       showAlertModal: false,
       errorMessage: '',
@@ -770,7 +770,9 @@ class GiftScreen extends React.Component<
             closeModal
           />
         </ModalContainer>
-        <NfcPrompt visible={this.state.showNFCModal} />
+        <NfcPrompt visible={this.state.showNFCModal} close={()=>this.setState( {
+          showNFCModal: false
+        } )}/>
         <ModalContainer onBackground={this.onClaimClose} visible={this.state.claimVerification} closeBottomSheet={this.onClaimClose}  >
           <ClaimSatComponent
             title={'Claim SATSCARD™'}
