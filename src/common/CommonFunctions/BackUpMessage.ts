32d755db1db47b71583892131ca802842c9a1a20
akshay8551 <akshaychavan8551@gmail.com>
2023-12-26 18:16:28 +0530

    Backup Confirmed message #6941

--- src/common/CommonFunctions/BackUpMessage.ts
+++ src/common/CommonFunctions/BackUpMessage.ts
@@ -12,7 +12,7 @@ export function backUpMessage( days, levelData, createWithKeeperStatus,
       backupWithKeeperStatus == BackupWithKeeperState.BACKEDUP ) &&
       levelData[ 0 ].keeper1ButtonText?.toLowerCase() == 'seed' &&
     levelData[ 0 ].keeper1.status != 'notSetup' )
-    return 'Your wallet is backup with keeper & backup Phrase confirmed'
+    return 'Recovery Phrase confirmed. Your wallet is backed-up with Bitcoin Keeper.'
   if( createWithKeeperStatus == CreateWithKeeperState.BACKEDUP ||
     backupWithKeeperStatus == BackupWithKeeperState.BACKEDUP )
     return 'Your wallet is backed up with Keeper'
