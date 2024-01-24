a1118951a1b7e56480e01e1237e4e26067a1ddb8
akshay8551 <akshaychavan8551@gmail.com>
2024-01-24 15:06:03 +0530

    Done - UI modification

--- src/components/rgb/RGBIntroModal.tsx
+++ src/components/rgb/RGBIntroModal.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { RFValue } from 'react-native-responsive-fontsize'
 import {
@@ -9,71 +9,46 @@ import MemoriableIllustration from '../../assets/images/svgs/MemoriableIllustrat
 import Colors from '../../common/Colors'
 import { translations } from '../../common/content/LocContext'
 import Fonts from '../../common/Fonts'
-import { AppBottomSheetTouchableWrapper } from '../AppBottomSheetTouchableWrapper'
 import BounceLoader from '../loader/BounceLoader'
 
-export default function RGBIntroModal( props ) {
+export default function RGBIntroModal(props) {
   const scrollViewRef = useRef<ScrollView>()
-  const strings  = translations[ 'accounts' ]
+  const strings = translations['accounts']
 
   return (
     <View style={{
       ...styles.modalContainer, ...props.containerStyle
     }}>
       <View style={{
-        height: hp( 82 )
+        height: hp(55)
       }}>
-        <AppBottomSheetTouchableWrapper
-          style={{
-            justifyContent: 'center', alignItems: 'center', marginTop: 10
-          }}
-          activeOpacity={10}
-        >
+        <View style={{ marginVertical: hp(1) }}>
           <Text style={styles.headerText}>{props.title}</Text>
+        </View>
 
-        </AppBottomSheetTouchableWrapper>
-        <ScrollView
-          ref={scrollViewRef}
-          style={{
-            flexGrow: 1,
-            backgroundColor: Colors.blue,
-          }}
-          snapToInterval={hp( '70%' )}
-          decelerationRate="fast"
-        >
-          <View style={styles.ElementView}>
-            <Text
-              style={{
-                ...styles.infoText,
-                marginTop: wp( '2%' ),
-              }}
-            >
+        <View style={{ marginVertical: hp(1) }}>
+          <Text
+            style={{
+              ...styles.infoText,
+            }}
+          >
             {props.info}
-            </Text>
-            <ImageBackground source={require( '../../assets/images/illustrationbackground.png' )}
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-              resizeMode='contain'
-            >
-              <View style={{
-                justifyContent: 'center', alignItems: 'center'
-              }}>
-                <MemoriableIllustration />
-              </View>
-            </ImageBackground>
-            <Text
-              style={{
-                ...styles.infoText,
-              }}
-            >
-             {props.otherText}
-            </Text>
-          </View>
-        </ScrollView>
+          </Text>
+        </View>
+        <View style={{ alignItems: 'center', marginVertical: hp(1) }}>
+          <MemoriableIllustration />
+        </View>
+        <View style={{ marginVertical: hp(1) }}>
+          <Text
+            style={{
+              ...styles.infoText,
+            }}
+          >
+            {props.otherText}
+          </Text>
+        </View>
         <View>
-        <View style={styles.modalMessageWrapper}>
+          <View style={styles.modalMessageWrapper}>
             <View style={{ width: '80%' }}>
               <Text style={styles.modalMessageText}>
                 This step will take a few seconds. You would be able to proceed soon
@@ -109,7 +84,7 @@ export default function RGBIntroModal( props ) {
     </View>
   )
 }
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
   modalContainer: {
     backgroundColor: Colors.blue,
     alignSelf: 'center',
@@ -118,26 +93,19 @@ const styles = StyleSheet.create( {
   headerText: {
     color: Colors.backgroundColor1,
     fontFamily: Fonts.Regular,
-    fontSize: RFValue( 18 ),
+    fontSize: RFValue(18),
     letterSpacing: 0.54,
-    marginTop: hp( '1%' ),
-    marginBottom: hp( '1%' ),
+    marginTop: hp('1%'),
+    marginBottom: hp('1%'),
     textAlign: 'center'
   },
   infoText: {
     textAlign: 'center',
     color: Colors.backgroundColor1,
-    fontSize: RFValue( 12 ),
+    fontSize: RFValue(12),
     letterSpacing: 0.6,
     fontFamily: Fonts.Regular,
-    marginHorizontal: wp( '8%' ),
-  },
-  ElementView: {
-    height: hp( '60%' ),
-    justifyContent: 'space-between',
-  },
-  boldText:{
-    fontWeight: 'bold'
+    marginHorizontal: wp('8%'),
   },
   buttonView: {
     padding: 10,
@@ -148,19 +116,19 @@ const styles = StyleSheet.create( {
   },
   buttonText: {
     color: Colors.blue,
-    fontSize: RFValue( 13 ),
+    fontSize: RFValue(13),
     fontFamily: Fonts.Medium,
   },
   modalMessageWrapper: {
     flexDirection: 'row',
     width: '100%',
     alignItems: 'center',
-    marginBottom: 10
+    marginVertical: 10
   },
   modalMessageText: {
     fontSize: 13,
     color: Colors.white,
     fontFamily: Fonts.Regular,
-    marginHorizontal: wp( '8%' ),
+    marginHorizontal: wp('8%'),
   },
-} )
+})
