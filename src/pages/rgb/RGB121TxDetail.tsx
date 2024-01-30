0b6d8237a3ba70d13fb71b8667989c2f375c09a3
cakesoft-shyam <shyam@cakesofttech.com>
2024-01-30 10:48:54 +0530

    Recent Transaction screen added for rgb asset

--- src/pages/rgb/RGB121TxDetail.tsx
+++ src/pages/rgb/RGB121TxDetail.tsx
@@ -127,9 +127,10 @@ export default function RGB121TxDetail( props ) {
       </TouchableOpacity>
     )
   }
-
   const onViewMorePressed = () => {
-
+    props.navigation.navigate( 'RGBTransactionsList', {
+      asset: asset
+    } )  
   }
 
   return (
@@ -198,7 +199,7 @@ export default function RGB121TxDetail( props ) {
         <View style={{
           flex: 1,
         }}>
-          {/* <View style={styles.viewMoreLinkRow}>
+          <View style={styles.viewMoreLinkRow}>
             <Text style={styles.headerDateText}>{accountStr.RecentTransactions}</Text>
             <TouchableOpacity
               onPress={onViewMorePressed}
@@ -217,7 +218,7 @@ export default function RGB121TxDetail( props ) {
                 </Text>
               </LinearGradient>
             </TouchableOpacity>
-          </View> */}
+          </View>
           <FlatList
             data={transactionData}
             renderItem={renderItem}
