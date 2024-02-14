ec9b47a166145a3797a94391dd7486bfe2cf59e4
akshay8551 <akshaychavan8551@gmail.com>
2024-02-14 12:17:55 +0530

    asset details screen changes #7083

--- src/pages/rgb/RGB121TxDetail.tsx
+++ src/pages/rgb/RGB121TxDetail.tsx
@@ -13,12 +13,12 @@ import {
   TouchableOpacity,
   View
 } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient'
 import { RFValue } from 'react-native-responsive-fontsize'
 import {
   widthPercentageToDP as wp
 } from 'react-native-responsive-screen'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import { useDispatch } from 'react-redux'
 import Colors from '../../common/Colors'
 import { LocalizationContext } from '../../common/content/LocContext'
@@ -31,49 +31,49 @@ import useAccountsState from '../../utils/hooks/state-selectors/accounts/UseAcco
 import SendAndReceiveButtonsFooter from '../Accounts/Details/SendAndReceiveButtonsFooter'
 import DetailsCard from './DetailsCard'
 
-export default function RGB121TxDetail( props ) {
+export default function RGB121TxDetail(props) {
   const dispatch = useDispatch()
-  const { translations } = useContext( LocalizationContext )
-  const accountStr = translations[ 'accounts' ]
+  const { translations } = useContext(LocalizationContext)
+  const accountStr = translations['accounts']
   const asset = props.route.params.asset
   const accountsState = useAccountsState()
   const { averageTxFees, exchangeRates } = accountsState
-  const [ loading, setLoading ] = useState( true )
-  const [ transactionData, setTransactionData ] = useState( [] )
+  const [loading, setLoading] = useState(true)
+  const [transactionData, setTransactionData] = useState([])
 
-  useEffect( () => {
+  useEffect(() => {
     getTransfers()
-  }, [] )
+  }, [])
 
   const getTransfers = async () => {
     try {
-      setLoading( true )
-      const txns = await RGBServices.getRgbAssetTransactions( asset.assetId )
-      setLoading( false )
-      if ( txns ) {
-        setTransactionData( txns )
+      setLoading(true)
+      const txns = await RGBServices.getRgbAssetTransactions(asset.assetId)
+      setLoading(false)
+      if (txns) {
+        setTransactionData(txns)
       } else {
         props.navigation.goBack()
       }
-    } catch ( error ) {
-      setLoading( false )
-      console.log( error )
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
       props.navigation.goBack()
     }
   }
 
 
-  useEffect( () => {
+  useEffect(() => {
     if (
       !averageTxFees ||
-      !Object.keys( averageTxFees ).length ||
+      !Object.keys(averageTxFees).length ||
       !exchangeRates ||
-      !Object.keys( exchangeRates ).length
+      !Object.keys(exchangeRates).length
     ) {
-      dispatch( fetchFeeRates() )
-      dispatch( fetchExchangeRates() )
+      dispatch(fetchFeeRates())
+      dispatch(fetchExchangeRates())
     }
-  }, [] )
+  }, [])
 
   const renderFooter = () => {
     return (
@@ -81,13 +81,13 @@ export default function RGB121TxDetail( props ) {
         <View style={styles.footerSection}>
           <SendAndReceiveButtonsFooter
             onSendPressed={() => {
-              props.navigation.navigate( 'RGBSendWithQR', {
+              props.navigation.navigate('RGBSendWithQR', {
                 asset
-              } )
+              })
             }}
             onReceivePressed={() => {
-              props.navigation.navigate( 'RGBReceive', {
-              } )
+              props.navigation.navigate('RGBReceive', {
+              })
             }}
             averageTxFees={averageTxFees}
             network={
@@ -100,37 +100,47 @@ export default function RGB121TxDetail( props ) {
     )
   }
 
-  const onItemClick = ( item ) => {
-    props.navigation.navigate( 'AssetTransferDetails', {
+  const onItemClick = (item) => {
+    props.navigation.navigate('AssetTransferDetails', {
       item, asset
-    } )
+    })
   }
 
-  const renderItem = ( { item } ) => {
+  const renderItem = ({ item }) => {
 
     return (
-      <TouchableOpacity style={styles.itemContainer} onPress={() => onItemClick( item )}>
+      <TouchableOpacity style={styles.itemContainer} onPress={() => onItemClick(item)}>
         <View style={styles.textContainer}>
           <Text style={styles.itemTitle}>{item.status}</Text>
-          <Text style={styles.itemDesc}>{moment.unix( item.createdAt ).format( 'DD/MM/YY • hh:MMa' )}</Text>
+          <Text style={styles.itemDesc}>{moment.unix(item.createdAt).format('DD/MM/YY • hh:MMa')}</Text>
         </View>
         <View style={styles.currencyContainer}>
           <Text
             numberOfLines={1}
-            style={[ styles.amountText, {
-              color: ( item.kind.toUpperCase() === 'RECEIVE_BLIND' || item.kind.toUpperCase() ==='ISSUANCE' || item.kind.toUpperCase() === 'RECEIVE_WITNESS' ) ? '#04A777' : '#FD746C'
-            } ]}
+            style={[styles.amountText, {
+              color: (item.kind.toUpperCase() === 'RECEIVE_BLIND' || item.kind.toUpperCase() === 'ISSUANCE' || item.kind.toUpperCase() === 'RECEIVE_WITNESS') ? '#04A777' : '#FD746C'
+            }]}
           >
             {item.amount}
           </Text>
+        </View>
+        <View style={{
+          width: '5%'
+        }}>
+          <Ionicons
+            name="chevron-forward"
+            color={Colors.Black}
+            size={15}
+            style={styles.forwardIcon}
+          />
         </View>
       </TouchableOpacity>
     )
   }
   const onViewMorePressed = () => {
-    props.navigation.navigate( 'RGBTransactionsList', {
+    props.navigation.navigate('RGBTransactionsList', {
       asset: asset
-    } )  
+    })
   }
 
   return (
@@ -168,28 +178,29 @@ export default function RGB121TxDetail( props ) {
         }}>
           <DetailsCard
             onKnowMorePressed={() => {
-              props.navigation.navigate( 'AssetMetaData', {
+              props.navigation.navigate('AssetMetaData', {
                 asset
-              } )
+              })
             }}
             showKnowMore
+            knowMoreText={'View details'}
             onSettingsPressed={() => { }}
             balance={asset.balance.settled}
             cardColor={'#B7B7B7'}
             title={asset.name}
             description={asset.description}
             assetId={asset.assetId}
-            renderIcon={() => <View style={[ styles.labelContainer, {
+            renderIcon={() => <View style={[styles.labelContainer, {
               backgroundColor: '#B7B7B7'
-            } ]}>
+            }]}>
               <Image style={{
                 height: 50, width: 50, borderRadius: 30
               }} source={{
-                uri: Platform.select( {
-                  android: `file://${asset.dataPaths[ 0 ].filePath}`,
-                  ios: asset.dataPaths[ 0 ].filePath
-                } )
-              }}/>
+                uri: Platform.select({
+                  android: `file://${asset.dataPaths[0].filePath}`,
+                  ios: asset.dataPaths[0].filePath
+                })
+              }} />
             </View>}
             isBitcoin={false}
           />
@@ -204,25 +215,17 @@ export default function RGB121TxDetail( props ) {
             <TouchableOpacity
               onPress={onViewMorePressed}
             >
-              <LinearGradient
-                start={{
-                  x: 0, y: 0
-                }} end={{
-                  x: 1, y: 0
-                }}
-                colors={[ Colors.skyBlue, Colors.darkBlue ]}
-                style={styles.viewMoreWrapper}
-              >
+              <View style={styles.viewMoreWrapper}>
                 <Text style={styles.headerTouchableText}>
-                  {accountStr.ViewMore}
+                  View All
                 </Text>
-              </LinearGradient>
+              </View>
             </TouchableOpacity>
           </View>
           <FlatList
             data={transactionData}
             renderItem={renderItem}
-            keyExtractor={( item, index ) => index.toString()}
+            keyExtractor={(item, index) => index.toString()}
           />
           {renderFooter()}
 
@@ -232,16 +235,16 @@ export default function RGB121TxDetail( props ) {
   )
 }
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
   headerTitleText: {
     color: Colors.blue,
-    fontSize: RFValue( 20 ),
+    fontSize: RFValue(20),
     fontFamily: Fonts.Regular,
     textAlign: 'center'
   },
   headerSubtitle: {
     color: 'gray',
-    fontSize: RFValue( 14 ),
+    fontSize: RFValue(14),
     fontFamily: Fonts.Regular,
     textAlign: 'center'
   },
@@ -253,13 +256,13 @@ const styles = StyleSheet.create( {
   },
   amountTextStyle: {
     fontFamily: Fonts.Regular,
-    fontSize: RFValue( 17 ),
+    fontSize: RFValue(17),
     color: 'gray'
   },
   amountText: {
     fontFamily: Fonts.Regular,
-    fontSize: RFValue( 14.5 ),
-    marginRight: wp( 1 ),
+    fontSize: RFValue(14.5),
+    marginRight: wp(1),
     // alignItems: 'baseline',
     // width:wp( 25 )
     color: Colors.textColorGrey
@@ -271,13 +274,13 @@ const styles = StyleSheet.create( {
     alignSelf: 'center',
   },
   currencyImage: {
-    width: wp( 3 ),
-    height: wp( 4 ),
+    width: wp(3),
+    height: wp(4),
     resizeMode: 'contain',
-    marginTop: wp( 0.3 )
+    marginTop: wp(0.3)
   },
   unitTextStyles: {
-    fontSize: RFValue( 11 ),
+    fontSize: RFValue(11),
     color: Colors.currencyGray,
     fontFamily: Fonts.Regular,
   },
@@ -289,20 +292,21 @@ const styles = StyleSheet.create( {
     marginHorizontal: 20,
     paddingVertical: 10,
     marginTop: 20,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   itemImage: {
     width: 35, height: 35, borderRadius: 20, backgroundColor: 'gray'
   },
   textContainer: {
-    flex: 1, marginStart: 10
+    flex: 1, 
   },
   itemTitle: {
-    fontFamily: Fonts.Regular, fontSize: RFValue( 13 ), textTransform: 'capitalize'
+    fontFamily: Fonts.Regular, fontSize: RFValue(13), textTransform: 'capitalize'
   },
   itemDesc: {
     color: Colors.textColorGrey, fontFamily: Fonts.Regular,
-    fontSize: RFValue( 10 ), marginTop: 2
+    fontSize: RFValue(10), marginTop: 2
   },
   balanceText: {
     fontSize: 18, fontFamily: Fonts.Medium,
@@ -324,13 +328,13 @@ const styles = StyleSheet.create( {
 
   headerDateText: {
     color: Colors.textColorGrey,
-    fontSize: RFValue( 13 ),
+    fontSize: RFValue(13),
     fontFamily: Fonts.Regular,
   },
 
   headerTouchableText: {
     color: Colors.white,
-    fontSize: RFValue( 12 ),
+    fontSize: RFValue(12),
     fontFamily: Fonts.Medium,
   },
   viewMoreWrapper: {
@@ -339,7 +343,8 @@ const styles = StyleSheet.create( {
     alignItems: 'center',
     justifyContent: 'center',
     padding: 3,
-    borderRadius: 5
+    borderRadius: 5,
+    backgroundColor: Colors.blue
   },
   labelContainer: {
     backgroundColor: Colors.THEAM_TEXT_COLOR,
@@ -350,8 +355,11 @@ const styles = StyleSheet.create( {
     alignItems: 'center'
   },
   labelText: {
-    fontSize: RFValue( 9 ),
+    fontSize: RFValue(9),
     fontFamily: Fonts.SemiBold,
     color: Colors.white,
-  }
-} )
+  },
+  forwardIcon: {
+    alignSelf: 'center',
+  },
+})
