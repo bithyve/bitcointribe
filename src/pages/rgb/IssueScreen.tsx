ec0d60745a37b2dd678e52bac6d597746062b8e6
akshay8551 <akshaychavan8551@gmail.com>
2024-02-16 10:32:07 +0530

    CTA is not visible in small devices #7077

--- src/pages/rgb/IssueScreen.tsx
+++ src/pages/rgb/IssueScreen.tsx
@@ -3,6 +3,7 @@ import { Keyboard, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react
 import { Input } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { launchImageLibrary } from 'react-native-image-picker'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { RFValue } from 'react-native-responsive-fontsize'
 import {
   widthPercentageToDP as wp
@@ -20,65 +21,65 @@ import Toast from '../../components/Toast'
 import RGBServices from '../../services/RGBServices'
 import { syncRgb } from '../../store/actions/rgb'
 
-export default function IssueScreen( props ) {
+export default function IssueScreen(props) {
 
   const issueType = props.route.params?.issueType
   const dispatch = useDispatch()
-  const [ name, setName ] = useState( '' )
-  const [ description, setDescription ] = useState( '' )
-  const [ totalAmount, setTotalAmount ] = useState( '' )
-  const [ ticker, setTicker ] = useState( '' )
-  const [ attachedfile, setAttachedFile ] = useState( 'Attach File' )
-  const [ requesting, setRequesting ] = useState( false )
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [totalAmount, setTotalAmount] = useState('')
+  const [ticker, setTicker] = useState('')
+  const [attachedfile, setAttachedFile] = useState('Attach File')
+  const [requesting, setRequesting] = useState(false)
   // const [ loading, setLoading ] = useState( false )
 
   async function IssueAssetClick() {
     Keyboard.dismiss()
     try {
       console.log('issueType', issueType)
-      if( issueType === 'collectible' ) {
-        if ( !name || !description || !totalAmount || attachedfile == 'Attach File' ) {
-          Toast( 'Please enter all details.' )
+      if (issueType === 'collectible') {
+        if (!name || !description || !totalAmount || attachedfile == 'Attach File') {
+          Toast('Please enter all details.')
         } else {
-          setRequesting( true )
-          setTimeout(async()=>{
-          const newAsset = await RGBServices.issueRgb25Asset( name, description, totalAmount, attachedfile )
-          setRequesting( false )
-          if( newAsset.assetId ) {
-            props.navigation.goBack()
-            dispatch( syncRgb() )
-            Toast( 'Asset created' )
-          } else {
-            Toast( `Failed ${newAsset.error}` )
-          }
-        },300)
+          setRequesting(true)
+          setTimeout(async () => {
+            const newAsset = await RGBServices.issueRgb25Asset(name, description, totalAmount, attachedfile)
+            setRequesting(false)
+            if (newAsset.assetId) {
+              props.navigation.goBack()
+              dispatch(syncRgb())
+              Toast('Asset created')
+            } else {
+              Toast(`Failed ${newAsset.error}`)
+            }
+          }, 300)
         }
       } else {
-        if ( !ticker || !name || !totalAmount ) {
-          Toast( 'Please enter all details.' )
+        if (!ticker || !name || !totalAmount) {
+          Toast('Please enter all details.')
         } else {
-          setRequesting( true )
-          setTimeout(async()=>{
-          const newAsset = await RGBServices.issueRgb20Asset( ticker, name, totalAmount )
-          setRequesting( false )
-          if( newAsset.assetId ) {
-            props.navigation.goBack()
-            dispatch( syncRgb() )
-            Toast( 'Asset created' )
-          } else {
-            Toast( `Failed ${newAsset.error}` )
-          }
-        },300)
+          setRequesting(true)
+          setTimeout(async () => {
+            const newAsset = await RGBServices.issueRgb20Asset(ticker, name, totalAmount)
+            setRequesting(false)
+            if (newAsset.assetId) {
+              props.navigation.goBack()
+              dispatch(syncRgb())
+              Toast('Asset created')
+            } else {
+              Toast(`Failed ${newAsset.error}`)
+            }
+          }, 300)
         }
       }
-    } catch ( error ) {
-      setRequesting( false )
-      Toast( `Failed ${error}` )
-      console.log( 'error', error )
+    } catch (error) {
+      setRequesting(false)
+      Toast(`Failed ${error}`)
+      console.log('error', error)
     }
   }
 
-  const pickFile = ()=> {
+  const pickFile = () => {
     launchImageLibrary(
       {
         title: 'Select a file',
@@ -87,15 +88,15 @@ export default function IssueScreen( props ) {
         selectionLimit: 1,
       },
       response => {
-        console.log( response )
-        if( response.assets ) {
-          setAttachedFile( response.assets[ 0 ].uri.replace( 'file://', '' ) )
+        console.log(response)
+        if (response.assets) {
+          setAttachedFile(response.assets[0].uri.replace('file://', ''))
 
         }
       },
     )
   }
-  
+
   return (
     <View style={{
       flex: 1, backgroundColor: Colors.backgroundColor
@@ -131,105 +132,111 @@ export default function IssueScreen( props ) {
         </TouchableOpacity>
       </View>
       <HeaderTitle
-        firstLineTitle={'Issue ' + issueType.slice(0,1).toUpperCase() + issueType.slice(1, issueType.length)}
-        secondLineTitle={issueType=== 'collectible' ? 'Enter collectible asset details' : 'Enter coin asset details'}
+        firstLineTitle={'Issue ' + issueType.slice(0, 1).toUpperCase() + issueType.slice(1, issueType.length)}
+        secondLineTitle={issueType === 'collectible' ? 'Enter collectible asset details' : 'Enter coin asset details'}
         infoTextNormal={''}
         infoTextBold={''}
         infoTextNormal1={''}
         step={''}
       />
-
-      <View style={styles.bodySection}>
-        <Input
-          inputContainerStyle={[
-            FormStyles.textInputContainer,
-            styles.textInputContainer,
-          ]}
-          inputStyle={FormStyles.inputText}
-          placeholder={'Asset Name'}
-          placeholderTextColor={FormStyles.placeholderText.color}
-          underlineColorAndroid={'transparent'}
-          value={name}
-          onChangeText={( text ) => {
-            setName( text )
-          }}
-          numberOfLines={1}
-          editable={!requesting}
-        />
-        {issueType != 'coin' &&
+      <KeyboardAwareScrollView
+        // scrollEnabled={false}
+        resetScrollToCoords={{
+          x: 0, y: 0
+        }}
+      >
+        <View style={styles.bodySection}>
           <Input
             inputContainerStyle={[
               FormStyles.textInputContainer,
               styles.textInputContainer,
             ]}
             inputStyle={FormStyles.inputText}
-            placeholder={'Asset Description'}
+            placeholder={'Asset Name'}
             placeholderTextColor={FormStyles.placeholderText.color}
             underlineColorAndroid={'transparent'}
-            value={description}
-            onChangeText={( text ) => {
-              setDescription( text )
+            value={name}
+            onChangeText={(text) => {
+              setName(text)
             }}
-            editable={!requesting}
             numberOfLines={1}
+            editable={!requesting}
           />
-        }
-        {issueType == 'coin' &&
+          {issueType != 'coin' &&
+            <Input
+              inputContainerStyle={[
+                FormStyles.textInputContainer,
+                styles.textInputContainer,
+              ]}
+              inputStyle={FormStyles.inputText}
+              placeholder={'Asset Description'}
+              placeholderTextColor={FormStyles.placeholderText.color}
+              underlineColorAndroid={'transparent'}
+              value={description}
+              onChangeText={(text) => {
+                setDescription(text)
+              }}
+              editable={!requesting}
+              numberOfLines={1}
+            />
+          }
+          {issueType == 'coin' &&
+            <Input
+              inputContainerStyle={[
+                FormStyles.textInputContainer,
+                styles.textInputContainer,
+              ]}
+              inputStyle={FormStyles.inputText}
+              placeholder={'Asset Ticker'}
+              placeholderTextColor={FormStyles.placeholderText.color}
+              underlineColorAndroid={'transparent'}
+              value={ticker}
+              onChangeText={(text) => {
+                setTicker(text.toUpperCase())
+              }}
+              numberOfLines={1}
+              editable={!requesting}
+              autoCapitalize={'characters'}
+            />
+          }
           <Input
             inputContainerStyle={[
               FormStyles.textInputContainer,
               styles.textInputContainer,
             ]}
             inputStyle={FormStyles.inputText}
-            placeholder={'Asset Ticker'}
+            placeholder={'Total Supply Amount'}
             placeholderTextColor={FormStyles.placeholderText.color}
             underlineColorAndroid={'transparent'}
-            value={ticker}
-            onChangeText={( text ) => {
-              setTicker( text.toUpperCase() )
+            value={totalAmount}
+            onChangeText={(text) => {
+              setTotalAmount(text.replace(/[^0-9]/g, ''),)
             }}
+            keyboardType="number-pad"
             numberOfLines={1}
             editable={!requesting}
-            autoCapitalize={'characters'}
           />
-        }
-        <Input
-          inputContainerStyle={[
-            FormStyles.textInputContainer,
-            styles.textInputContainer,
-          ]}
-          inputStyle={FormStyles.inputText}
-          placeholder={'Total Supply Amount'}
-          placeholderTextColor={FormStyles.placeholderText.color}
-          underlineColorAndroid={'transparent'}
-          value={totalAmount}
-          onChangeText={( text ) => {
-            setTotalAmount( text.replace( /[^0-9]/g, '' ), )
-          }}
-          keyboardType="number-pad"
-          numberOfLines={1}
-          editable={!requesting}
-        />
-        {issueType != 'coin' &&
-          <TouchableOpacity onPress={pickFile} activeOpacity={0.6} disabled={requesting} style={[ FormStyles.textInputContainer, {
-            marginHorizontal: 12, alignItems: 'center', backgroundColor: 'white'
-          } ]}>
-            <Text style={attachedfile == 'Attach File' ? styles.attachPlaceholderText : styles.attachText}>{attachedfile}</Text>
-          </TouchableOpacity>
-        }
-        <View style={styles.footerSection}>
-          <TouchableOpacity disabled={requesting} activeOpacity={0.6} onPress={IssueAssetClick}>
-            <View
-              style={styles.IssueAssetWrapper}
-            >
-              <Text style={styles.IssueAssetText}>{'Submit'}</Text>
-            </View>
-          </TouchableOpacity>
+          {issueType != 'coin' &&
+            <TouchableOpacity onPress={pickFile} activeOpacity={0.6} disabled={requesting} style={[FormStyles.textInputContainer, {
+              marginHorizontal: 12, alignItems: 'center', backgroundColor: 'white'
+            }]}>
+              <Text style={attachedfile == 'Attach File' ? styles.attachPlaceholderText : styles.attachText}>{attachedfile}</Text>
+            </TouchableOpacity>
+          }
+          <View style={styles.footerSection}>
+            <TouchableOpacity disabled={requesting} activeOpacity={0.6} onPress={IssueAssetClick}>
+              <View
+                style={styles.IssueAssetWrapper}
+              >
+                <Text style={styles.IssueAssetText}>{'Submit'}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </KeyboardAwareScrollView>
       <ModalContainer
-        onBackground={()=>{}}
-        closeBottomSheet={() => {}}
+        onBackground={() => { }}
+        closeBottomSheet={() => { }}
         visible={requesting}
       >
         <RGBIntroModal
@@ -238,20 +245,20 @@ export default function IssueScreen( props ) {
           otherText={'Syncing assets with RGB nodes'}
           proceedButtonText={'Continue'}
           isIgnoreButton={false}
-          closeModal={()=> setRequesting( false )}
-          onPressProceed={() => {setRequesting( false )}}
+          closeModal={() => setRequesting(false)}
+          onPressProceed={() => { setRequesting(false) }}
           onPressIgnore={() => {
-            setRequesting( false )
+            setRequesting(false)
           }}
           isBottomImage={true}
-          bottomImage={require( '../../assets/images/icons/contactPermission.png' )}
+          bottomImage={require('../../assets/images/icons/contactPermission.png')}
         />
       </ModalContainer>
     </View>
   )
 }
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
   rootContainer: {
   },
   bodySection: {
@@ -271,8 +278,8 @@ const styles = StyleSheet.create( {
     marginVertical: 20
   },
   IssueAssetWrapper: {
-    height: wp( '13%' ),
-    width: wp( '35%' ),
+    height: wp('13%'),
+    width: wp('35%'),
     justifyContent: 'center',
     borderRadius: 8,
     alignItems: 'center',
@@ -281,7 +288,7 @@ const styles = StyleSheet.create( {
   },
   IssueAssetText: {
     color: Colors.white,
-    fontSize: RFValue( 13 ),
+    fontSize: RFValue(13),
     fontFamily: Fonts.Medium
   },
   attachPlaceholderText: {
@@ -289,7 +296,7 @@ const styles = StyleSheet.create( {
     paddingHorizontal: 20,
     color: FormStyles.placeholderText.color,
     fontFamily: Fonts.Medium,
-    fontSize: RFValue( 12 ),
+    fontSize: RFValue(12),
     textAlign: 'left',
   },
   attachText: {
@@ -297,8 +304,8 @@ const styles = StyleSheet.create( {
     paddingHorizontal: 20,
     color: Colors.textColorGrey,
     fontFamily: Fonts.Medium,
-    fontSize: RFValue( 12 ),
+    fontSize: RFValue(12),
     textAlign: 'left',
   }
 
-} )
+})
