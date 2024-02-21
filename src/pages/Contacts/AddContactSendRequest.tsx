f6073d3d2ed964247d2ff7ea1604fc849aaa41f9
shyam-cakesoft <shyam@cakesofttech.com>
2024-02-21 09:18:03 +0530

    fnf qr reject fix

--- src/pages/Contacts/AddContactSendRequest.tsx
+++ src/pages/Contacts/AddContactSendRequest.tsx
@@ -277,6 +277,7 @@ export default function AddContactSendRequest( props ) {
           encryptionHint,
           walletName: wallet.walletName,
           version: appVersion,
+          channelAddress: channelKey
         } )
       )
     }
